067d33b711c565908a45b9fbead47e27bfde4f7d
Jeff <jzinger100@gmail.com>
2017-02-14 16:16:21 +0100

    iteration 3 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,35 +1,99 @@
-$.ajax({
-  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
-  method: "GET",
-  success: function (response) {
-		var coinData = JSON.parse(response).bpi;
-		console.log(coinData);
-		var chartData = [];
-		var chartLabels = [];
-		var ctx = $("#myChart");
+// $.ajax({
+//   url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+//   method: "GET",
+//   success: function (response) {
+// 		var coinData = JSON.parse(response).bpi;
+// 		console.log(coinData);
+// 		var chartData = [];
+// 		var chartLabels = [];
+// 		var ctx = $("#myChart");
+//
+// 		for (var date in coinData) {
+// 			chartData.push(coinData[date]);
+// 			chartLabels.push(date);
+// 		}
+//
+// 		console.log(chartData);
+//
+// 		var myLineChart = new Chart(ctx, {
+// 	    type: 'line',
+// 			data: {
+// 				datasets: [{
+// 					data: chartData,
+// 					label: 'dataset1'
+// 				}],
+// 				labels: chartLabels
+// 			}
+// 		});
+//
+// 		console.log(myLineChart);
+//
+//   },
+//   error: function (err) {
+//     console.log(err);
+//   },
+// })
 
-		for (var date in coinData) {
-			chartData.push(coinData[date]);
-			chartLabels.push(date);
-		}
 
-		console.log(chartData);
+var typingTimer;                //timer identifier
+var doneTypingInterval = 1000;  //time in ms, 5 second for example
+var $input = $('.input-date');
 
-		var myLineChart = new Chart(ctx, {
-	    type: 'line',
-			data: {
-				datasets: [{
-					data: chartData,
-					label: 'dataset1'
-				}],
-				labels: chartLabels
-			}
-		});
+//on keyup, start the countdown
+$input.on('keyup', function () {
+  clearTimeout(typingTimer);
+  typingTimer = setTimeout(doneTyping, doneTypingInterval);
+});
 
-		console.log(myLineChart);
+//on keydown, clear the countdown
+$input.on('keydown', function () {
+  clearTimeout(typingTimer);
+});
 
-  },
-  error: function (err) {
-    console.log(err);
-  },
-})
+	//user is "finished typing," do something
+function doneTyping () {
+	setTimeout(function(){
+
+		var startDate = $("#start-date").val();
+		var endDate 	= $("#end-date").val();
+		var dateURL = "http://api.coindesk.com/v1/bpi/historical/close.json?start=" + startDate + "&end=" + endDate;
+
+		console.log(dateURL);
+
+		$.ajax({
+		  url: dateURL,
+		  method: "GET",
+		  success: function (response) {
+				var coinData = JSON.parse(response).bpi;
+				console.log(coinData);
+				var chartData = [];
+				var chartLabels = [];
+				var ctx = $("#myChart");
+
+				for (var date in coinData) {
+					chartData.push(coinData[date]);
+					chartLabels.push(date);
+				}
+
+				console.log(chartData);
+
+				var myLineChart = new Chart(ctx, {
+			    type: 'line',
+					data: {
+						datasets: [{
+							data: chartData,
+							label: 'dataset1'
+						}],
+						labels: chartLabels
+					}
+				});
+
+				console.log(myLineChart);
+
+		  },
+		  error: function (err) {
+		    console.log(err);
+		  },
+		})
+	},5000)
+}
