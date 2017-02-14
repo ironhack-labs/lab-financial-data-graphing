69dd693cfa3f7652d8f6dc917696611295a87b42
juliapolbach <juliapolbach@gmail.com>
2017-02-14 17:14:41 +0100

    done

--- starter-code/public/javascript/index.js
+++ starter-code/public/javascript/index.js
@@ -1,22 +1,32 @@
 /*jshint esversion:6 */
-function updateData(){
-$.ajax({
-  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
-  method: "GET",
-  dataType: "json",
-  success: function (response) {
-    console.log(response.bpi);
-    let labels = [];
-    let data = [];
-    labels = Object.keys(response.bpi);
-    data = Object.values(response.bpi);
-    createChart(labels, data);
-},
+function updateData(start, end){
+  if (typeof(start)==="string"&&typeof(end)==="string") {
+    var string = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`;
+  }else{
+    var string = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
+  }
+  $.ajax({
+    url: string,
+    method: "GET",
+    dataType: "json",
+    success: function (response) {
+      console.log(response.bpi);
+      let labels = [];
+      let data = [];
+      labels = Object.keys(response.bpi);
+      data = Object.values(response.bpi);
+      console.log(labels, data);
+      createChart(labels, data);
+  },
   error: function (err) {
     console.log(err);
       },
   });
 }
+
+updateData();
+
 function createChart (labelsForChart, dataForChart){
   let data = {
     labels: labelsForChart,
@@ -31,4 +41,12 @@ function createChart (labelsForChart, dataForChart){
   });
 }
 
-updateData();
+$(document).ready(()=>{
+  $('#character-form').submit((event) => {
+    event.preventDefault();
+    var init = document.getElementsByClassName("js-init-date")[0].value;
+    var final = document.getElementsByClassName("js-final-date")[0].value;
+    console.log(typeof(init));
+    updateData(init, final);
+  });
+});
