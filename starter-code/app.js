813628dab82bbfe06cabe78da1afefd23424f56f
Sergi Garcia <sergigarciapena@gmail.com>
2017-05-03 17:19:02 +0200

    done

--- starter-code/app.js
+++ starter-code/app.js
@@ -22,6 +22,9 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//bower
+app.use(express.static(path.join(__dirname, "bower_components")));
+
 app.use('/', index);
 app.use('/users', users);
 
