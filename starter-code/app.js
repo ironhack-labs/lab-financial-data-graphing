7c561b11700ab51a8e7f62c95c66ee05deb4ad93
Juampi <juanpabloloza@gmail.com>
2017-05-03 18:58:35 +0200

    itn 1, 2

--- starter-code/app.js
+++ starter-code/app.js
@@ -15,13 +15,15 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
+
+
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/', index);
 app.use('/users', users);
 
