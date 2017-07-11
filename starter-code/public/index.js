6acd1134fc56ae16f1f7470a624c06939cad579b
Victor Rodriguez <victor.irix@gmail.com>
2017-07-11 13:39:55 +0200

    Paint HTML w/ JS

--- starter-code/public/index.js
+++ starter-code/public/index.js
@@ -1,5 +1,50 @@
 $(document).ready(() => {
-  const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json`;
-  console.log("Hello");
-  $.ajax({url:baseURL, dataType:'json'}).then(data => console.log(data));
+      const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+      console.log("Hello");
+      console.log(Chart);
+      $.ajax({
+        url: baseURL,
+        dataType: 'json'
+      }).then(data => {
+        console.log(data.bpi);
+        const canvas = $("#chart")[0].getContext('2d');
+        console.log(canvas);
+        let myChart = new Chart(canvas, {
+            type: 'bar',
+            data: {
+                labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+                datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero:true
+                        }
+                    }]
+                }
+            }
+        });
+      //end of chart
+      });
 });
