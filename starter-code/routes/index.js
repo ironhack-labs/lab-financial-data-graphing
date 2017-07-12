6090e86ee9d20be90c90776fadaef74460862b5c
JB-Tellez <jb.tellez@gmail.com>
2017-07-12 18:21:53 +0200

    PP exercise done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,7 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
+
   res.render('index', { title: 'Express' });
 });
 
