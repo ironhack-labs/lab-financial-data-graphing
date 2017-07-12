06a1280ad430fd3a645f58a9ac84ae90c99f8f60
Luis Amez <luis.amez.vitoria@gmail.com>
2017-07-12 17:05:32 +0200

    Iteration 3, Celebrate good times

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,28 +1,41 @@
-$.ajax({
-  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
-  method: "GET",
-  success: showMeTheMoney,
-  error: thereIsNoMoney
-});
-
-function showMeTheMoney(response) {
-  const ctx = document.getElementById("myChart");
-  const jsonResponse = JSON.parse(response);
-  const labels = Object.keys(jsonResponse.bpi);
-  const data = Object.values(jsonResponse.bpi);
-  const myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        datasets: [{
-          label: "Bitcoin Price Index",
-          data: data
-        }],
-        labels: labels
-      }
-      // options: options
+$(document).ready(() => {
+  $.ajax({
+    url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+    method: "GET",
+    success: showMeTheMoney,
+    error: thereIsNoMoney
   });
-}
 
-function thereIsNoMoney(error) {
-  console.log(error);
-}
+  $(".form-filters input").on("change", event => {
+    let dateFrom = $("#dateFrom").val();
+    let dateTo = $("#dateTo").val();
+    $.ajax({
+      url: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`,
+      method: "GET",
+      success: showMeTheMoney,
+      error: thereIsNoMoney
+    });
+  });
+
+  function showMeTheMoney(response) {
+    const ctx = document.getElementById("myChart");
+    const jsonResponse = JSON.parse(response);
+    const labels = Object.keys(jsonResponse.bpi);
+    const data = Object.values(jsonResponse.bpi);
+    const myLineChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            label: "Bitcoin Price Index",
+            data: data
+          }],
+          labels: labels
+        }
+        // options: options
+    });
+  }
+
+  function thereIsNoMoney(error) {
+    console.log(error);
+  }
+});
