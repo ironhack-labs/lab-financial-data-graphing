c1ca40eba2445fc4e91c916ee2b1ac611e7ca629
Stephanos Theodotou <stephanostheodotou@Stephanoss-MacBook-Pro.local>
2017-07-12 17:58:08 +0200

    iteration 2, need to render dates on X-Axis

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -4,7 +4,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Financial Data' });
 });
 
 module.exports = router;
