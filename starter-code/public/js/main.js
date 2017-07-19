a9ff290d5074611b42a439a469401f1d0f276dad
Manuel Valle <vallerem17@gmail.com>
2017-07-19 18:28:50 +0200

    Iep

--- starter-code/public/js/main.js
+++ starter-code/public/js/main.js
@@ -53,11 +53,9 @@ $(document).ready(function() {
           e.preventDefault();
            var initalDate = $('#ini').val();
            var endDate = $('#end').val();
-           var cur = $(".dates option[type='text']:checked").val();
-
 
            $.ajax({
-             url: "http://api.coindesk.com/v1/bpi/historical/close.json?start=" + initalDate + "&end=" + endDate + "?currency=" + cur,
+             url: "http://api.coindesk.com/v1/bpi/historical/close.json?start=" + initalDate + "&end=" + endDate,
              method: "GET",
              data: "",
              success: function (response) {
@@ -106,10 +104,67 @@ $(document).ready(function() {
 
           error: function (err) {
 console.log(err);
-console.log(cur);
         },
       });
   });
+
+  $('.currency').on('submit', function(e){
+    e.preventDefault();
+     var cur = $(".currency option[type='text']:checked").val();
+
+     $.ajax({
+       url: "http://api.coindesk.com/v1/bpi/historical/close.json?currency=" + cur,
+       method: "GET",
+       data: "",
+
+       success: function (response) {
+
+        
+           var mainObject = JSON.parse(response);
+           var xData = Object.keys(mainObject.bpi);
+           var yData = $.map(mainObject.bpi, function(val, key) { return val; });
+           // var yData = ;
+          //  console.log(xData);
+          //  console.log(yData);
+
+             var ctx = document.getElementById("myChart").getContext('2d');
+             var myLineChart = new Chart(ctx, {
+                 type: 'line',
+                 data: {
+                     labels: xData,
+                     datasets: [{
+                         label: 'Currency' + cur,
+                         data: yData,
+                         backgroundColor: [
+                             'rgba(255, 10, 87, 0.2)'
+                         ],
+                         borderColor: [
+                             'rgba(55, 59, 6, 1)'
+                         ],
+                         borderWidth: 5
+                     }]
+                 },
+                 options: {
+                     scales: {
+                         yAxes: [{
+                             ticks: {
+                                 beginAtZero:true
+                               }
+                           }]
+                       }
+                   }
+               });
+                console.log(cur);
+  $('#myChart').append(myLineChart);
+
+},
+
+    error: function (err) {
+console.log(err);
+
+  },
+});
+});
 
 
 
