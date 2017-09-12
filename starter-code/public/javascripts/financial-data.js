7fa42aa3108907ab9c97d875a0faf6216b9c865c
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-12 17:22:32 +0200

    third iteration start

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,7 +1,7 @@
 var keys = [];
 var values = [];
 $(() => {
-  $.ajax({
+ function ajaFun() {  $.ajax({
     method: 'GET',
     url: 'http://api.coindesk.com/v1/bpi/historical/close.json',
     success: function(response){
@@ -22,22 +22,8 @@ $(() => {
           datasets: [{
               label: '# of Votes',
               data: values,
-              backgroundColor: [
-                  'rgba(255, 99, 132, 0.2)',
-                  'rgba(54, 162, 235, 0.2)',
-                  'rgba(255, 206, 86, 0.2)',
-                  'rgba(75, 192, 192, 0.2)',
-                  'rgba(153, 102, 255, 0.2)',
-                  'rgba(255, 159, 64, 0.2)'
-              ],
-              borderColor: [
-                  'rgba(255,99,132,1)',
-                  'rgba(54, 162, 235, 1)',
-                  'rgba(255, 206, 86, 1)',
-                  'rgba(75, 192, 192, 1)',
-                  'rgba(153, 102, 255, 1)',
-                  'rgba(255, 159, 64, 1)'
-              ],
+              backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+              borderColor: ['rgba(255,99,132,1)'],
               borderWidth: 1
           }]
       },
@@ -50,9 +36,8 @@ $(() => {
               }]
           }
       }
-  });
- 
-  
-  
-})
+    });
+  })
+ }
+$("#btn").on("click", () => {return ajaFun()} )
 })
