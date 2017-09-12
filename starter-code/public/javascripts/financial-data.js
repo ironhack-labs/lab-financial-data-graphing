de6ac3a5e7961ab722e939d84760f6df73f5ce86
MHFP <markus.hifp@gmail.com>
2017-09-12 19:22:09 +0200

    iteration 4: done - on change of currency timeperiod gets lost though

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -78,6 +78,45 @@ function updateFinancialData(start, end){
   });
 }
 
+$("select").on("change", function(){
+  var crc = $("#currency").val();
+  selectCurrencyFinancialData(crc);
+});
+
+function selectCurrencyFinancialData(crc){
+  $.ajax({
+    url: "http://api.coindesk.com/v1/bpi/historical/close.json?currency="+crc,
+    method: "GET",
+    success: function (response) {
+
+      var obj = JSON.parse(response);
+
+      var array = [];
+      var values = [];
+      var labels = [];
+
+      $.each(obj.bpi, function(x,y){
+        array.push({x,y});
+        values.push(y);
+        labels.push(x);
+      });
+
+      var ctx = document.getElementById("myChart").getContext('2d');
+        var myLineChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: labels,
+            datasets: [{label: "BitcoinIndex", data: array}]
+          }
+      });
+    },
+    error: function (err) {
+      //The callback function that will be executed if the request fails, whether it was a client or a server error
+      //It will have a parameter with error that caused the request to fail
+    },
+  });
+}
+
 
 $("#createGraph").on("click", function(){
   getFinancialData();
