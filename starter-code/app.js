9c675dead6db4222b97d2439730fe3298000058c
Carlos Cabezas <carloscabezascontra@gmail.com>
2017-09-12 15:49:59 +0200

    primer paso

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var expressLayouts = require('express-ejs-layouts');
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -13,6 +13,8 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.set('layout', 'layout');
+app.use(expressLayouts);
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
