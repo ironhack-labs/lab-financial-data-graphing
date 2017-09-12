36d3d44ba00e14b34e4cfaebdffb07073f913653
Álvar <alvar@MacBook-Air-de-Alvar.local>
2017-09-12 17:05:06 +0200

    done+abit of bonus

--- starter-code/public/javascript/financial-data.js
+++ starter-code/public/javascript/financial-data.js
@@ -1,7 +1,16 @@
 $(document).ready(function(){
   console.log("entro");
+  $("#submit").on('click',function(e){
+    e.preventDefault();
+  const start=$("#start-date").val();
+  const end=$("#end-date").val();
+  const currency=$("select option").val();
+  showChart(start,end,currency);
+});
+function showChart(start,end,currency){
+
   $.ajax({
-    url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+    url: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`,
     method: "GET",
 
     success: function (response) {
@@ -12,6 +21,7 @@ $(document).ready(function(){
       const dates=(Object.keys(data.bpi));
       const price=(Object.values(data.bpi));
       createChart(dates,price);
+
     },
     error: function (err) {
       //The callback function that will be executed if the request fails, whether it was a client or a server error
@@ -19,6 +29,7 @@ $(document).ready(function(){
       console.log(err);
     },
   });
+}
   function createChart(dates,price){
   var ctx = document.getElementById('myChart').getContext('2d');
 var chart = new Chart(ctx, {
