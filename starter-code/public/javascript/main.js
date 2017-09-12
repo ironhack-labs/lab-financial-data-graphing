f3ba127f8ed701dfc9b219f2d762fe8227d0f0f0
ByronGBP <byrongbp@gmail.com>
2017-09-12 12:29:49 +0200

    first iteration done

---
+++ starter-code/public/javascript/main.js
@@ -0,0 +1,10 @@
+$.ajax({
+  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+  method: "GET",
+  success: function (response) {
+    console.log(response);
+  },
+  error: function (err) {
+    console.log(err);
+  },
+});
