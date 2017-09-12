46b765ae24f5a9bd26213bbe47cf1a6f6bdaf91d
MHFP <markus.hifp@gmail.com>
2017-09-12 13:00:11 +0200

    chart for bar-graph implemented

--- starter-code/app.js
+++ starter-code/app.js
@@ -27,6 +27,7 @@ app.use(express.static(path.join(__dirname, 'bower_components')));
 app.use('/', index);
 app.use('/users', users);
 
+
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
