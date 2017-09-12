3c75e8f46e0a3d2be56ab635051dd61ae8bd6e25
lele <lele@crackberry.local>
2017-09-12 12:46:03 +0200

    retrive bpi obj data

--- starter-code/public/js/financial-data.js
+++ starter-code/public/js/financial-data.js
@@ -3,6 +3,10 @@ function getFinancial() {
     url: "http://api.coindesk.com/v1/bpi/historical/close.json",
     method: "GET",
     success: function (response) {
+
+     var parse = JSON.parse(response);
+     console.log(parse.bpi);
+
       console.log(response);
     },
     error: function (err) {
@@ -14,3 +18,44 @@ function getFinancial() {
 $("#button").on('click', function(){
   getFinancial();
 })
+
+
+
+
+
+// var myChart = new Chart(ctx, {
+//     type: 'line',
+//     data: {
+//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//         datasets: [{
+//             label: '# of Votes',
+//             data: [12, 19, 3, 5, 2, 3],
+//             backgroundColor: [
+//                 'rgba(255, 99, 132, 0.2)',
+//                 'rgba(54, 162, 235, 0.2)',
+//                 'rgba(255, 206, 86, 0.2)',
+//                 'rgba(75, 192, 192, 0.2)',
+//                 'rgba(153, 102, 255, 0.2)',
+//                 'rgba(255, 159, 64, 0.2)'
+//             ],
+//             borderColor: [
+//                 'rgba(255,99,132,1)',
+//                 'rgba(54, 162, 235, 1)',
+//                 'rgba(255, 206, 86, 1)',
+//                 'rgba(75, 192, 192, 1)',
+//                 'rgba(153, 102, 255, 1)',
+//                 'rgba(255, 159, 64, 1)'
+//             ],
+//             borderWidth: 1
+//         }]
+//     },
+//     options: {
+//         scales: {
+//             yAxes: [{
+//                 ticks: {
+//                     beginAtZero:true
+//                 }
+//             }]
+//         }
+//     }
+// });
