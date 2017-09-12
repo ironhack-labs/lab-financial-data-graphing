39dad1b280d15ecd4674194b0e208a1aa2d725da
Luisca108 <luiscarlos3106@gmail.com>
2017-09-12 16:59:09 +0200

    2nd iteration done

--- starter-code/JS/app.js
+++ starter-code/JS/app.js
@@ -1,10 +1,34 @@
-
 function getDataInfo() {
   $.ajax({
     url: "http://api.coindesk.com/v1/bpi/historical/close.json",
     method: "GET",
     success: function(response) {
-      console.log(response);
+      const data = response;
+      console.log(JSON.parse(data).bpi);
+      var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+  type: "line",
+   data: {
+        labels: Object.keys(JSON.parse(data).bpi),
+        datasets: [{
+            label: "My First dataset",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: Object.values(JSON.parse(data).bpi),
+        }]
+    },
+  options: {
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
     },
     error: function(err) {
       console.log(err);
