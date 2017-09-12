802213db70b3196c330767cff5d18e678bb6c45d
Danigp <danielentj@gmail.com>
2017-09-12 16:51:27 +0200

    iteration 2

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,16 +1,31 @@
+$("#dataButton").on('click', function() {
+  getFinancialInfo(1);
+});
+
 function getFinancialInfo(id) {
   $.ajax({
     url: "http://api.coindesk.com/v1/bpi/historical/close.json" + id,
     method: "GET",
-    success: function (response) {
-      console.log(response);
+    success: function(response) {
+      const responseJson = JSON.parse(response);
+      console.log(responseJson.bpi);
+      const valuesAxisX = Object.keys(responseJson.bpi)
+      const valuesAxisY = Object.values(responseJson.bpi)
+      console.log(valuesAxisX)
+      console.log(valuesAxisY)
+      const ctx = $("#ctx")
+      let chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          datasets: [{
+            data: valuesAxisY,
+          }],
+          labels: valuesAxisX,
+        },
+      });
     },
-    error: function (err) {
+    error: function(err) {
       console.log(err);
     },
   });
 }
-
-$("#dataButton").on('click', function(){
-  getFinancialInfo(1);
-});
