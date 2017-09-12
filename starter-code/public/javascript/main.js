6ab4de578b8126f68418ba0ed721798a32fcce1e
ByronGBP <byrongbp@gmail.com>
2017-09-12 15:33:40 +0200

    iteration 4 done

--- starter-code/public/javascript/main.js
+++ starter-code/public/javascript/main.js
@@ -1,18 +1,89 @@
 
-$.ajax({
-  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
-  method: "GET",
-  success: function (response) {
-    var aux = $.parseJSON(response);
-    var labels = Object.keys(aux.bpi);
-    var data = Object.values(aux.bpi);
-    renderChart(labels, data);
-  },
-  error: function (err) {
-    console.log(err);
-  },
+//https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
+
+
+const CURRENCY = {
+  eu: "EUR",
+  us: "USD"
+};
+
+$('#chartForm').on('submit', (event) => {
+  event.preventDefault();
+  //getDataFromCoinDesk();
+
+  // let newUrl = createUrl();
+  // console.log(newUrl);
+  // $.ajax({
+  //   url: newUrl,
+  //   method: "GET",
+  //   success: function (response) {
+  //     console.log(response);
+  //     var aux = $.parseJSON(response);
+  //     var labels = Object.keys(aux.bpi);
+  //     var data = Object.values(aux.bpi);
+  //     renderChart(labels, data);
+  //   },
+  //   error: function (err) {
+  //     console.log(err);
+  //   },
+  // });
+  getDataFromCoinDesk();
+
 });
 
+function getDataFromCoinDesk(){
+  let newUrl = createUrl();
+  console.log(newUrl);
+  $.ajax({
+    url: newUrl,
+    method: "GET",
+    success: function (response) {
+      console.log(response);
+      var aux = $.parseJSON(response);
+      var labels = Object.keys(aux.bpi);
+      var data = Object.values(aux.bpi);
+      renderChart(labels, data);
+    },
+    error: function (err) {
+      //TODO:- mostrar por pantalla que ha habido un error
+      console.log(err);
+    },
+  });
+}
+
+function createUrl(){
+  var from = $("#fromDate").val();
+  var to = $("#toDate").val();
+  var currency =  $("#select").val();
+  var url = "http://api.coindesk.com/v1/bpi/historical/close.json";
+  if (from === ""){
+    from = getCurrentDay();
+  }
+  if (to === ""){
+    to = getCurrentDay();
+  }
+
+  newUrl = url + "?start=" + from + "&end=" + to+ "&currency=" + currency;
+  console.log(newUrl);
+  return newUrl;
+}
+
+function getCurrentDay() {
+  var today = new Date();
+  today.setFullYear(2015);
+  var dd = today.getDate();
+  var mm = today.getMonth()+1; //January is 0!
+
+  var yyyy = today.getFullYear();
+  if(dd<10){
+      dd='0'+dd;
+  }
+  if(mm<10){
+      mm='0'+mm;
+  }
+  var newDay = yyyy+'-'+mm+'-'+dd;
+  return newDay;
+  }
 
 function renderChart(labels, data){
   var ctx = document.getElementById("myChart").getContext('2d');
