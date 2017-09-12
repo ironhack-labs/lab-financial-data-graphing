c9f10ce3978f61355c4124755410c0f844569ecf
willjthorp <willthorp99@gmail.com>
2017-09-12 15:03:38 +0200

    done

--- starter-code/public/javascript/financial-data.js
+++ starter-code/public/javascript/financial-data.js
@@ -4,8 +4,50 @@ function getDataAjax(startDate, endDate, currency) {
     method: 'GET',
     success: function(response) {
       var x = JSON.parse(response);
-      console.log(x);
-      // console.log(Object.keys(response));
+      ctx = document.getElementById("myChart").getContext('2d');
+      myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+              labels: Object.keys(x.bpi),
+              datasets: [{
+                  label: 'BPI',
+                  data: Object.values(x.bpi),
+                  backgroundColor: [
+                      'rgba(255, 99, 132, 0.2)',
+                      'rgba(54, 162, 235, 0.2)',
+                      'rgba(255, 206, 86, 0.2)',
+                      'rgba(75, 192, 192, 0.2)',
+                      'rgba(153, 102, 255, 0.2)',
+                      'rgba(255, 159, 64, 0.2)'
+                  ],
+                  borderColor: [
+                      'rgba(255,99,132,1)',
+                      'rgba(54, 162, 235, 1)',
+                      'rgba(255, 206, 86, 1)',
+                      'rgba(75, 192, 192, 1)',
+                      'rgba(153, 102, 255, 1)',
+                      'rgba(255, 159, 64, 1)'
+                  ],
+                  borderWidth: 1
+              }]
+          },
+          options: {
+              responsive: false,
+              scales: {
+                  yAxes: [{
+                      ticks: {
+                          beginAtZero:false
+                      }
+                  }]
+              }
+          }
+      });
+
+      // myChart.data.labels = Object.keys(x.bpi);
+      // myChart.data.datasets[0].data = Object.values(x.bpi);
+      // console.log(x);
+      // console.log(Object.keys(x.bpi));
+      // console.log(Object.values(x.bpi));
     },
     error: function(err) {
       console.log(err);
@@ -16,45 +58,8 @@ function getDataAjax(startDate, endDate, currency) {
 $('document').ready(function() {
   $('.financial-data-load').on('click', function() {
     console.log('clicked');
-    getDataAjax('2016-01-01', '2016-03-01', 'USD');
+    getDataAjax($('#startDate').val(), $('#endDate').val(), $('#currency').val());
   });
+
+  var ctx = document.getElementById("myChart").getContext('2d');
 });
-
-
-// var ctx = document.getElementById("myChart");
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255,99,132,1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero:true
-//                 }
-//             }]
-//         }
-//     }
-// });
