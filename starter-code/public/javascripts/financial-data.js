08eeffb9eec77f1ac794346d95dc676d8e6273ae
nadiacarreiranunez <nadiacarreiranunez@gmail.com>
2017-11-14 17:58:24 +0100

    second itineration

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,3 +1,45 @@
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json', {responseType: 'json'}).then(response=>{
+let values = axios.get('http://api.coindesk.com/v1/bpi/historical/close.json',
+{responseType: 'json'}).then(response=>{
   console.log(response.data);
+  console.log(Object.keys(response.data.bpi));
+    console.log(Object.values(response.data.bpi))
+
+var ctx = document.getElementById("myChart");
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+
+        labels: Object.keys(response.data.bpi),
+        datasets: [{
+            label: '# of Votes',
+            data: Object.values(response.data.bpi),
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
 })
