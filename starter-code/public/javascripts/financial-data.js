c4061b474f15b681dd3c2e929d0f5abdeef387d1
Alber Pedraza <alberpedrazaes@gmail.com>
2017-11-14 15:50:42 +0100

    Iteration 2 - OK

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -14,51 +14,57 @@
 //   },
 // })
 // Make a request for a user with a given ID
+
+$( document ).ready(function() {
 let response = "";
+let fromInput = $('#from').val();
+let toInput = $('#to').val();
 
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
   .then(function (response) {
-    //console.log(response);
-      let obj = response.data.bpi;
-      let dates = Object.keys(obj);
-      let datesValues = Object.values(obj);
-      var ctx = document.getElementById("myChart").getContext('2d');
-      var myChart = new Chart(ctx, {
-          type: 'line',
-          data: {
-              labels: dates,
-              datasets: [{
-                  label: '# of Votes',
-                  data: datesValues,
-                  backgroundColor: [
-                      'rgba(255, 99, 132, 0.2)'
-                  ],
-                  borderColor: [
-                      'rgba(255,99,132,1)'
-                  ],
-                  borderWidth: 1
-              }]
-          },
-          options: {
-              scales: {
-                  yAxes: [{
-                      ticks: {
-                          beginAtZero:true
-                      }
-                  }]
-              }
-          }
-      });
+    console.log(response);
+    getData(response);
 
-      console.log(dates);
       })
   .catch(function (error) {
     console.log(error);
   });
 
 
+function getData(response){
+  let obj = response.data.bpi;
+  let dates = Object.keys(obj);
+  let datesValues = Object.values(obj);
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: dates,
+          datasets: [{
+              label: '# of Votes',
+              data: datesValues,
+              backgroundColor:
+                  'rgba(255, 99, 132, 0.2)'
+              ,
+              borderColor:
+                  'rgba(255,99,132,1)'
+              ,
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
 
-
+}
+});
 // // Optionally the request above could also be done as
 // axios.get('/user', {
 //     params: {
