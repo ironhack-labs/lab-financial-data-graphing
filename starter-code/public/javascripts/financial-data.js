460f2362bb66c4c66382d20e712734a190f3560f
pablohk <pablusmmid@gmail.com>
2017-11-14 13:43:57 +0100

    iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -7,10 +7,64 @@ $( document ).ready(function() {
   }).then(function(response) {
      //console.log(response);
      let data= response.data.bpi;
+
+     let label= Object.keys(data);
+     let value= Object.values(data);
+
      console.log(data);
    //response.data.pipe(fs.createWriteStream('ada_lovelace.jpg'));
-  });
 
+   let ctx = document.getElementById("myChart").getContext('2d');
+   let myChart = new Chart(ctx, {
+       type: 'line',
+       data: {
+           labels:label,
+           datasets: [{
+               label: '# of Votes',
+               data: value,
+               backgroundColor: [
+                   'rgba(255, 99, 132, 0.2)',
+                   'rgba(54, 162, 235, 0.2)',
+                   'rgba(255, 206, 86, 0.2)',
+                   'rgba(75, 192, 192, 0.2)',
+                   'rgba(153, 102, 255, 0.2)',
+                   'rgba(255, 159, 64, 0.2)'
+               ],
+               borderColor: [
+                   'rgba(255,99,132,1)',
+                   'rgba(54, 162, 235, 1)',
+                   'rgba(255, 206, 86, 1)',
+                   'rgba(75, 192, 192, 1)',
+                   'rgba(153, 102, 255, 1)',
+                   'rgba(255, 159, 64, 1)'
+               ],
+               borderWidth: 1
+           }]
+       },
+       options: {
+           scales: {
+               yAxes: [{
+                   ticks: {
+                       beginAtZero:true
+                   }
+               }]
+           }
+       }
+   });
+
+
+
+
+
+
+
+
+
+
+
+
+
+  });
 
 
 
