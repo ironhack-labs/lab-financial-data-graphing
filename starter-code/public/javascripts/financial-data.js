a8c07293f1c31c496d69e771346dcafff7815273
Sara <saraeme20@gmail.com>
2017-11-14 13:46:26 +0100

    first commit

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,44 @@
+const currency = getElementById("currency");
+
+const url = "http://api.coindesk.com/v1/bpi/historical/close.json";
+function update (){
+  let currency=
+  currency.options[currency.selectedIndex].value;
+  let currencyUrl= url+currency=
+
+}
+function ejecute (url){
+
+  axios.get(url)
+  .then(function(response) {
+    console.log(response);
+const ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+
+    type: 'line',
+    data: {
+      labels: Object.keys(response.data.bpi),
+      datasets: [{
+          data: Object.values(response.data.bpi),
+          label: "Bitcoin price index",
+          borderColor: "Grey",
+          fill: true
+        }
+      ]
+    },
+    options: {
+      title: {
+        display: true,
+        text: ''
+      }
+    }
+  });
+
+  })
+  .catch(function(error) {
+    console.log(error);
+  });
+}
+
+
+ejecute(url);
