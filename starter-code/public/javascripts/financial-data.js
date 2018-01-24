638e1563a951c15328653aceb8375fc383ca4d94
mouyaq <mouyaq@gmail.com>
2018-01-24 01:07:17 +0100

    feat: Iteration 3 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,39 +1,42 @@
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-.then(function (response) {
-  console.log(response.data.bpi);
+function getBpiData() {
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+    if( startDate > endDate ) {
+        alert("END DATE must be greater than START DATE");
+    }
+    else {
+        axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+        .then(function (response) {
+            console.log(response.data.bpi);
+      
+            const labels = Object.keys(response.data.bpi);
+            const datas = Object.values(response.data.bpi);
+      
+            var ctx = document.getElementById("myChart").getContext('2d');
+      
+            var myLineChart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: labels,
+                    datasets: [{ 
+                        data: datas,
+                        label: "Bitcoin Price Index",
+                        borderColor: "#3e95cd",
+                        fill: true
+                    }]
+                },
+                options: {
+                    responsive: false,
+                    title: {
+                        display: true,
+                        text: 'Bitcoins'
+                    }
+                }
+            });
+        })
+        .catch(function (error) {
+            console.log(error);
+        });
+    }
 
-  const labels = Object.keys(response.data.bpi);
-  const datas = Object.values(response.data.bpi);
-
-    var ctx = document.getElementById("myChart").getContext('2d');
-
-    var myLineChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: labels,
-          datasets: [{ 
-              data: datas,
-              label: "Bitcoin Price Index",
-              borderColor: "#3e95cd",
-              fill: true
-            }]
-        },
-        options: {
-          title: {
-            display: true,
-            text: 'Bitcoins'
-          }
-        }
-      });
-
-})
-.catch(function (error) {
-  console.log(error);
-});
-
-
-
-
-
-
-    
+}
