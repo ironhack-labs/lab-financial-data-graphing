e4e218975fd5fcd46f2bad222991f78d3d4c8c99
mouyaq <mouyaq@gmail.com>
2018-01-24 19:17:56 +0100

    feat: Iteration 4 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,42 +1,44 @@
 function getBpiData() {
     const startDate = document.getElementById("startDate").value;
     const endDate = document.getElementById("endDate").value;
-    if( startDate > endDate ) {
-        alert("END DATE must be greater than START DATE");
-    }
-    else {
-        axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
-        .then(function (response) {
-            console.log(response.data.bpi);
-      
-            const labels = Object.keys(response.data.bpi);
-            const datas = Object.values(response.data.bpi);
-      
-            var ctx = document.getElementById("myChart").getContext('2d');
-      
-            var myLineChart = new Chart(ctx, {
-                type: 'line',
-                data: {
-                    labels: labels,
-                    datasets: [{ 
-                        data: datas,
-                        label: "Bitcoin Price Index",
-                        borderColor: "#3e95cd",
-                        fill: true
-                    }]
-                },
-                options: {
-                    responsive: false,
-                    title: {
-                        display: true,
-                        text: 'Bitcoins'
+    const currency = document.getElementById("currency").value;
+    console.log(startDate + " " + endDate + " " + currency);
+    if( startDate && endDate ) {
+        if ( startDate < endDate ) {
+            axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
+            .then(function (response) {
+                const labels = Object.keys(response.data.bpi);
+                const datas = Object.values(response.data.bpi);
+          
+                const canvas = document.getElementById("myChart");
+                const ctx = canvas.getContext('2d');
+                //ctx.fillRect(0, 0, canvas.width, canvas.height);
+                const myLineChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: labels,
+                        datasets: [{ 
+                            data: datas,
+                            label: `Bitcoin Price Index in ${currency}`,
+                            borderColor: "#3e95cd",
+                            fill: true
+                        }]
+                    },
+                    options: {
+                        //responsive: false,
+                        title: {
+                            display: true,
+                            text: 'Bitcoins'
+                        }
                     }
-                }
+                });
+            })
+            .catch(function (error) {
+                console.log(error);
             });
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
+        }
+        else {
+            alert("endDate must be greater than startDate");
+        }
     }
-
 }
