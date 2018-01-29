bb116beec36cd79eb2ebfcf64f0d54455142d545
Barbara Sanser <bsanser@gmail.com>
2018-01-29 10:53:52 +0100

    ft: final commit

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,29 +1,16 @@
-// querría haber podido poner el canvas con menos altura pero no sé cómo
-//también me habría gustado poner un icono en el title pero sólo lee plain text y no entiendo bien como cambiarlo
-// para que me muestre los datos cuando cambio de currency necesitaría recargar los datos cada vez que se selecciona una moneda.
+
 
 var ctx = document.getElementById("myChart").getContext("2d");
 var currency = "USD";
 
-// Get current rate:
-
-//aquí habría que meter la variable currency en lugar de USD (he intentado meter `${currency} en vez de USD pero no funciona)
-// no sé por qué el lastUpdate no se me pinta, si en el console.log sí que sale
-
-
-axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(data){
-  currentRate= data.data.bpi.USD.rate; // quería que sólo me mostrara 2 decimales pero .toFixed(2) hace que no se me muestre.
-  lastUpdate= data.data.time.updated;
-  $('#current-rate').text(currentRate + ' ' + currency);
-  $('#last-checked').text(lastUpdate + ' ' + currency);
-  console.log(lastUpdate);
-
-
-});
 
 
 function drawChart(dates, values) {
+
   new Chart(ctx, {
+    options: {
+      responsive: false
+    },
     type: 'line',
     data: {
       labels: dates,
@@ -55,26 +42,41 @@ function drawChart(dates, values) {
 
 function getCurrency(){
   currency = this.value.split(" ")[0];
-  console.log(currency);
+  getDataAndDrawChart();
 }
-  // Currencies (onchange event). Aqui supongo que es donde le debería decir que me recargue los datos cada vez que cambio la currency
+
+
+
+
+
 $("#currency").change(getCurrency);
+
+
 
 
 // Get historical data by currency
 
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json?currency=' + currency +'').then(function(data) {
+function getDataAndDrawChart() {
+  axios.get('http://api.coindesk.com/v1/bpi/historical/close.json?currency=' + currency +'').then(function(data) {
+    let dates = Object.keys(data.data.bpi);
+    let values = Object.values(data.data.bpi);
 
-  let dates = Object.keys(data.data.bpi);
-  let values = Object.values(data.data.bpi);
+    drawChart(dates, values);
+    let minVal = Math.min(...values);
+    let maxVal = Math.max(...values);
+    $('#max').text(maxVal + ' ' + currency);
+    $('#min').text(minVal + ' ' + currency);
+  });
 
-  drawChart(dates, values);
-  let minVal = Math.min(...values);
-  let maxVal = Math.max(...values);
-  $('#max').text(maxVal + ' ' + currency);
-  $('#min').text(minVal + ' ' + currency);
+  axios.get('https://api.coindesk.com/v1/bpi/currentprice.json').then(function(data){
+    currentRate= data.data.bpi[currency].rate_float.toFixed(2); // quería que sólo me mostrara 2 decimales pero .toFixed(2) hace que no se me muestre.
+    lastUpdate= data.data.time.updated;
+    $('#current-rate').text(currentRate + ' ' + currency);
+    $('#last-checked').text(lastUpdate + ' ' + currency);
+  });
+}
 
-});
+getDataAndDrawChart();
 
 
 
