716670e56324aa360277a195ca7d05588fd3bdb3
alltimelaura13 <laura.marcos@ricoh.es>
2018-01-30 09:10:10 +0100

    iteracion 1 esta si

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,8 +1,8 @@
 
-//Add the Axios reference in the HTML through CDN to be able to use it
+//iteracion 1 Axios reference
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
 
-//You have to create an Axios Request to this URL and get the date. Use a console.log() to be sure that we are getting the correct data
+//iteracion 1 You have to create an Axios Request to this URL and get the date. Use a console.log() to be sure that we are getting the correct data
  .then(function (response) {
    console.log(response);
  })
