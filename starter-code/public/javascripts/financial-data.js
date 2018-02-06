1a44cd0975169f56307d78f51d046d1dbce77cfa
Luis Herrera <la.herrera457@gmail.com>
2018-02-06 12:57:07 +0100

    retrieving keys and values separated

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -7,7 +7,20 @@ function main () {
   function getCoinInfo (bpi) {
     coinApi.get(bpi)
       .then(response => {
-        console.log(response.data);
+        const data1 = [];
+        const data2 = [];
+
+        for (const property in response.data.bpi) {
+          if (!response.data.bpi.hasOwnProperty(property)) {
+            continue;
+          }
+
+          data1.push(property);
+          data2.push(response.data.bpi[property]);
+        }
+
+        console.log(data1);
+        console.log(data2);
       })
       .catch(err => {
         console.error(err);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -2,8 +2,12 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+router.get('/chart', function (req, res, next) {
+  res.render('chart', { title: 'Express' });
 });
 
 module.exports = router;
