1b89beb5cc36165cbd4f81cd962355f78e08d10c
kissy89 <stephanie.senoner@gmail.com>
2018-02-06 11:38:38 +0100

    CDN, JS connection done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+var express = require('express')
+var router = express.Router()
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', function (req, res, next) {
+  res.render('index', { title: 'Ajax PP' })
+})
 
-module.exports = router;
+module.exports = router
