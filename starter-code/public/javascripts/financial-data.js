8de3d31a6e3ea9ff6584f37c00001e01400109eb
Kathia Rieser <kath.medulla@gmail.com>
2018-02-06 19:18:47 +0100

    segunda iteracion

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,3 +1,7 @@
+//Globals vars
+var ctx = document.getElementById("myChart").getContext('2d');
+let dateProof;
+
 //HTTP request
 axios({
   //"The HTTP method (verb) we are going to use"
@@ -18,9 +22,61 @@ axios({
 
 axios.get('/')
   .then((response) => {
-    console.log(response.headers.date);
-    //console.log(response.status);
-    //console.log(response.statusText);
-    //console.log(response.headers);
-    //console.log(response.config);
-  });
+
+    dateProof = response.headers.date;
+    data.datasets[0].label = response.headers.date;
+    console.log("asignamos valor" + data.datasets[0].label);
+    
+    var myChart = new Chart(ctx, {
+      type: 'bar',
+      data: data,
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+    });
+
+
+});
+
+
+var data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [{
+      label: '# of Votes proof',
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+      ],
+      borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+      ],
+      borderWidth: 1
+  }]
+}
+
+//console.log("valor variable" + dateProof);
+//data.datasets[0].label = "Tue, 06 Feb 2018 18:10:49 GMT";
+//console.log(data.datasets[0].label);
+
+
+
+//var rightNow = new Date();
+//console.log(myChart.data.datasets[0].label);
+
+
