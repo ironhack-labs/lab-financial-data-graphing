7719acb39b3a392de3300c61c478b381ceca300b
Ana Recuero <anarecsa@gmail.com>
2018-02-06 18:00:27 +0100

    showing data in the graph

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,5 +1,21 @@
+const ctx = document.getElementById("myChart").getContext('2d');
+let data;
+
+
 
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
 .then(response => {
-  console.log(response)
-})
+  console.log(response.data.bpi)
+  let dataData = Object.values(response.data.bpi)
+  let labelsData = Object.keys(response.data.bpi)
+  let myLineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      datasets: [{
+          label: 'Bitcoins Value',
+          data: dataData
+      }],
+      labels: labelsData
+    },
+  });
+}).catch(e => console.log(e))
