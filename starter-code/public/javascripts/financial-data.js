d3668eb5c114d66469a372976f88a1c5fc7c7bef
Pere Amorós <amorospere@gmail.com>
2018-02-06 12:29:09 +0100

    Iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,17 +1,51 @@
-const coinDeskApi = axios.create({
-  baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json'
-})
+function main () {
+  const coinDeskApi = axios.create({
+    baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json'
+  });
 
-function getData(id) {
-  coinDeskApi.get(id)
-  .then(response => {
-    console.log(response.data)
-  })
-  .catch((err) => {
-    console.error(err)
-  })
-}
+  function getData (id) {
+    coinDeskApi.get(id)
+      .then(response => {
+        // separate key and value into different arrays
+        const data = response.data.bpi;
+        const data1 = [];
+        const data2 = [];
 
-document.getElementById("coin-data").onclick = function(){
-  console.log(getData());
+        for (let property in data) {
+          if (!data.hasOwnProperty(property)) {
+            continue;
+          }
+
+          data1.push(property);
+          data2.push(data[property]);
+        }
+        const ctx = document.getElementById('myChart').getContext('2d');
+        const myChart = new Chart(ctx, {
+          type: 'line',
+          data: {
+            labels: data1,
+            datasets: [{
+              label: 'Bitcoin Price Index',
+              data: data2,
+              borderWidth: 1
+            }]
+          },
+          options: {
+            scales: {
+              yAxes: [{
+                ticks: {
+                  beginAtZero: true
+                }
+              }]
+            }
+          }
+        });
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+
+  getData();
 }
+window.onload = main;
