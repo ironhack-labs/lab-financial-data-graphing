455be3a1584d4d7357075e72a959eb5441771401
Enrique Hernández <l6cenrique@gmail.com>
2018-02-06 19:45:03 +0100

    Done Iteration 1 and 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,19 +1,48 @@
+axios
+    .get('http://api.coindesk.com/v1/bpi/historical/close.json')
+    .then((response) => {
 
-// const axios = require("")
+        var ctx = document.getElementById("myChart").getContext('2d');
+        var myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: Object.keys(response.data.bpi),
+                datasets: [{
+                    label: 'Bitcoin price index',
+                    data: Object.values(response.data.bpi)                        
+                    ,
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
+        });
 
-// const finanApi = axios.create({
-//     baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json",
-// })
-
-
-// finan.get("/", (req, res, next) => {
-// console.log(baseURL.bpi)
-
-// })
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-    .then(function (response) {
-        console.log(response.data);
     })
-    .catch(function (error) {
+
+    .catch((error) => {
         console.log(error);
-    });
+    })
