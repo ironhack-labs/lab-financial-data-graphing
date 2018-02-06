be2952f93c7709f232b77be2846c34a55630ec81
Adrián Chacón <adrianchaconmadueno@gmail.com>
2018-02-06 18:29:21 +0100

    Iteración x

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,13 +1,33 @@
 const baseURL = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+let ctx = document.getElementById("myChart").getContext('2d');
 
-document.getElementById("coinButton").onclick = function(){
-    axios.get(baseURL)
+axios.get(baseURL)
     .then(response => {
-      console.log(response.data)
+        console.log(response);
+        let myChart = new Chart(ctx, {
+            type: 'line',
+            data: {
+                labels: Object.keys(response.data.bpi),
+                datasets: [{
+                    label: 'BTC/$',
+                    data: Object.values(response.data.bpi),
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)'
+                    ],
+                    borderWidth: 1
+                }]
+            },
+            options: {
+            }
+        });
+        
     })
     .catch(err => {
-      console.error(err)
+        console.error(err)
     })
-}
+
 
 
