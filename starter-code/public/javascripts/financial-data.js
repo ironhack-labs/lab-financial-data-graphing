1b89890264652f014f79f91102dfec85a232da9e
Pere Amorós <amorospere@gmail.com>
2018-02-06 12:41:27 +0100

    Iteration 2 - fixed function

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -7,26 +7,19 @@ function main () {
     coinDeskApi.get(id)
       .then(response => {
         // separate key and value into different arrays
-        const data = response.data.bpi;
-        const data1 = [];
-        const data2 = [];
+        const data = getSeparateData(response.data.bpi);
+        const xData = data[0];
+        const yData = data[1];
 
-        for (let property in data) {
-          if (!data.hasOwnProperty(property)) {
-            continue;
-          }
-
-          data1.push(property);
-          data2.push(data[property]);
-        }
+        // create the chart
         const ctx = document.getElementById('myChart').getContext('2d');
         const myChart = new Chart(ctx, {
           type: 'line',
           data: {
-            labels: data1,
+            labels: xData,
             datasets: [{
               label: 'Bitcoin Price Index',
-              data: data2,
+              data: yData,
               borderWidth: 1
             }]
           },
@@ -44,6 +37,21 @@ function main () {
       .catch((err) => {
         console.error(err);
       });
+  }
+
+  function getSeparateData (data) {
+    const data1 = [];
+    const data2 = [];
+
+    for (let property in data) {
+      if (!data.hasOwnProperty(property)) {
+        continue;
+      }
+
+      data1.push(property);
+      data2.push(data[property]);
+    }
+    return [data1, data2];
   }
 
   getData();
