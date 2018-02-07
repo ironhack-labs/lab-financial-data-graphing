533e454c7c748cb4ee9b2f8a577b546b5f4bb513
Ana Recuero <anarecsa@gmail.com>
2018-02-07 09:45:11 +0100

    cleaner code

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,54 +1,37 @@
 const ctx = document.getElementById("myChart").getContext('2d');
 let data;
-let fromData = "2018-01-06";
-let toData = "2018-02-05";
+let fromData;
+let toData;
 
-
-
-// axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-// .then(response => {
-//   console.log(response.data.bpi)
-//   let dataData = Object.values(response.data.bpi)
-//   let labelsData = Object.keys(response.data.bpi)
-//   let myLineChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//       datasets: [{
-//           label: 'Bitcoins Value',
-//           data: dataData
-//       }],
-//       labels: labelsData
-//     },
-//   });
-// }).catch(e => console.log(e))
+function drawChart(route){ axios
+    .get(route)
+    .then(response => {
+      console.log(response.data.bpi);
+      let dataData = Object.values(response.data.bpi);
+      let labelsData = Object.keys(response.data.bpi);
+      let myLineChart = new Chart(ctx, {
+        type: "line",
+        data: {
+          datasets: [
+            {
+              label: "Bitcoins Value",
+              data: dataData
+            }
+          ],
+          labels: labelsData
+        }
+      });
+    })
+    .catch(e => console.log(e));
+}
 
 document.getElementById("from").addEventListener('input', function (evt) {
   fromData = document.getElementById("from").value;
-  filterChart()
+  drawChart(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromData}&end=${toData}`)
 });
 document.getElementById("to").addEventListener('input', function (evt) {
   toData = document.getElementById("to").value;
-  filterChart()
+  drawChart(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromData}&end=${toData}`)
 });
 
-
-
-function filterChart(){
-  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromData}&end=${toData}`)
-  .then(response => {
-    console.log(response.data.bpi)
-    let dataData = Object.values(response.data.bpi)
-    let labelsData = Object.keys(response.data.bpi)
-    let myLineChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        datasets: [{
-            label: 'Bitcoins Value',
-            data: dataData
-        }],
-        labels: labelsData
-      },
-    });
-  }).catch(e => console.log(e))
-}
-filterChart()
+drawChart('http://api.coindesk.com/v1/bpi/historical/close.json')
