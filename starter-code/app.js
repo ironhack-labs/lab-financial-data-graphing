796e5d3a7e933d026454093165aeb6855beb4db6
AliciaJz <zilaphono@gmail.com>
2018-02-20 14:13:55 -0600

    algo mas

--- starter-code/app.js
+++ starter-code/app.js
@@ -24,7 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/', financial-data);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,7 +1,51 @@
-function getBitcoinInfo(id) {
-    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+var priceArray = [];
+var dateArray = [];
+
+function getBitcoinInfo() {
+    var startDate = document.getElementById("startDate").value;
+    var endDate = document.getElementById("endDate").value;
+
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json/?start=${startDate}&end=${endDate}`)
         .then(response => {
             console.log(response.data)
+            dateArray= Object.keys(response.date.bpi)
+            priceArray= Object.values(response.date.bpi)
+            var myChart = new Chart(ctx, {
+                type: 'Line',
+                data: {
+                    labels: dateArray,
+                    datasets: [{
+                        label: 'Bitcoin Price Index',
+                        data: priceArray,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)',
+                            'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)',
+                            'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)',
+                            'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderColor: [
+                            'rgba(255,99,132,1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
+                options: {
+                    scales: {
+                        yAxes: [{
+                            ticks: {
+                                beginAtZero:true
+                            }
+                        }]
+                    }
+                }
+            });
         })
         .catch(err => {
             console.error(err)
@@ -11,3 +55,4 @@ function getBitcoinInfo(id) {
 document.getElementById("BitButton").onclick = function () {
     getBitcoinInfo();
 }
+var ctx = document.getElementById("myChart").getContext('2d');
