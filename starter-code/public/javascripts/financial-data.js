1bdf1ec0350d52acce4cbbc2681fef83494e2818
Davinia Roca <daviniaroca@Davinias-MacBook-Air.local>
2018-02-20 15:11:24 -0600

    2 iteration complete

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -8,12 +8,44 @@ var btcdate;
         function getBitCoinDatas() {
             btcApi.get()
             .then(response => {
-                     //btcdate= Object.keys(response.data.bpi);
-                    btcdate= this.Object.values(response.data.bpi);
-                   //console.log(btcprice);
-                   console.log('hola');
-                    return btcdate;
-                  
+                
+                var ctx = document.getElementById("canva").getContext('2d');
+                var myChart = new Chart(ctx, {
+                    type: 'line',
+                    data: {
+                        labels: Object.keys(response.data.bpi),
+                        datasets: [{
+                            label: '# of Votes',
+                            data: Object.values(response.data.bpi),
+                            backgroundColor: [
+                                'rgba(255, 99, 132, 0.2)',
+                                'rgba(54, 162, 235, 0.2)',
+                                'rgba(255, 206, 86, 0.2)',
+                                'rgba(75, 192, 192, 0.2)',
+                                'rgba(153, 102, 255, 0.2)',
+                                'rgba(255, 159, 64, 0.2)'
+                            ],
+                            borderColor: [
+                                'rgba(255,99,132,1)',
+                                'rgba(54, 162, 235, 1)',
+                                'rgba(255, 206, 86, 1)',
+                                'rgba(75, 192, 192, 1)',
+                                'rgba(153, 102, 255, 1)',
+                                'rgba(255, 159, 64, 1)'
+                            ],
+                            borderWidth: 1
+                        }]
+                    },
+                    options: {
+                        scales: {
+                            yAxes: [{
+                                ticks: {
+                                    beginAtZero:true
+                                }
+                            }]
+                        }
+                    }
+                });    
                
             })
             .catch(err => {
@@ -24,13 +56,11 @@ var btcdate;
 
 
 
-            function dataChart(){
-                getBitCoinDatas();
-                console.log( getBitCoinDatas())
-            }
          
             
-            document.getElementById("BTC").onclick= dataChart;
+            document.getElementById("BTC").onclick= function (){
+                    getBitCoinDatas();
+            };
 
             
             
