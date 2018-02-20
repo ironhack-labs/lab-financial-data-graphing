afa5896ee700b18d141ea8824b03e237b5df9547
AliciaJz <zilaphono@gmail.com>
2018-02-20 15:13:37 -0600

    Done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,3 +1,4 @@
+
 var priceArray = [];
 var dateArray = [];
 
@@ -5,47 +6,14 @@ function getBitcoinInfo() {
     var startDate = document.getElementById("startDate").value;
     var endDate = document.getElementById("endDate").value;
 
-    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json/?start=${startDate}&end=${endDate}`)
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
         .then(response => {
             console.log(response.data)
-            dateArray= Object.keys(response.date.bpi)
-            priceArray= Object.values(response.date.bpi)
-            var myChart = new Chart(ctx, {
-                type: 'Line',
-                data: {
-                    labels: dateArray,
-                    datasets: [{
-                        label: 'Bitcoin Price Index',
-                        data: priceArray,
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255,99,132,1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderWidth: 1
-                    }]
-                },
-                options: {
-                    scales: {
-                        yAxes: [{
-                            ticks: {
-                                beginAtZero:true
-                            }
-                        }]
-                    }
-                }
-            });
+            dataArray= Object.keys(response.data.bpi)
+            priceArray= Object.values(response.data.bpi)
+
+            friedita(dataArray, priceArray);
+            
         })
         .catch(err => {
             console.error(err)
@@ -55,4 +23,44 @@ function getBitcoinInfo() {
 document.getElementById("BitButton").onclick = function () {
     getBitcoinInfo();
 }
-var ctx = document.getElementById("myChart").getContext('2d');
+
+function friedita(dataArray, priceArray){
+
+    var ctx = document.getElementById("myChart");
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: dataArray,
+            datasets: [{
+                label: 'Valor',
+                data: priceArray,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)',
+                    'rgba(153, 102, 255, 0.2)',
+                    'rgba(255, 159, 64, 0.2)'
+                ],
+                borderColor: [
+                    'rgba(255,99,132,1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero:true
+                    }
+                }]
+            }
+        }
+    });
+} //function
