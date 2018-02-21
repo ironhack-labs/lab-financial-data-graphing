685c0f3b528dbfa90295db38e9b091fc614cf9ca
Pablo Gomez <gomezpa@bellsouth.net>
2018-02-21 14:02:51 -0500

    Done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,55 @@
+const coinDesk = axios.create({
+    baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+})
+
+function getCoinDesk() {
+    axios.get(id)
+    .then(response => {
+        console.log(response.data)
+    })
+    .catch(err => {
+        console.error(err)
+      })
+}
+
+document.getElementById("showCoinButton").onclick = function(){
+    getCoinDesk();
+  }
+
+var ctx = document.getElementById("myChart").getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
