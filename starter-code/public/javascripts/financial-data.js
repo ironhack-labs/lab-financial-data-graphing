2bde1f97456bc9dc960605aa71b0c08347ccd66b
Luna Fidalgo <lunafidalgo@gmail.com>
2018-04-18 16:13:57 +0200

    DONE. Incluyen bonus. Quedan detalles por pulir

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,24 +1,62 @@
-const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+let api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 
-axios
-  .get(api_url)
-  .then(res => res.data)
-  .then(data => {
-    const dates = Object.keys(data.bpi);
-    const prices = Object.values(data.bpi);
-    let ctx = document.getElementById("myChart").getContext("2d");
-    let chart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: dates,
-        datasets: [
-          {
-            label: "Stock Chart",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: prices
-          }
-        ]
-      }
+window.onload = () => {
+  axiosF(api_url);
+};
+
+const axiosF = url => {
+  axios
+    .get(url)
+    .then(res => res.data)
+    .then(data => {
+      drawChart(data);
     });
+
+  document.getElementById("draw-chart").addEventListener("click", () => {
+    api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+    let from = document.getElementById("from").value;
+    let to = document.getElementById("to").value;
+    setTimes(from, to);
+    let currency = document.getElementById("currency").value;
+    setCurrency(currency);
+    axiosF(api_url);
   });
+};
+
+const setCurrency = currency => {
+  api_url += `&currency=${currency}`;
+};
+
+const setTimes = (from, to) => {
+  api_url += `?start=${from}&end=${to}`;
+};
+
+const drawChart = data => {
+  const dates = Object.keys(data.bpi);
+  const prices = Object.values(data.bpi);
+
+  const min = Math.min.apply(null, prices);
+  const max = Math.max.apply(null, prices);
+
+  maxMin(min, max);
+
+  let ctx = document.getElementById("myChart").getContext("2d");
+  let chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: prices
+        }
+      ]
+    }
+  });
+};
+
+const maxMin = (min, max) => {
+  document.getElementById("max-min").innerHTML = `MIN: ${min}, MAX: ${max}`;
+};
