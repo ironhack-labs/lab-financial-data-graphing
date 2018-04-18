223b03f653ea9790c060497d551abdd91d23fc0d
esthvega <esthvega@hotmail.es>
2018-04-18 16:51:44 +0200

    hasta el bonus no incluido

--- js/main.js
+++ js/main.js
@@ -1,48 +1,56 @@
-console.log("js funciona")
+var myURL ='';
 
-const btcApi = axios.create({
-    baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json'
-})
 
-function getBtcInfo() {
-  btcApi.get()
-  .then(response => {
-    console.log(response.data)
-    return response.data.bpi;
-  })
-  .then(response => {
-      drawChart(response);
-  })
 
-  .catch(err => {
-    console.error(err)
-  })
-}
+function getBtcInfo(url) {
 
  
+
+ axios.get(url)
+   .then(response => {
+     console.log(response.data)
+     return response.data.bpi;
+   })
+   .then(response => {
+     drawChart(response);
+   })
+
+   .catch(err => {
+     console.error(err)
+   })
+}
+
+
 const drawChart = bitcoinPrices => {
-    console.log(bitcoinPrices);
-    let stockLabels = Object.keys(bitcoinPrices);
-    let stockPrice = Object.values(bitcoinPrices);
-    console.log(stockLabels)
-    console.log(stockPrice)
-    let ctx = document.getElementById("myChart").getContext("2d");
-    let chart = new Chart(ctx, {
-        type: "line",
-        data: {
-          labels: stockLabels,
-          datasets: [
-            {
-              label: "Stock Chart",
-              backgroundColor: "rgb(255, 99, 132)",
-              borderColor: "rgb(255, 99, 132)",
-              data: stockPrice  
-            }
-          ]
-        }
-    })
+ console.log(bitcoinPrices);
+ let stockLabels = Object.keys(bitcoinPrices);
+ let stockPrice = Object.values(bitcoinPrices);
+ console.log(stockLabels)
+ console.log(stockPrice)
+ let ctx = document.getElementById('ppp').getContext('2d');
+ let chart = new Chart(ctx, {
+   type: 'line',
+   data: {
+     labels: stockLabels,
+     datasets: [{
+       label: 'Stock Chart',
+       data: stockPrice
+     }]
+   }
+ })
 }
 
 
 
-getBtcInfo()
+$(document).ready( function(){
+  $('#btnActualizar').click(function(){
+   fecha1 = $('#fechaInicio').val();
+   fecha2 = $('#fechaFin').val();
+   myURL = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fecha1}&end=${fecha2}`  
+   getBtcInfo(myURL);
+  })
+ 
+ 
+})
+
+;
