8d8622081a6f936d36de7a6fda0721486de8205a
Raul.dll <rdl.llave@gmail.com>
2018-04-18 15:22:49 +0200

    segunda iteraccion

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,33 +1,30 @@
-const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 
+const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+let values;
 axios
   .get(api_url)
-  .then(res => {
-    
-    res.data;
-    console.log(res.data)
-  })
+  .then(res => values = res.data.bpi
+  )
 
-  // .then(data => data.map(e => ({ date: e.date, close: e.close })))
-  // .then(closes => drawChart(closes));
+   .then(closes => drawChart(closes));
 
-// const drawChart = data => {
-//   let stockLabels = data.map(e => e.date);
-//   let stockPrice = data.map(e => e.close);
+const drawChart = data => {
+  console.log(values)
 
-//   let ctx = document.getElementById("myChart").getContext("2d");
-//   let chart = new Chart(ctx, {
-//     type: "bar",
-//     data: {
-//       labels: stockLabels,
-//       datasets: [
-//         {
-//           label: "Stock Chart",
-//           backgroundColor: "rgb(255, 99, 132)",
-//           borderColor: "rgb(255, 99, 132)",
-//           data: stockPrice
-//         }
-//       ]
-//     }
-//   });
-// };
+   let stockDate = Object.keys(values);
+   let stockPrice = Object.values(values);
+  let ctx = document.getElementById("myChart").getContext("2d");
+  let chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: stockDate,
+      datasets: [
+        {
+          label: "Stock Chart",
+          borderColor: "rgb(255, 99, 132)",
+          data: stockPrice
+        }
+      ]
+    }
+  });
+};
