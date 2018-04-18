ae4ce206cf2f60966a9970a9bae963ed0b2de00a
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-18 16:04:23 +0200

    Ya coge los selectores de fecha y actualiza con el boton

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,15 +1,21 @@
-const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
-let values;
-axios.get(api_url)
-  .then(res => ( values = res.data.bpi))
-  //.then(data => data.map(e => ({ date: e.date, close: e.close })))
-  .then(closes => drawChart(closes));
+document.addEventListener('DOMContentLoaded', () => {
+
+  const startDate = document.getElementById("startDate").value;
+  const endDate = document.getElementById("endDate").value;
+  const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
+
+  let values;
+
+  // axios.get(api_url)
+  //   .then(res => (values = res.data.bpi))
+  //   .then(closes => drawChart(closes));
 
   const drawChart = data => {
     console.log(values)
+
     let stockDate = Object.keys(values); // Eje X
     let stockPrice = Object.values(values); // Eje Y
-  
+
     let ctx = document.getElementById("myChart").getContext("2d");
     let chart = new Chart(ctx, {
       type: "line",
@@ -18,11 +24,28 @@ axios.get(api_url)
         datasets: [
           {
             label: "Stock Chart",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
+            backgroundColor: "rgb(96, 96, 96)",
+            borderColor: "rgb(255, 0, 0)",
             data: stockPrice
           }
         ]
       }
     });
   };
+
+  document.getElementById("update").addEventListener('click', () => {
+    const startDate = document.getElementById("startDate").value;
+    const endDate = document.getElementById("endDate").value;
+    const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
+
+    axios.get(api_url)
+      .then(res => (values = res.data.bpi))
+      .then(closes => drawChart(closes));
+
+  })
+
+
+  console.log('IronGenerator JS imported successfully!');
+
+}, false);
+
