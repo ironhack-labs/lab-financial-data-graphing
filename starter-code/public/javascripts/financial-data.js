6866ba259234dd42361c3f5e58ade7cd3a089e2d
RISTO TAPANI RAIHALA <tyokone@RISTOs-MacBook-Pro.local>
2018-04-18 15:48:36 +0200

    solo falta bonus

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -31,19 +31,17 @@ let drawChart = () => {
   });
 };
 
-/* new Chart(ctx, {
-      type: "line",
-      data: {
-          labels: date,
-          datasets: [
-              {
-                  label: "Bitcoin Chart",
-                  backgroundColor: "red",
-                  borderColor: "red",
-                  data: price
-              }
-          ]
-      }
-  })*/
-//.then(data => data.map(e => ({ date: e.date, close: e.close })))
-//.then(closes => drawChart(closes));
+
+document.getElementById("button").addEventListener("click", () =>{
+    let start = document.getElementById("start").value;
+    let end = document.getElementById("end").value;
+    axios.get(`${api_url}?start=${start}&end=${end}`)
+    .then(res => res.data)
+    .then(data => (data = data.bpi))
+    .then(data => {
+      date = Object.keys(data);
+      price = Object.values(data);
+    })
+    .then(closes => drawChart());
+    //.then(res => drawChart())
+})
