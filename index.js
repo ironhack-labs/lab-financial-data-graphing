62b907c877afe97765107aae1cbb4cfca3086cd1
luisnueda <nueda.luis@gmail.com>
2018-04-18 15:41:52 +0200

    iteration 3 done

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
-const api_url = "http://api.coindesk.com/v1/bpi/historical/close.json";
+window.onload = () => {
 
-axios.get(api_url).then(res => {
+  let firstDate = document.getElementById("first-date").value;
+  let seconDate = document.getElementById("second-date").value;
+  
+  document.getElementById("update").addEventListener("click",()=> {
+    firstDate = document.getElementById("first-date").value
+    seconDate = document.getElementById("second-date").value
+
+    console.log(firstDate, seconDate)
+    start();
+  })
+
+  
+  
+  const start = () => {
+    let api_url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${firstDate}&end=${seconDate}`;
+  
+  axios.get(api_url).then(res => {
   
   let keysBpi = Object.keys(res.data.bpi);
   let valuesBpi = Object.values(res.data.bpi);
@@ -22,5 +38,9 @@ axios.get(api_url).then(res => {
     }
   });
 
-});
+})
+}
     
+
+start();
+}
