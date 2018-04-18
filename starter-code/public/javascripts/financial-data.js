8a37cafc889206fd96796355618d3d41763a9bdd
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-18 14:11:01 +0200

    Iteracion 2 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,28 @@
 const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
-
+let values;
 axios.get(api_url)
-  .then(res => console.log(res.data))
+  .then(res => ( values = res.data.bpi))
   //.then(data => data.map(e => ({ date: e.date, close: e.close })))
-  //.then(closes => drawChart(closes));
+  .then(closes => drawChart(closes));
+
+  const drawChart = data => {
+    console.log(values)
+    let stockDate = Object.keys(values); // Eje X
+    let stockPrice = Object.values(values); // Eje Y
+  
+    let ctx = document.getElementById("myChart").getContext("2d");
+    let chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: stockDate,
+        datasets: [
+          {
+            label: "Stock Chart",
+            backgroundColor: "rgb(255, 99, 132)",
+            borderColor: "rgb(255, 99, 132)",
+            data: stockPrice
+          }
+        ]
+      }
+    });
+  };
