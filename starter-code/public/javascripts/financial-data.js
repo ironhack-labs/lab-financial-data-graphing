37609af3206fd6cc0dc3bb3494c5001031b0c3d1
Pedro Piury Blanquez <pedropiuryblanquez@MacBook-Air-de-Pedro.local>
2018-04-18 13:38:12 +0200

    Iteracion 1 done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,6 @@
+const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+
+axios.get(api_url)
+  .then(res => console.log(res.data))
+  //.then(data => data.map(e => ({ date: e.date, close: e.close })))
+.then(closes => drawChart(closes));
