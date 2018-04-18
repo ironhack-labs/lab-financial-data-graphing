137d6860ec82011e62b45b736f6be38504873649
luisnueda <nueda.luis@gmail.com>
2018-04-18 14:28:59 +0200

    Iteracion 2 done

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 const api_url = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
-axios
-  .get(api_url)
-  .then(res => {
-    console.log(Object.keys(res.data.bpi));
-    console.log(Object.values(res.data.bpi));
-    res.data });
+axios.get(api_url).then(res => {
+  
+  let keysBpi = Object.keys(res.data.bpi);
+  let valuesBpi = Object.values(res.data.bpi);
+
+  let ctx = document.getElementById("myChart").getContext("2d");
+
+  let chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: keysBpi,
+      datasets: [
+        {
+          label: "Coin Value",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: valuesBpi
+        }
+      ]
+    }
+  });
+
+});
+    
