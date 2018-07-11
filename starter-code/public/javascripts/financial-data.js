4d914580c781ef5abdac02ad1aa91a9e796d51e0
Paula <paulagarizcun@gmail.com>
2018-07-11 14:17:02 +0200

    Añadiendo data

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,14 +1,52 @@
-
+let arrDate;
+let arrVal;
 
 axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
   .then(res => {
     console.log(res.data)
-    let arrDate = Object.keys(res.data.bpi);
+    arrDate = Object.keys(res.data.bpi);
     console.log(arrDate);
-    let arrVal = Object.values(res.data.bpi);
+    arrVal = Object.values(res.data.bpi);
     console.log(arrVal);
-
+    //return arrDate, arrVal;
   })
   .catch(err => console.log(err));
 
+let ctx = document.getElementById("myChart").getContext('2d');
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: arrDate,
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
   
