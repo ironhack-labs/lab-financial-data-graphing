f9ebd7cb79ea4011e757b2eeb10283797b0f4388
MateoReparaz <mateoreparaz1989@gmail.com>
2018-07-11 16:43:19 +0200

    Hasta iteración 4

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,9 +1,11 @@
 let labels = [];
 let value = [];
 
+let dateOne = "", dateTwo = "", currency = "";
+
 var ctx = document.getElementById("myChart").getContext("2d");
 var myChart = new Chart(ctx, {
-  type: "bar",
+  type: "line",
   data: {
     labels: [],
     datasets: [
@@ -21,7 +23,7 @@ var myChart = new Chart(ctx, {
       yAxes: [
         {
           ticks: {
-            beginAtZero: true
+            beginAtZero: false
           }
         }
       ]
@@ -29,14 +31,41 @@ var myChart = new Chart(ctx, {
   }
 });
 
-axios.get("http://api.coindesk.com/v1/bpi/historical/close.json").then(data => {
-  labels = Object.keys(data.data.bpi);
-  value = Object.values(data.data.bpi);
-  for (var i = 0; i < labels.length; i++) {
-    myChart.data.labels[i] = labels[i];
-    myChart.data.datasets[0].data[i] = value[i];
-    myChart.data.datasets[0].backgroundColor[i] = "rgba(75, 192, 192, 1)";
-  }
-  myChart.update();
-  console.log(labels, value);
-});
+function apiCall(dateOne, dateTwo, currency) {
+  let url = "http://api.coindesk.com/v1/bpi/historical/close.json"
+  if(dateOne != "" && dateTwo != ""){
+    url += `?start=${dateOne}&end=${dateTwo}`
+    if(currency != "") {
+      url += `&currency=${currency}`
+    }
+  } else {
+    if(currency != "") {
+      url += `?currency=${currency}`
+    }
+  } 
+
+  axios
+    .get(url)
+    .then(data => {
+      labels = Object.keys(data.data.bpi);
+      value = Object.values(data.data.bpi);
+      for (var i = 0; i < labels.length; i++) {
+        myChart.data.labels[i] = labels[i];
+        myChart.data.datasets[0].data[i] = value[i];
+      }
+      myChart.update();
+      console.log(labels, value);
+    });
+}
+
+let print = () => {
+  dateTwo = document.getElementById("dateTwo").value;
+  dateOne = document.getElementById("dateOne").value;
+  currency = document.getElementById("currency").value
+  apiCall(dateOne, dateTwo, currency)
+};
+
+apiCall("", "", "");
+document.getElementById("dateTwo").addEventListener("change", print);
+
+document.getElementById("currency").addEventListener("change", print);
