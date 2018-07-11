2131fd89787291f18b551bf26c362c8964584e2e
Anna Hogberg <am.hogberg@gmail.com>
2018-07-11 16:11:02 +0200

    done sin bonus

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,39 +1,39 @@
-const api_url = "http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-06-11&end=2018-07-07&currency=EUR";
 let start = document.getElementById("start").value;
 let end = document.getElementById("end").value;
+let btn = document.getElementById("btn");
 
+const getData = (inDate, outDate) => {
+  const api_url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${inDate}&end=${outDate}&currency=EUR`;
+  return axios.get(api_url).then(res => printTheChart(res.data));
+}
 
-  axios
-  .get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}&currency=EUR`)
-  .then(res => printTheChart(res.data));
-
-  let printTheChart = ((stockData) => {
-    console.log(stockData);
-    let stockLabels = Object.keys(stockData.bpi);
-    let stockPrice = Object.values(stockData.bpi);
-    console.log(stockLabels);
-    console.log(stockPrice);
-    let ctx = document.getElementById('myChart').getContext('2d');
-    let chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: stockLabels,
-        datasets: [{
+let printTheChart = stockData => {
+  console.log(stockData);
+  let stockLabels = Object.keys(stockData.bpi);
+  let stockPrice = Object.values(stockData.bpi);
+  console.log(stockLabels);
+  console.log(stockPrice);
+  let ctx = document.getElementById("myChart").getContext("2d");
+  let chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: stockLabels,
+      datasets: [
+        {
           label: "Stock Chart",
-          borderColor: 'gray',
-          backgroundColor: 'transparent',
-          data: stockPrice,
-        }]
-      }
-    });
-   
+          borderColor: "gray",
+          backgroundColor: "transparent",
+          data: stockPrice
+        }
+      ]
+    }
   });
-
+};
+getData(start, end);
 
 btn.addEventListener("click", () => {
-    boolean = false;
-    printTheChart();
-   });
-
-
-if (boolean) draw();
+  start = document.getElementById("start").value;
+  end = document.getElementById("end").value;
+  console.log(start, end);
+  getData(start, end);
+});
