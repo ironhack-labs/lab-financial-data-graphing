73a66fdd55d357f322acbc2ab39cd5a3988aabbd
Leismale <leismale@gmail.com>
2018-07-11 14:12:04 +0200

    Iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,51 +1,61 @@
-
-
-
-
-
-let data = axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-.then(function(data){
-    chart(data.data)
-    console.log(data.data.bpi);
+$(document).ready(function() {
+  $("#send").click(function() {
+    console.log("hola");
+    let start = $("#start").val();
+    let end = $("#end").val();
+    console.log(start);
+    console.log(end);
+    let data = axios
+      .get(
+        `http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`
+      )
+      .then(function(data) {
+        chart(data.data);
+        console.log(data.data.bpi);
+      });
   });
 
-let chart = function(data) {
-  var ctx = document.getElementById("myChart").getContext('2d');
-  var myChart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: Object.keys(data.bpi),
-      datasets: [{
-        label: '# of Votes',
-        data: Object.values(data.bpi),
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255,99,132,1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        yAxes: [{
-          ticks: {
-            beginAtZero: false
+  let chart = function(data) {
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: Object.keys(data.bpi),
+        datasets: [
+          {
+            label: "Bitcoin Price Index",
+            data: Object.values(data.bpi),
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)"
+            ],
+            borderColor: [
+              "rgba(255,99,132,1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)"
+            ],
+            borderWidth: 1
           }
-        }]
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                beginAtZero: false
+              }
+            }
+          ]
+        }
       }
-    }
-  });
-
-}
+    });
+  };
+});
