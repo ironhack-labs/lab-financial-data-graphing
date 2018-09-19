9337ddf4f5fa147f8d0a4784f025679869f4a223
Chris Lopez <pagarizabal2@gmail.com>
2018-09-19 16:24:10 +0200

    [Iteration 3] done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,42 +1,66 @@
 let ctx = document.getElementById("myChart").getContext('2d');
 
+
+
+document.addEventListener('change',(e)=>{
+  console.log(document.getElementById('from-date').value)
+  console.log(document.getElementById('to-date').value)
+  let startDate = $("#from-date").val()
+  let endDate = $("#to-date").val()
+  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+  .then(res => chart(res.data.bpi))
+   
+}) 
+
+let endDate = document.getElementById("to-date").value;
+
+
+
+
+
+const chart = (value) => {
+  new Chart(ctx,{
+    type: 'line',
+    data: {
+      labels: Object.keys(value),
+      datasets: [{
+        label: 'BTC PRICE',
+        data: Object.values(value),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    },
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+}
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
   .then(res => {
     //console.log(res.data.bpi);
-    var myChart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: Object.keys(res.data.bpi),
-        datasets: [{
-          label: '# of Votes',
-          data: Object.values(res.data.bpi),
-          backgroundColor: [
-            'rgba(255, 99, 132, 0.2)',
-            'rgba(54, 162, 235, 0.2)',
-            'rgba(255, 206, 86, 0.2)',
-            'rgba(75, 192, 192, 0.2)',
-            'rgba(153, 102, 255, 0.2)',
-            'rgba(255, 159, 64, 0.2)'
-          ],
-          borderColor: [
-            'rgba(255,99,132,1)',
-            'rgba(54, 162, 235, 1)',
-            'rgba(255, 206, 86, 1)',
-            'rgba(75, 192, 192, 1)',
-            'rgba(153, 102, 255, 1)',
-            'rgba(255, 159, 64, 1)'
-          ],
-          borderWidth: 1
-        }]
-      },
-      options: {
-        scales: {
-          yAxes: [{
-            ticks: {
-              beginAtZero: true
-            }
-          }]
-        }
-      }
-    });
+    chart(res.data.bpi)
   })
+
+  
+
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -7,7 +7,7 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-
+ 
 
 
 module.exports = router;
