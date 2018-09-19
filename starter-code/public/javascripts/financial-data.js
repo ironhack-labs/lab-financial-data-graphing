75647e4e224ab88dd7afaa44d087c9f4df15faea
wergilia <weronika.zdrojewska@gmail.com>
2018-09-19 17:30:25 +0200

    Dates filter done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,5 +1,3 @@
-
-
 const getData = () => {
     return axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
     .then( res => {
@@ -27,7 +25,49 @@ let printTheChart = ((datos) => {
     });
   });
 
- getData()
+document.getElementById("date-form").onsubmit = function() {
+  event.preventDefault();
+
+  const dataInfo = {
+    startDate: document.getElementById("from-date").value,
+    endDate: document.getElementById("to-date").value
+  };
+  axios.post(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+  .then(res => {
+    printTheChart(dataInfo)  
+  })
+  .catch(e => {
+    console.log(e);
+    
+  })
+}
+
+document.addEventListener("change", (e) => { 
+  console.log(document.getElementById("from-date").value);
+  const dataInfo = {
+    startDate: document.getElementById("from-date").value,
+    endDate: document.getElementById("to-date").value
+  };
+  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${dataInfo.startDate}&end=${dataInfo.endDate}`)
+  .then(res => {
+    printTheChart(res.data.bpi) 
+    console.log(res);
+     
+  })
+  .catch(e => {
+    console.log(e);
+    
+  })
+  
+})
+
+
+
+
+
+ getData();
+
+
 
     
   
