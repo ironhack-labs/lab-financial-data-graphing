64238f90d5d5d67060e15dbe550195620a43c9d3
Dennis <ratenniel@gmail.com>
2018-09-19 17:24:35 +0200

    importado datos, creado gráfico y filtro fecha. Done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,16 +1,53 @@
-
-const coindesk = axios.create({
-  baseURL: 'http://api.coindesk.com/v1/bpi/historical'
-})
-
-let startDate = '2018-01-01';
-let endDate = '2018-31-01';
-let currency = 'EUR'
-
- coindesk.get(`close.json`)
-  .then(response => {
-    console.log(response.data)
+$(document).ready(function () {
+  const coindesk = axios.create({
+    baseURL: 'http://api.coindesk.com/v1/bpi/historical'
   })
-  .catch(err => {
-    console.log(err)
+
+  let startDate;
+  let endDate;
+  const Dates = () => {
+    startDate = $("#start").val();
+    endDate = $("#end").val();
+  }
+  let currency = 'EUR';
+
+  let printTheChart = ((obj, keyDate, valuePrice) => {
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: keyDate,
+        datasets: [{
+          label: "Stock Chart",
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: valuePrice,
+        }]
+      }
+    });
   })
+
+ const print = () => {
+  Dates();
+  getCoin();
+ }
+
+  let getCoin = () => coindesk.get(`close.json?start=${startDate}&end=${endDate}`)
+    .then(response => {
+      const obj = response.data.bpi;
+      const keyDate = Object.keys(obj);
+      const valuePrice = Object.values(obj);
+
+      printTheChart(obj, keyDate, valuePrice);
+
+
+    }).catch(err => {
+      console.log(err)
+    })
+
+  print();
+  
+  document.getElementById('buton').addEventListener('click', () => {
+      print();
+  }, false);
+});
