88a6e86d1c6166cafd9666291725896067812d4b
RaulG <guiaraul@gmail.com>
2018-09-19 16:30:29 +0200

    Iteration4

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,9 +1,41 @@
 
 
+let elem1 = document.getElementById("start");
+let elem2 = document.getElementById("end");
+let elemCurr = document.getElementById("currency");
+//DEFAULT DATES
+let startdate = '2018-08-01';
+let enddate = '2018-08-31';
+let curr = 'USD';
+elem1.addEventListener('input', (evt) => {
+    startdate = evt.target.value;
+    getFinancial().then(prices => 
+        printTheChart(prices)
+        );
+
+});
+
+elem2.addEventListener('input', (evt) => {
+    enddate= evt.target.value;
+    getFinancial().then(prices => 
+        printTheChart(prices)
+        );
+});
+
+elemCurr.addEventListener('change', (evt) => {
+    curr = evt.target.value;
+    getFinancial().then(prices => 
+        printTheChart(prices)
+        );
+});
+
+
 const getFinancial = () => {
+    console.log("CURR EN FUNCION:", curr)
  return axios
-  .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+  .get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startdate}&end=${enddate}&currency=${curr}`)
   .then(res =>  {  
+      console.log(res);
     return {
         dates: Object.keys(res.data.bpi),
         prices: Object.values(res.data.bpi)
