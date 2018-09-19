eabfd16af7b117b626d4bab3fdb8462c45ff7a2e
Ilham Moutaharik <ilham@prescrypto.com>
2018-09-19 15:40:33 -0500

    Lo que pudimos avanzar

--- starter-code/app.js
+++ starter-code/app.js
@@ -4,6 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+const hbs = require('hbs')
 
 var index = require('./routes/index');
 var users = require('./routes/users');
@@ -12,13 +13,13 @@ var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('view engine', 'hbs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
