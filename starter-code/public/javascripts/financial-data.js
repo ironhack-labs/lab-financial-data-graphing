b7321e212ea71bce5c655a2cb60d847cfb9abdf5
Celia Arnelas <carnelas@gmail.com>
2018-09-19 16:18:45 +0200

    tercera iteracion casi hecha

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,44 +1,66 @@
-
 axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-  .then( res => {console.log(res);
+  .then(res => {
+    console.log(res);
     let labels = Object.keys(res.data.bpi);
     let datas = Object.values(res.data.bpi);
     console.log(datas);
     let data = res.data.bpi;
     let arr = [];
-    for (const el in data){
+    for (const el in data) {
       arr.push({
         x: el,
         y: data[el]
       })
     }
     console.log(arr)
-    
+
     let ctx = document.getElementById("chart").getContext('2d');
     let chart = new Chart(ctx, {
       type: 'line',
-      data: {labels, 
-            datasets:[{data:datas}]},
-      options:   {legend: {
-        display: true,
-        position: 'top',
-        labels: {
-          boxWidth: 80,
-          fontColor: 'black'
-        }
-      }}
-     /*  data: arr,
+      data: {
+        labels,
+        datasets: [{
+          data: datas
+        }]
+      },
       options: {
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero:true,
-                    max: 1000000
-                }
-            }]
+        legend: {
+          display: true,
+          position: 'top',
+          labels: {
+            boxWidth: 80,
+            fontColor: 'black'
+          }
         }
-    } */
+      }
     })
-    console.log(chart)
-  } ) 
 
+    let dateFrom = document.getElementById('from').addEventListener('input', function (evt) {
+      let from = evt.target.value;
+      let to = document.getElementById('to').value;
+      getNewData(chart, from, to);
+    });
+
+    let dateTo = document.getElementById('to').addEventListener('input', function (evt) {
+      let to = evt.target.value;
+      let from = document.getElementById('from').value;
+      getNewData(chart, from, to);
+    });
+  })
+
+let getNewData = (chart, start, end) => {
+  console.log("console log aqui", chart.data)
+  axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+    .then(res => {
+      let labels = Object.keys(res.data.bpi);
+      let datas = Object.values(res.data.bpi);
+      updateDates(chart, labels, datas)
+    })
+}
+
+const updateDates = (chart, labels, data) => {
+  chart.data.labels = labels;
+  chart.data.datasets[0] = {data};
+  chart.update();
+  console.log(chart)
+}
