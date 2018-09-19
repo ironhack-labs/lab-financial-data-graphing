94efa6df4c97ef3111a2aaae4f928f7a0ab20e57
LauraMiguelanez <laura.miguelanez@gmail.com>
2018-09-19 15:34:47 +0200

    Done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -2,17 +2,19 @@ let data = [];
 let dates = [];
 let url = "https://api.coindesk.com/v1/bpi/historical/close.json";
 let bounds = "";
+let changeCurr = "";
 let dateF;
 let dateT;
 
 const drawChart = () => {
   axios
-    .get(url + bounds)
+    .get(url + "?" + bounds + "&" + changeCurr)
     .then(res => {
       data = Object.values(res.data.bpi);
       dates = Object.keys(res.data.bpi);
     })
     .then(() => {
+      getMaxMin();
       const ctx = document.getElementById("myChart").getContext("2d");
       const myChart = new Chart(ctx, {
         type: "line",
@@ -32,16 +34,28 @@ const drawChart = () => {
 
 $("#dateF").change(() => {
   dateF = $("#dateF").val();
-  bounds = `?start=${dateF}&end=${dateT}`;
+  bounds = `start=${dateF}&end=${dateT}`;
   drawChart();
 });
 $("#dateT").change(() => {
   dateT = $("#dateT").val();
-  bounds = `?start=${dateF}&end=${dateT}`;
+  bounds = `start=${dateF}&end=${dateT}`;
+  drawChart();
+});
+$("#curr").change(() => {
+  curr = $("#curr").val();
+  changeCurr = `currency=${curr}`;
   drawChart();
 });
 
+const getMaxMin = () => {
+  let min = Math.min(...data);
+  let max = Math.max(...data);
+  $("#min").text(min);
+  $("#max").text(max);
+  console.log(data);
+};
 
-$(document).ready(()=>{
-    drawChart();
+$(document).ready(() => {
+  drawChart();
 });
