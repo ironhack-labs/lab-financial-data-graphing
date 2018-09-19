384eab20907eb033354ef0791545c4cd5ea6cd2c
robotic-amoeba <raul.martin.majadas@gmail.com>
2018-09-19 18:02:54 +0200

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,33 +1,37 @@
-const getData = () => {
+const getData = (start, end) => {
   return axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+  //return axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
     .then(res => {
       return {
         data: res.data.bpi,
         time: res.data.time.updated
       }
     })
-    /* .then(res => {
-      return Object.keys(res.data), Object.values(res.data)
-    }) */
+    .then(res => {
+      return [Object.keys(res.data), Object.values(res.data)]
+    })
     .catch(e => console.log(e))
 }
 
 
 document.addEventListener('DOMContentLoaded', () => {
-  
-  getData()
+
+  const from = document.getElementById("from");
+  const to = document.getElementById("to");
+
+  getData(from, to)
   .then(function (res) {
     let ctx = document.getElementById("myChart").getContext('2d');
     console.log(ctx)
       let chart = new Chart(ctx, {
         type: 'line',
         data: {
-          labels: Object.keys(res.data),
+          labels: res[0],
           datasets: [{
             label: "BitCoins",
             backgroundColor: "rgb(3, 169, 244)",
             borderColor: "rgb(63, 81, 181)",
-            data: Object.values(res.data)
+            data: res[1]
           }]
         }
       })
