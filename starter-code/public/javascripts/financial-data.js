683d820c230c28e1e4ecc7e3e6ebea3c697dd22b
wergilia <weronika.zdrojewska@gmail.com>
2018-09-19 16:13:54 +0200

    Iteration 2 &3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -3,11 +3,31 @@
 const getData = () => {
     return axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
     .then( res => {
-    
-            console.log(res)
-        
-     }).catch(e => console.log(e));
-  }
-  
+        printTheChart(res.data.bpi)    
+    }).catch(e => console.log(e));
+}
 
-getData()
+let printTheChart = ((datos) => {
+    
+    let stockLabels = Object.keys(datos)
+    let stockPrice = Object.values(datos)
+    let ctx = document.getElementById('myChart').getContext('2d');
+    
+    let chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: stockLabels,
+        datasets: [{
+          label: "Bicoin Price Index",
+          backgroundColor: 'rgb(234, 242, 255)',
+          borderColor: 'rgb(165, 217, 255)',
+          data: stockPrice,
+        }]
+      }
+    });
+  });
+
+ getData()
+
+    
+  
