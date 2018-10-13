33f28d3d8c9671ce31af6b09f57c735206f50d10
Adolfo Vela <vela@tekplanner.com>
2018-10-13 13:44:17 -0500

    Segunda iteracion

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -2,7 +2,50 @@
 //const axios = require("axios")
 
 //Peticion axios
-axios.get("http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-08-01&end=2018-10-13")
+axios.get("http://api.coindesk.com/v1/bpi/historical/close.json?start=2018-08-01&end=2018-08-13")
 .then(res =>{
-  console.log(res.data.time.updated)
+  let labels = Object.keys(res.data.bpi)
+  let values = Object.values(res.data.bpi)
+  console.log(labels, values);
+  printChart(res.data, labels, values)
 })
+
+let printChart = ((coinData, labels, values) => {
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: labels,
+          datasets: [{
+              label: '# of Votes',
+              data: values,
+              backgroundColor: [
+                  'rgba(255, 99, 132, 0.2)',
+                  'rgba(54, 162, 235, 0.2)',
+                  'rgba(255, 206, 86, 0.2)',
+                  'rgba(75, 192, 192, 0.2)',
+                  'rgba(153, 102, 255, 0.2)',
+                  'rgba(255, 159, 64, 0.2)'
+              ],
+              borderColor: [
+                  'rgba(255,99,132,1)',
+                  'rgba(54, 162, 235, 1)',
+                  'rgba(255, 206, 86, 1)',
+                  'rgba(75, 192, 192, 1)',
+                  'rgba(153, 102, 255, 1)',
+                  'rgba(255, 159, 64, 1)'
+              ],
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero:true
+                  }
+              }]
+          }
+      }
+  });
+}) 
