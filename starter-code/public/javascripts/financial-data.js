5eef14c0793c5bec136d7656f5d6038653592c69
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-20 15:35:19 +0100

    Iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,8 +1,25 @@
 function AJAXRequest() {
     axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
         .then((data) => {
-            console.log(data);
+            console.log(data.data.bpi)
+            print(data.data.bpi)
         })
+}
+
+function print(data) {
+    var ctx = document.getElementById('myChart').getContext('2d');
+    var chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: Object.keys(data),
+            datasets: [{
+                label: "My First dataset",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: Object.values(data)
+            }]
+        }
+    });
 }
 
 AJAXRequest()
