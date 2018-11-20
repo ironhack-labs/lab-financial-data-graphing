1ba0226d61f2f6c7cd52ed3721d2e06f003d8c97
Steven M <steve5mv5@gmail.com>
2018-11-20 17:38:27 +0100

    iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,40 +1,54 @@
 window.onload = function() {
-  function ajaxrequest() {
-    axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-      .then(response => {
-        console.log(response.data.bpi);
-        printTheChart(response.data.bpi);
-      })
+  function ajaxrequest(ruta) {
+    axios.get(ruta).then(response => {
+      printTheChart(response.data.bpi);
+    });
   }
 
   function getDataFromApi() {
-    ajaxrequest();
+    ajaxrequest("http://api.coindesk.com/v1/bpi/historical/close.json");
   }
 
-  const printTheChart = (dataObject) => {
+  function changeDate(start, end) {
+    if (start.length > 0 && end.length > 0) {
+      let dateruta = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`;
+      ajaxrequest(dateruta);
+    }
+  }
+
+  const printTheChart = dataObject => {
     const stockLabels = Object.keys(dataObject);
     const stockPrice = Object.values(dataObject);
-    const ctx = document.getElementById('mycanvas').getContext('2d');
-
-    console.log(stockLabels,stockPrice);
+    const ctx = document.getElementById("mycanvas").getContext("2d");
 
     //here we give the chart the data it needs
     const chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: stockLabels,
-            datasets: [{
-                label: "Stock Chart",
-                backgroundColor: 'rgb(255, 99, 132)',
-                fill: true,
-                tension: 0,
-                pointHoverRadius: 20,
-                borderColor: 'rgb(255, 99, 132)',
-                data: stockPrice,
-            }]
-        }
+      type: "line",
+      data: {
+        labels: stockLabels,
+        datasets: [
+          {
+            label: "Stock Chart",
+            backgroundColor: "rgb(255, 99, 132)",
+            fill: true,
+            tension: 0,
+            pointHoverRadius: 20,
+            borderColor: "rgb(255, 99, 132)",
+            data: stockPrice
+          }
+        ]
+      }
     });
-};
+  };
 
   document.getElementById("getData").onclick = getDataFromApi;
+  var startDate = document.getElementById("startDate");
+  var endDate = document.getElementById("endDate");
+
+  startDate.addEventListener("change", () =>
+    changeDate(startDate.value, endDate.value)
+  );
+  endDate.addEventListener("change", () =>
+    changeDate(startDate.value, endDate.value)
+  );
 };
