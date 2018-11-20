3c418d4251d1d52c9ffcef275a7b4f72852b324a
N R <mnpuzzle@gmail.com>
2018-11-20 18:19:29 +0100

    Second Iteration

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,35 @@
+let bitcoinDate;
+let bitcoinPrices;
+
 axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
-.then((x) => {
-   console.log(x)
-   const data= x.data.bpi
+.then((res) => {
+   console.log(res)
+   const data= res.data.bpi
    console.log(data);
+   bitcoinDate = Object.keys(data)
+   bitcoinPrices = Object.values(data)
+   printTheChart(bitcoinDate, bitcoinPrices)
 })
+
+
+
+
+
+const printTheChart = (bitcoinDate, bitcoinPrices) => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: bitcoinDate,
+      datasets: [{
+        label: "Stock Chart",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: bitcoinPrices,
+      }]
+    }
+  });
+}
+
+
+
