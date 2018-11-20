bac2b82a1caf52a066e28a3164f042f1d998c700
David <davidmunozvi@icloud.com>
2018-11-20 16:46:33 +0100

    Iteration 2 Create Chart

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -2,13 +2,21 @@
 
 document.getElementById('getCountryData').onclick = countryDataClickHandler;
 
+
+function renderHTMLIronhack(bitcoinData) {
+    document.querySelector('.country-name').innerText = bitcoinData.name
+    document.querySelector('.country-phone-prefix').innerText = bitcoinData.prefix
+}
+
+
+
 function makeCountryAJAXRequest() {
     // let chosenCurrency = document.querySelector('#').value
     
     axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-    .then((x) => {
-        console.log(x) 
-        // renderHTMLIronhack(new CountryData(countryData.data[0].name, countryData.data[0].callingCodes[0]))
+    .then((bitcoinData) => {
+        
+        printTheChart(bitcoinData.data.bpi);
     })
     }
 
@@ -16,5 +24,56 @@ function makeCountryAJAXRequest() {
     function countryDataClickHandler() {
         makeCountryAJAXRequest()
     }
+
+
+
+
+
+
+    
+
+    // function drawCompanyResultsChart(stockTicket) {
+        
+        //  const stockInfo = axios.create({
+        //      baseURL: `http://api.coindesk.com/v1/bpi/historical/close.json` ,
+        //  });
+        // //we are crafting this URL : https://api.iextrading.com/1.0/stock/aapl/chart
+        // stockInfo.get(`${stockTicket}/chart`)
+        //     .then(response => {
+                // printTheChart(stockInfo.data.bpi);
+            // })
+            // .catch(error => {
+            //     console.log(error);
+            // });
+        //chart rendering function
+        const printTheChart = (stockData => {
+
+
+            const stockLabels = Object.keys(stockData);
+
+            const stockPrice = Object.values(stockData);
+
+
+            const ctx = document.getElementById('myChart').getContext('2d');
+
+            //here we give the chart the data it needs
+
+            const chart = new Chart(ctx, {
+                type: 'line',
+                data: {
+                    labels: stockLabels,
+                    datasets: [{
+                        label: "Stock Chart",
+                        backgroundColor: 'rgb(21, 170, 151)',
+                        fill: true,
+                        tension: 0,
+                        pointHoverRadius: 20,
+                        borderColor: 'rgb(0, 0, 0)',
+                        data: stockPrice,
+                    }]
+                }
+            });
+        });
+    // }
 
     
