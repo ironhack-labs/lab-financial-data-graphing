48c4e07cb92f85dbe8dfbf760ba5d21ae19bdf82
Zeswen <zeswentyrael@gmail.com>
2018-11-20 14:01:10 +0100

    Iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -3,10 +3,28 @@ const bitcoinChart = axios.create({
   baseURL: 'https://api.coindesk.com/v1/bpi/historical/close.json',
 });
 
-function getBitcoinChat() {
+function getBitcoinChart() {
   bitcoinChart.get()
-    .then(res => console.log(res.data.bpi))
+    .then(res => printTheChart(res.data.bpi))
     .catch(err => next(err));
 }
 
-getBitcoinChat();
+const printTheChart = ((stockData) => {
+  const stockLabels = Object.keys(stockData);
+  const stockPrice = Object.values(stockData);
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: stockLabels,
+      datasets: [{
+        label: 'Stock Chart',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: stockPrice,
+      }],
+    },
+  });
+});
+
+getBitcoinChart();
