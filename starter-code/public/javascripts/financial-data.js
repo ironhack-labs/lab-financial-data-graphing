aaf7e8ffc24e015aa434858d3318243c716eedb8
Christian Romero <christian@MacBook-Air-de-Christian.local>
2018-11-20 18:01:27 +0100

    DONE

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,25 +1,33 @@
-function AJAXRequest() {
-    axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+function AJAXRequest(fechaini, fechafin) {
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${fechaini}&end=${fechafin}`)
         .then((data) => {
-            console.log(data.data.bpi)
             print(data.data.bpi)
         })
+        .catch(err => console.log(err))
 }
 
 function print(data) {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
+    let keys = Object.keys(data)
+    let values = Object.values(data)
+    let ctx = document.getElementById('myChart').getContext('2d');
+    let chart = new Chart(ctx, {
         type: 'line',
         data: {
-            labels: Object.keys(data),
+            labels: keys,
             datasets: [{
                 label: "My First dataset",
                 backgroundColor: 'rgb(255, 99, 132)',
                 borderColor: 'rgb(255, 99, 132)',
-                data: Object.values(data)
+                data: values
             }]
         }
     });
 }
 
-AJAXRequest()
+function printBetweenTimes(){
+    const fechaini = document.getElementById('fechaini').value
+    const fechafin = document.getElementById('fechafin').value
+    AJAXRequest(fechaini, fechafin)
+}
+
+document.getElementById('getdata').onclick = printBetweenTimes
