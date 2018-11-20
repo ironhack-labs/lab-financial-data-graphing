0cd17a779f324410f0bd58cf56cd982ee881bd16
jrecio <recio.juan@gmail.com>
2018-11-20 15:32:03 +0100

    Iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,10 +1,42 @@
+
+
 function makeCurrencyAJAXRequest() {
   axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-      .then((currencyData) => {
-          //renders the data obtained and passes it to the view renderer
-          //we are instantiating a CurrencyData object, so we can have the information in one place
-          console.log(currencyData)
-      })
-      .catch((err)=> {console.log(err)});
+    .then((currencyData) => {
+      //we are instantiating a CurrencyData object, so we can have the information in one place
+      printTheChart(currencyData.data.bpi)
+    })
+    .catch((err) => { console.log(err) });
 }
- makeCurrencyAJAXRequest();
+
+
+const printTheChart = (stockData => {
+  const days = Object.keys(stockData);
+  const values = Object.values(stockData);
+  console.log(days);
+  console.log(values);
+  const ctx = document.getElementById("myChart").getContext('2d');
+  //here we give the chart the data it needs
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: days,
+      datasets: [{
+        label: "Values Chart",
+        backgroundColor: 'rgb(255, 99, 132)',
+        fill: false,
+        tension: 0,
+        pointHoverRadius: 20,
+        borderColor: 'rgb(255, 99, 132)',
+        data: values,
+      }]
+    }
+  });
+});
+
+
+
+
+
+makeCurrencyAJAXRequest();
+// printTheChart(currencyData.data.bpi);
