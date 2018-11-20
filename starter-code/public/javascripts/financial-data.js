4f848340d3151eece5603eb3696c7947291e6652
danielsuidan <danielsuidan@gmail.com>
2018-11-20 16:41:32 +0100

    iteration 2 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,9 +1,35 @@
-//  window.onload =  function() {
-    function ajaxRecuest() {
-        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
-        .then((coinData)=> {
-            console.log(coinData.data.bpi);
-        })
-    }
-    ajaxRecuest();
-// }
+ window.onload = function () {
+     function ajaxRecuest() {
+         axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+             .then((coinData) => {
+                 // console.log(coinData.data.bpi);
+                 printTheChart(coinData.data.bpi)
+             })
+     }
+     ajaxRecuest();
+
+
+     const printTheChart = (jsonData => {
+         const stockLabels = Object.keys(jsonData);
+         const stockCoins = Object.values(jsonData);
+         const ctx = document.getElementById('table').getContext('2d');
+         //here we give the chart the data it needs
+         const chart = new Chart(ctx, {
+             type: 'line',
+             data: {
+                 labels: stockLabels,
+                 datasets: [{
+                     label: "Stock Chart",
+                     backgroundColor: 'rgb(255, 99, 132)',
+                     fill: false,
+                     tension: 0,
+                     pointHoverRadius: 20,
+                     borderColor: 'rgb(255, 99, 132)',
+                     tension: 0,
+                     data: stockCoins,
+                 }]
+             }
+         });
+     });
+
+ }
