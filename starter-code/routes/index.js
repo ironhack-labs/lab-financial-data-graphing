f3a4ebc94090f5d0796bd3097141a15efbe9fbc4
Bianca Castro <biiianca_00@hotmail.com>
2018-11-20 13:47:17 +0100

    iteration 1

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
+const axios= require('axios');
+
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
+
+  axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
+  .then((x) => {
+     console.log(x)
+  })
   res.render('index', { title: 'Express' });
 });
+
+
 
 module.exports = router;
