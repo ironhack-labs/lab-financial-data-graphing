af347f908bef4035ac60203434256f9b894c89b2
Zeswen <zeswentyrael@gmail.com>
2018-11-20 15:28:48 +0100

    Iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,30 +1,41 @@
-// Ask if axios is already defined
-const bitcoinChart = axios.create({
-  baseURL: 'https://api.coindesk.com/v1/bpi/historical/close.json',
-});
+
+let startDate = '2018-10-01';
+let endDate = '2018-11-01';
+
 
 function getBitcoinChart() {
+  const bitcoinChart = axios.create({
+    baseURL: `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`,
+  });
   bitcoinChart.get()
     .then(res => printTheChart(res.data.bpi))
     .catch(err => next(err));
+
+  const printTheChart = ((stockData) => {
+    const stockLabels = Object.keys(stockData);
+    const stockPrice = Object.values(stockData);
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: stockLabels,
+        datasets: [{
+          label: 'Stock Chart',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
+          data: stockPrice,
+        }],
+      },
+    });
+  });
 }
 
-const printTheChart = ((stockData) => {
-  const stockLabels = Object.keys(stockData);
-  const stockPrice = Object.values(stockData);
-  const ctx = document.getElementById('myChart').getContext('2d');
-  const chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: stockLabels,
-      datasets: [{
-        label: 'Stock Chart',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: stockPrice,
-      }],
-    },
-  });
-});
+function clickHandler() {
+  startDate = document.querySelector('#start').value;
+  endDate = document.querySelector('#end').value;
+  getBitcoinChart();
+}
 
-getBitcoinChart();
+document.querySelector('#submit').onclick = clickHandler;
+
+window.onload = () => getBitcoinChart();
