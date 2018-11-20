e9458504876215b9a71b681635b0f98b20a354c1
Bianca Castro <biiianca_00@hotmail.com>
2018-11-20 18:48:27 +0100

    hasta la iteracion 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,36 +1,43 @@
+// let fromListener = "2018-10-01";
+// let toListener = "2018-10-08";
+// let myUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromListener}&end=${toListener}`;
 
-function drawCompanyResultsChart(fecha, valor){
+document.getElementById("date-from-input").onchange = function() {
+  fromListener = document.getElementById("date-from-input").value;
+  
+};
 
+document.getElementById("date-to-input").onchange = function() {
+  toListener = document.getElementById("date-to-input").value;
+  
+};
 
-    var ctx = document.getElementById("myChart").getContext('2d');
-    var myChart = new Chart(ctx, {
-        type: 'line',
-        data: {
-          labels: fecha,
-          datasets: [{
-              label: "My First dataset",
-              backgroundColor: 'rgb(255, 99, 132)',
-              fill:false,
-              borderColor: 'rgb(255, 99, 132)',
-              data: valor,
-              
-          }]
+function drawCompanyResultsChart(fecha, valor) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: fecha,
+      datasets: [
+        {
+          label: "My First dataset",
+          backgroundColor: "rgb(255, 99, 132)",
+          fill: false,
+          borderColor: "rgb(255, 99, 132)",
+          data: valor
         }
-    });
-    
+      ]
+    }
+  });
 }
 
-axios
-  .get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
-  .then(bitcoinData => {
-    const data = bitcoinData.data.bpi;
-    //  const bitcoinValueArray= Object.keys(data).map(i=> data[i])
-    const bitcoinArray = Object.entries(data);
-    const bitcoinDate = bitcoinArray.map(elem => elem[0]);
-    const bitcoinPrices = bitcoinArray.map(elem => elem[1]);
+axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`).then(bitcoinData => {
+  const data = bitcoinData.data.bpi;
+  //  const bitcoinValueArray= Object.keys(data).map(i=> data[i])
+  const bitcoinArray = Object.entries(data);
+  const bitcoinDate = bitcoinArray.map(elem => elem[0]);
+  const bitcoinPrices = bitcoinArray.map(elem => elem[1]);
 
-    console.log(bitcoinDate);
-    drawCompanyResultsChart(bitcoinDate,bitcoinPrices);
-  });
-
-
+  console.log(bitcoinDate);
+  drawCompanyResultsChart(bitcoinDate, bitcoinPrices);
+});
