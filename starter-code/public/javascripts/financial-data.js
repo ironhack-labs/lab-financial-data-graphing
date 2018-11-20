8b8ada630b523fb41266b2bcf915b1c757a7c4d1
Bianca Castro <biiianca_00@hotmail.com>
2018-11-20 17:56:15 +0100

    iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,36 @@
 
+function drawCompanyResultsChart(fecha, valor){
 
-axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
-  .then((x) => {
-     console.log(x)
-  })
+
+    var ctx = document.getElementById("myChart").getContext('2d');
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: fecha,
+          datasets: [{
+              label: "My First dataset",
+              backgroundColor: 'rgb(255, 99, 132)',
+              fill:false,
+              borderColor: 'rgb(255, 99, 132)',
+              data: valor,
+              
+          }]
+        }
+    });
+    
+}
+
+axios
+  .get(`https://api.coindesk.com/v1/bpi/historical/close.json`)
+  .then(bitcoinData => {
+    const data = bitcoinData.data.bpi;
+    //  const bitcoinValueArray= Object.keys(data).map(i=> data[i])
+    const bitcoinArray = Object.entries(data);
+    const bitcoinDate = bitcoinArray.map(elem => elem[0]);
+    const bitcoinPrices = bitcoinArray.map(elem => elem[1]);
+
+    console.log(bitcoinDate);
+    drawCompanyResultsChart(bitcoinDate,bitcoinPrices);
+  });
+
+
