b14c3d922fe2834bb0545c565d1686d502cf9e36
Patricia LG <patri.lopg5@gmail.com>
2018-11-20 17:50:19 +0100

    añadido data

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,33 @@
+window.onload = function() {
+  document.getElementById(
+    "myButton"
+  ).onclick = function makeCountryAJAXRequest() {
+    axios
+      .get("https://api.coindesk.com/v1/bpi/historical/close.json")
+      .then(response => {
+        updateChart(response);
+      });
+  };
 
+  function updateChart(response) {
+    const bpiDate = Object.keys(response.data.bpi);
+    const bpiValue = Object.values(response.data.bpi);
 
-axios.get("https://api.coindesk.com/v1/bpi/historical/close.json")
-  .then((data) => {
-    console.log(data)
-  });
+    var ctx = document.getElementById("myChart").getContext("2d");
+    var myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: bpiDate,
+        datasets: [
+          {
+            label: "# of Votes",
+            data: bpiValue,
+            backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+            borderColor: ["rgba(255,99,132,1)"],
+            borderWidth: 1
+          }
+        ]
+      }
+    });
+  }
+};
