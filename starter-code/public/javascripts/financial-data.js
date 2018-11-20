b94262f35f7a416946bf9b4e6a6fc5baad5238ea
danielsuidan <danielsuidan@gmail.com>
2018-11-20 18:17:21 +0100

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,16 @@
  window.onload = function () {
-     function ajaxRecuest() {
-         axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+     function ajaxRecuest(myDate) {
+         let rute = 'https://api.coindesk.com/v1/bpi/historical/close.json';
+         if (myDate) {
+             if (myDate.type === 'start') {
+                 rute = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${myDate.date}&end=${myDate.otherDate}`
+             } else {
+                 rute = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${myDate.otherDate}&end=${myDate.date}`
+             }
+
+         }
+
+         axios.get(rute)
              .then((coinData) => {
                  // console.log(coinData.data.bpi);
                  printTheChart(coinData.data.bpi)
@@ -31,5 +41,27 @@
              }
          });
      });
+     let date1 = document.getElementById('date1').value;
+     let date2 = document.getElementById('date2').value;
 
+     document.getElementById('date1').onchange = function () {
+
+         let dateStart = {
+             type: 'start',
+             date: date1,
+             otherDate: date2
+         }
+         ajaxRecuest(dateStart);
+     }
+
+     document.getElementById('date2').onchange = function () {
+
+         let dateEnd = {
+             type: 'end',
+             date: date2,
+             otherDate: date1
+         }
+         ajaxRecuest(dateEnd);
+     }
+     //  document.getElementById('date2').onchange = countrySelectorChangeHandler
  }
