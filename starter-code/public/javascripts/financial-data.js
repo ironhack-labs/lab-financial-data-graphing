c88a46310b7d72cc6693469cd5b29d26572b48f1
Teresa Romero <tromerolozano@gmail.com>
2018-11-24 13:13:24 +0100

    iteration 2

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,39 @@
+const bitApi = axios.create({
+  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+});
+
+
+function getBitInfo() {
+  bitApi
+    .get()
+    .then(response => {
+      
+      printTheChart(response.data.bpi);
+      
+    })
+    .catch(err => {
+      console.error(err);
+    });
+}
+
+let printTheChart = ((data) => {
+  let dataLabels = Object.keys(data);
+  let dataPrice = Object.values(data);
+  let ctx = document.getElementById('myChart').getContext('2d');
+  let chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dataLabels,
+      datasets: [{
+        label: "Stock Chart",
+        backgroundColor: 'rgb(205, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: dataPrice,
+      }]
+    }
+  });
+});
+
+document.getElementById("bitButton").onclick = function() {
+  getBitInfo();
+};
