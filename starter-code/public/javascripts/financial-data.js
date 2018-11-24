e95ffca979972e2a93bdfbcff65efa9f3af01f03
Teresa Romero <tromerolozano@gmail.com>
2018-11-24 18:37:27 +0100

    all done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,4 +1,4 @@
-const getBitInfo = (dFrom, dTo,curr) => {
+const getBitInfo = (dFrom, dTo, curr) => {
   return axios
     .get(
       `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dFrom}&end=${dTo}&currency=${curr}`
@@ -6,11 +6,24 @@ const getBitInfo = (dFrom, dTo,curr) => {
     .then(res => {
       let dataLabels = Object.keys(res.data.bpi);
       let dataPrice = Object.values(res.data.bpi);
-      return { dataLabels, dataPrice ,curr};
+      return { dataLabels, dataPrice, curr };
     });
 };
 
-const printTheChart = ({ dataLabels, dataPrice,curr }) => {
+const printValues = ({ dataLabels, dataPrice, curr }) => {
+
+  let min=Math.min.apply(null,dataPrice).toFixed(2);
+  let max=Math.max.apply(null,dataPrice).toFixed(2);
+
+  document.getElementById("minVal").innerHTML=`${min}`;
+  document.getElementById("minCurr").innerHTML=` ${curr}`;
+
+  document.getElementById("maxVal").innerHTML=`${max}`;
+  document.getElementById("maxCurr").innerHTML=` ${curr}`;
+  
+};
+
+const printTheChart = ({ dataLabels, dataPrice, curr }) => {
   let ctx = document.getElementById("myChart").getContext("2d");
   let chart = new Chart(ctx, {
     type: "line",
@@ -25,21 +38,25 @@ const printTheChart = ({ dataLabels, dataPrice,curr }) => {
         }
       ]
     },
-    options : {
+    options: {
       scales: {
-        yAxes: [{
-          scaleLabel: {
-            display: true,
-            labelString: `${curr}`
+        yAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: `${curr}`
+            }
           }
-        }],
-      xAxes: [{
-        scaleLabel: {
-          display: true,
-          labelString: 'Dates'
-        }
-      }]
-    }     
+        ],
+        xAxes: [
+          {
+            scaleLabel: {
+              display: true,
+              labelString: "Dates"
+            }
+          }
+        ]
+      }
     }
   });
 };
@@ -47,14 +64,16 @@ const printTheChart = ({ dataLabels, dataPrice,curr }) => {
 document.getElementById("bitButton").onclick = function() {
   let dFrom = document.getElementById("dateFrom").value;
   let dTo = document.getElementById("dateTo").value;
-  let curr=document.getElementById("currSelec").value;
+  let curr = document.getElementById("currSelec").value;
   if (dFrom == "" || dTo == "") {
-    let error=document.createElement('p');
-    let divError=document.getElementById("errorMessage")
-    error.innerHTML="Please enter a valid date";
+    let error = document.createElement("p");
+    let divError = document.getElementById("errorMessage");
+    error.innerHTML = "Please enter a valid date";
     divError.appendChild(error);
   } else {
-    getBitInfo(dFrom, dTo,curr).then(data => {
+    getBitInfo(dFrom, dTo, curr).then(data => {
       printTheChart(data);
-  })
-}};
+      printValues(data);
+    });
+  }
+};
