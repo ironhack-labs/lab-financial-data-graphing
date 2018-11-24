9a77264310bdee7ae74f1a3696a1d3e0869202ac
Teresa Romero <tromerolozano@gmail.com>
2018-11-24 17:10:02 +0100

    iteration 3 -done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,39 +1,43 @@
-const bitApi = axios.create({
-  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
-});
-
-
-function getBitInfo() {
-  bitApi
-    .get()
-    .then(response => {
-      
-      printTheChart(response.data.bpi);
-      
-    })
-    .catch(err => {
-      console.error(err);
+const getBitInfo = (dFrom, dTo) => {
+  return axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dFrom}&end=${dTo}`
+    )
+    .then(res => {
+      let dataLabels = Object.keys(res.data.bpi);
+      let dataPrice = Object.values(res.data.bpi);
+      return { dataLabels, dataPrice };
     });
-}
+};
 
-let printTheChart = ((data) => {
-  let dataLabels = Object.keys(data);
-  let dataPrice = Object.values(data);
-  let ctx = document.getElementById('myChart').getContext('2d');
+const printTheChart = ({ dataLabels, dataPrice }) => {
+  let ctx = document.getElementById("myChart").getContext("2d");
   let chart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: dataLabels,
-      datasets: [{
-        label: "Stock Chart",
-        backgroundColor: '',
-        borderColor: 'rgb(255, 99, 132)',
-        data: dataPrice,
-      }]
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "",
+          borderColor: "rgb(255, 99, 132)",
+          data: dataPrice
+        }
+      ]
     }
   });
-});
+};
 
 document.getElementById("bitButton").onclick = function() {
-  getBitInfo();
-};
+  let dFrom = document.getElementById("dateFrom").value;
+  let dTo = document.getElementById("dateTo").value;
+  if (dFrom == "" || dTo == "") {
+    let error=document.createElement('p');
+    let divError=document.getElementById("errorMessage")
+    error.innerHTML="Please enter a valid date";
+    divError.appendChild(error);
+  } else {
+    getBitInfo(dFrom, dTo).then(data => {
+      printTheChart(data);
+  })
+}};
