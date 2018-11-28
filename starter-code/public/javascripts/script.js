4117ac9976ba98c993232baefaeff58b41c32d4d
AsterDecember <aster.december@gmail.com>
2018-11-27 18:01:45 -0600

    working till 3 Iteration

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,13 +3,12 @@ console.log('ei')
 document.addEventListener(
     "DOMContentLoaded",
     ()=>{
-        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
+        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?currency=GBP')
             .then(function (response) {
-                console.log(response.data.bpi);
+                console.log(response.data);
                 const keys = Object.keys(response.data.bpi)
                 const values = Object.values(response.data.bpi)
-                console.log(Object.keys(response.data.bpi))
-                console.log(Object.values(response.data.bpi))
+
                 var ctx = document.getElementById("myChart").getContext('2d');
                 var myChart = new Chart(ctx, {
                     type: 'line',
@@ -35,9 +34,63 @@ document.addEventListener(
             .catch(function (error) {
                 console.log(error);
             });
+
+
+        var startDate
+        var endDate
+        var inputStart = document.getElementById('start')
+        var inputEnd = document.getElementById('end')
+        console.log(inputStart)
+        inputStart.onchange= () => {
+            console.log("Hola perro")
+            console.log(inputStart.value)
+            startDate = inputStart.value
+            inputEnd.onchange = () => {
+                endDate = inputEnd.value
+                axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+                    .then(function (response) {
+                        console.log(response.data);
+                        const keys = Object.keys(response.data.bpi)
+                        const values = Object.values(response.data.bpi)
+
+                        var ctx = document.getElementById("myChart").getContext('2d');
+                        var myChart = new Chart(ctx, {
+                            type: 'line',
+                            data: {
+                                labels: keys,
+                                datasets: [
+                                    {
+                                        data: values,
+                                        label: "Bitcoin values",
+                                        borderColor: "#c45850",
+                                        fill: false
+                                    }
+                                ]
+                            },
+                            options: {
+                                title: {
+                                    display: true,
+                                    text: 'Bitcoin live values'
+                                }
+                            }
+                        });
+                    })
+                    .catch(function (error) {
+                        console.log(error);
+                    });
+
+            }
+
+        }
+        /*inputStart.addEventListener('onchange',function(){
+            console.log('cambio')
+
+        })*/
     },
     false
 );
 
 
 
+document.getElementById('start')
+document.getElementById('end')
