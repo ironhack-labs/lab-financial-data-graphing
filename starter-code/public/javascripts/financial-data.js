f5ab6d54f5a5c628e5c3bc1baa2802639e8b6a8f
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2018-11-27 20:07:49 -0600

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -44,6 +44,51 @@ document.addEventListener("DOMContentLoaded",
 
        
     }) .catch(e=>console.log(e))
-},
+    document.getElementById("form").onchange = ()=>{
+
+        var from = document.getElementById("from").value
+        var to = document.getElementById("to").value
+   
+        axios.get("http://api.coindesk.com/v1/bpi/historical/close.json?start="+ from + "&end="+ to)
+        .then(info =>{ 
+    
+          const labels = Object.keys(info.data.bpi) 
+          const data = Object.values(info.data.bpi) 
+          
+          var ctx = document.getElementById("myChart").getContext('2d');
+          var myChart = new Chart(ctx, {
+              type: 'line',
+              data: {
+                  labels: labels,
+                  datasets: [{
+                      data: data ,
+                      label:"bitcoin",
+                      backgroundColor: [
+                          'rgba(255, 99, 132, 0.2)',
+                          'rgba(54, 162, 235, 0.2)',
+                          'rgba(255, 206, 86, 0.2)',
+                          'rgba(75, 192, 192, 0.2)',
+                          'rgba(153, 102, 255, 0.2)',
+                          'rgba(255, 159, 64, 0.2)'
+                      ],
+                      borderColor: [
+                          'rgba(255,99,132,1)',
+                          'rgba(54, 162, 235, 1)',
+                          'rgba(255, 206, 86, 1)',
+                          'rgba(75, 192, 192, 1)',
+                          'rgba(153, 102, 255, 1)',
+                          'rgba(255, 159, 64, 1)'
+                      ],
+                      borderWidth: 1
+                  }]
+              },
+          });
+    
+    
+        })
+        .catch(e=> console.log(e))
+   
+       } 
+    },
 false);
 
