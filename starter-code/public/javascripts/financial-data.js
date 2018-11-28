b197b7f97c6c34248ec196d5552e7ad9aa16e772
Fernando Hernandez <fernandohernandez@MacBook-Pro-de-Fernando.local>
2018-11-27 18:20:54 -0600

    firstone

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded",
+() =>{
+    axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+    .then (data =>{
+        console.log(data.data.time.updated)
+    })
+},
+false)
