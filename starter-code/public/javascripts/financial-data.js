f2fc0eef985d8ee9b06a940d44436497cdadcff5
Carlos Rivera <carlos.rivera.cabrera@gmail.com>
2018-11-27 20:05:05 -0600

    Iteration 2 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,15 +1,38 @@
 document.addEventListener('DOMContentLoaded', () => {
-  setTimeout(apiRequest,5000)
-  //Javascript se comunica con el 
-  function apiRequest(){
-    axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-    .then(result=>{
-      console.log(result)
-    })
-    .catch(e=>{
-      console.log(e)
-    })
-  }
+  // setTimeout(apiRequest,3000)
   console.log('Financial Data Graphing!');
 
 }, false);
+
+//form
+document.querySelector("#graph").addEventListener("submit", apiRequest);
+
+//Javascript se comunica con el 
+function apiRequest(e){
+  e.preventDefault();
+  axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+  .then(result=>{
+    let labels = Object.keys(result.data.bpi)
+    let data = Object.values(result.data.bpi)
+    graph(labels, data)
+  })
+  .catch(e=>{
+    console.log(e)
+  })
+}
+
+
+function graph(labels,data){
+  var ctx = document.getElementById("myChart").getContext('2d');
+  var myLineChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels:labels,
+    datasets:[{
+      label: 'Bitcoin Index Price',
+      data:data
+    }]
+  },
+  options: {}
+});
+}
