b3c74455b94e30b837879e4bfca43c7682c6d3d5
AsterDecember <aster.december@gmail.com>
2018-11-27 18:18:05 -0600

    bug fixed

--- starter-code/public/javascripts/script.js
+++ starter-code/public/javascripts/script.js
@@ -3,7 +3,7 @@ console.log('ei')
 document.addEventListener(
     "DOMContentLoaded",
     ()=>{
-        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?currency=GBP')
+        axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
             .then(function (response) {
                 console.log(response.data);
                 const keys = Object.keys(response.data.bpi)
@@ -48,37 +48,6 @@ document.addEventListener(
             inputEnd.onchange = () => {
                 endDate = inputEnd.value
                 axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
-                    .then(function (response) {
-                        console.log(response.data);
-                        const keys = Object.keys(response.data.bpi)
-                        const values = Object.values(response.data.bpi)
-
-                        var ctx = document.getElementById("myChart").getContext('2d');
-                        var myChart = new Chart(ctx, {
-                            type: 'line',
-                            data: {
-                                labels: keys,
-                                datasets: [
-                                    {
-                                        data: values,
-                                        label: "Bitcoin values",
-                                        borderColor: "#c45850",
-                                        fill: false
-                                    }
-                                ]
-                            },
-                            options: {
-                                title: {
-                                    display: true,
-                                    text: 'Bitcoin live values'
-                                }
-                            }
-                        });
-                    })
-                    .catch(function (error) {
-                        console.log(error);
-                    });
-
             }
 
         }
