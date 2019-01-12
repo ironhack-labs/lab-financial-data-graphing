19526584ece3905e3dabd2c009be19080492ad15
Arthur Houdebine <arthur.houdebine@gmail.com>
2019-01-12 16:31:58 +0100

    maximum value gets printed but min does not work

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,54 +1,62 @@
-const bitInfo  = axios.create({
-    baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json',
-});     
+const bitInfo = axios.create({
+  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+});
 
 const startDate = document.getElementById("startDate");
-const endDate = document.getElementById("endDate");  
-const currency = document.getElementById("currency-select");  
+const endDate = document.getElementById("endDate");
+const currency = document.getElementById("currency-select");
+const maxPriceElement = document.getElementById("maxPrice");
+const minPriceElement = document.getElementById("minPrice");
 
-const printGraph = () =>{
-bitInfo.get(``,{
-    params : {
-        start : startDate.value,
-        end : endDate.value,
-        currency : currency.value
-    }
-})
+
+const printGraph = () => {
+  bitInfo
+    .get(``, {
+      params: {
+        start: startDate.value,
+        end: endDate.value,
+        currency: currency.value
+      }
+    })
     .then(response => {
-        const dateLabels = Object.keys(response.data.bpi);
-        const bitPrice = Object.values(response.data.bpi);
-        const ctx = document.getElementById('myChart').getContext('2d');
-        const chart = new Chart(ctx, {
-        type: 'line',
+      const dateLabels = Object.keys(response.data.bpi);
+      const bitPrice = Object.values(response.data.bpi);
+      const maxPrice = Math.max(parseFloat(Object.values(response.data.bpi)));
+      const minPrice = Math.min(parseFloat(Object.values(response.data.bpi)));
+      const ctx = document.getElementById("myChart").getContext("2d");
+      console.log(Math.max(parseFloat(Object.values(response.data.bpi))));
+      maxPriceElement.innerHTML = maxPrice;
+      minPriceElement.innerHTML = minPrice;
+      const chart = new Chart(ctx, {
+        type: "line",
         data: {
-            labels: dateLabels,
-            datasets: [{
-            label: currency.value,
-            backgroundColor: 'rgb(0,0,139)',
-            borderColor: 'rgb(0,0,139)',
-            data: bitPrice,
-            }]
+          labels: dateLabels,
+          datasets: [
+            {
+              label: currency.value,
+              backgroundColor: "rgb(0,0,139)",
+              borderColor: "rgb(0,0,139)",
+              data: bitPrice
+            }
+          ]
         }
-        });
+      });
     })
     .catch(error => {
-    console.log(error);
-});
-}
+      console.log(error);
+    });
+};
 
 printGraph();
 
-startDate.onchange = () =>{ 
-printGraph();
-}
+startDate.onchange = () => {
+  printGraph();
+};
 
-endDate.onchange = () =>{ 
-    printGraph();
-}
+endDate.onchange = () => {
+  printGraph();
+};
 
-currency.onchange = () =>{ 
-    printGraph();
-    }
-    
-
-
+currency.onchange = () => {
+  printGraph();
+};
