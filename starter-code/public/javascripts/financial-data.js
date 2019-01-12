89523150ef1c3cfb951b78eb9d2f174c235dcbec
justinezahiri <justinezahiri@gmail.com>
2019-01-12 16:05:04 +0100

    iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -5,10 +5,40 @@ axios.get("https://api.coindesk.com/v1/bpi/historical/close.json")
   console.log("dates: " + dates);
   const values = Object.values(response.data.bpi)
   console.log("values: " + values);
+  printTheChart(dates, values);
 })
 .catch(err => {
   console.error("Error API", err);
 });
 
 
+// const getInfo = 
 
+// Chart setup 
+const printTheChart = (dates, values) => {
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: dates,
+      datasets: [{
+        label: "Amount",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: values,
+        borderWidth: 1
+      }]
+    },
+options: {
+    scales: {
+        yAxes: [{
+            ticks: {
+                beginAtZero:true
+            }
+        }]
+      }
+    }
+ });
+}
+
+// const paintTheChart = ()
