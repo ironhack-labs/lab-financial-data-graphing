b16aed9042831b5ba46fcc028e0abf2e9b71efe8
Zenchy <alvaro.corral.soilan@gmail.com>
2019-02-05 16:52:34 +0100

    [MAD PP]Eugenio y Zenchy

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
-
+var axios = require ('axios')
 /* GET home page. */
+
+
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Tu puta madre' });
 });
+
+
+
+axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+.then(data=>{
+  console.log(data)
+})
+
+.catch(err=>{console.log('Nope')})
 
 module.exports = router;
