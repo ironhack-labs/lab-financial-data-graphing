88b46d219e3ad16fc4fb7c5c8962cee655dc72e8
federicoCassettai <f.cassettai@gmail.com>
2019-02-05 17:55:06 +0100

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,51 +1,83 @@
 const stockInfo = axios.create({
-  baseURL: 'https://api.coindesk.com/v1/bpi/historical/close.json',
+  baseURL: "https://api.coindesk.com/v1/bpi/historical/close.json"
 });
 
-const printTheChart = (stockData => {
-  const stockLabels = []
-  const stockPrice =[]
+const printTheChart = stockData => {
+  const stockLabels = [];
+  const stockPrice = [];
   for (var key in stockData) {
-   
-    stockLabels.push(key)
-    stockPrice.push(stockData[key])
+    stockLabels.push(key);
+    stockPrice.push(stockData[key]);
   }
-  const ctx = document.getElementById('myChart').getContext('2d');
+
+  //display value
+  let maxValue = Math.max.apply(null,stockPrice)
+  let minValue = Math.min.apply(null,stockPrice)
+  document.getElementById('max-value').innerText = 'Max: '+maxValue+ ' '+ document.getElementById("currency-change").value
+  document.getElementById('min-value').innerText = 'Min: '+minValue + ' '+ document.getElementById("currency-change").value 
+  
+  
+  const ctx = document.getElementById("myChart").getContext("2d");
   const chart = new Chart(ctx, {
-    type: 'line',
+    type: "line",
     data: {
       labels: stockLabels,
-      datasets: [{
-        label: "Stock Chart",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: stockPrice,
-      }]
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: stockPrice
+        }
+      ]
     }
   });
-});
+};
 
-stockInfo.get()
+stockInfo
+  .get()
   .then(response => {
-    console.log(response.data.bpi)
+    console.log(response.data.bpi);
     printTheChart(response.data.bpi);
   })
   .catch(error => {
     console.log(error);
   });
 
- function changeDate(){
-  let dateFrom = document.getElementById('date-from').value 
-  let dateTo = document.getElementById('date-to').value 
 
- 
- axios.create().get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`)
-  .then(response => {
-    console.log(response.data.bpi)
-    printTheChart(response.data.bpi);
-  })
-  .catch(error => {
-    console.log(error);
-  });
+//FILTER DATE
+document.getElementById("date-to").onchange = function() {
+  let dateFrom = document.getElementById("date-from").value;
+  let dateTo = document.getElementById("date-to").value;
 
-}
+  axios
+    .create()
+    .get(
+      `https://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`
+    )
+    .then(response => {
+      console.log(response.data.bpi);
+      printTheChart(response.data.bpi);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
+
+
+//CHANGE CURRENCY
+document.getElementById("currency-change").onchange = function() {
+  let currency = document.getElementById("currency-change").value;
+
+  axios
+    .create()
+    .get(
+      `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`
+    )
+    .then(response => {
+      printTheChart(response.data.bpi);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+};
