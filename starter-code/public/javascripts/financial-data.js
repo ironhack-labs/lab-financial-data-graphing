be8ca116561c2e256b9d81e1675990f0f9c6f8f6
Dani Garcia <danielgarciamartinez1985@gmail.com>
2019-02-05 19:02:24 +0100

    almost done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,67 @@
+function axiosDates (fromDate, toDate) {
+
+  
+  axios.get ('http://api.coindesk.com/v1/bpi/historical/close.json', {
+      params: {
+        start: fromDate.value,
+        end: toDate.value,
+      },
+    })
+    .then (coins => {
+      var ctx = document.getElementById ('myChart').getContext ('2d');
+      var myChart = new Chart (ctx, {
+        type: 'line',
+        data: {
+          labels: Object.keys(coins.data.bpi),
+          datasets: [
+            {
+              label: '# of Votes',
+              data: Object.values(coins.data.bpi),
+              backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)',
+              ],
+              borderColor: [
+                'rgba(255,99,132,1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+      console.log (coins.data.bpi);
+    })
+    .catch (err => {});
+}
+
+  const fromDate = document.getElementById('fromDate')
+  const toDate = document.getElementById('toDate')
+
+  fromDate.addEventListener('change', function (e) {
+  axiosDates (fromDate, toDate);
+  })
+  toDate.addEventListener('change', function (e) {
+    axiosDates (fromDate, toDate);
+  });
+
+//axiosDates (fromDate, toDate);
