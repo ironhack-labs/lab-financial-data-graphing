70dff341aab2eb92ee5e3f2eb8b527fa17cefe0d
Jorge Silva <jorgeaferreirasilva@gmail.com>
2019-02-06 15:57:58 -0200

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,23 +1,18 @@
 const stockInfo = axios.create({
-  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json "
+  baseURL: 'http://api.coindesk.com/v1/bpi/historical/close.json '
 });
 
-// let ticket = entrada => ;
-
-// const stockTicket = 'amzn';
-// document.getElementById('ent').value.addEventListner('click',ticket);
-// console.log(stockTicket);
 
 stockInfo
   .get()
-  .then(response => {
+  .then((response) => {
     printTheChart(response.data);
   })
-  .catch(error => {
+  .catch((error) => {
     console.log(error);
   });
 
-const printTheChart = stockData => {
+const printTheChart = (stockData) => {
   const stockLabels = [];
   const stockPrice = [];
   for (key in stockData.bpi) {
@@ -25,16 +20,16 @@ const printTheChart = stockData => {
     stockPrice.push(stockData.bpi[key]);
   }
 
-  const ctx = document.getElementById("myChart").getContext("2d");
+  const ctx = document.getElementById('myChart').getContext('2d');
   const chart = new Chart(ctx, {
-    type: "line",
+    type: 'line',
     data: {
       labels: stockLabels,
       datasets: [
         {
-          label: "Stock Chart",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          label: 'Stock Chart',
+          backgroundColor: 'rgb(255, 99, 132)',
+          borderColor: 'rgb(255, 99, 132)',
           data: stockPrice
         }
       ]
