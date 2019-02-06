46a6ba0ffac4039bb8f2ef5e556566cb18ff05de
Jorge Silva <jorgeaferreirasilva@gmail.com>
2019-02-06 10:09:30 -0200

    done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,43 @@
+const stockInfo = axios.create({
+  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json "
+});
+
+// let ticket = entrada => ;
+
+// const stockTicket = 'amzn';
+// document.getElementById('ent').value.addEventListner('click',ticket);
+// console.log(stockTicket);
+
+stockInfo
+  .get()
+  .then(response => {
+    printTheChart(response.data);
+  })
+  .catch(error => {
+    console.log(error);
+  });
+
+const printTheChart = stockData => {
+  const stockLabels = [];
+  const stockPrice = [];
+  for (key in stockData.bpi) {
+    stockLabels.push(key);
+    stockPrice.push(stockData.bpi[key]);
+  }
+
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: stockLabels,
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: stockPrice
+        }
+      ]
+    }
+  });
+};
