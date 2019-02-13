ee15eb91a6be568da81ca7ee0d9b553a5d783ef0
Daniela Lopez <danielalopez@MacBook-Air-de-Daniela.local>
2019-02-13 11:24:41 -0600

    MEX-DE Daniela Mariana

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,34 @@
-var express = require('express');
-var router = express.Router();
+const router = require('express').Router();
+const axios = require ('axios')
+const url = 'https://api.coindesk.com/v1/bpi/historical/close.json'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+
+
+
+router.get('/', (req, res, next) =>{
+  axios.get(url)
+  .then(results=>{
+    let labels = Object.keys(results.data.bpi)
+    let data = Object.values(results.data.bpi)
+    res.render('index',{labels, data})
+  })
+  .catch(e =>{
+    res.render('error', e)
+  })
+})
+
+router.post('/', (req, res) => {
+  const url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${req.body.from}&end=${req.body.to}`
+  axios.get(url)
+ .then(results => {
+   console.log(results)
+    let keys = Object.keys(results.data.bpi)
+    let values = Object.values(results.data.bpi)
+    res.render('index', {keys, values} );
+ })
+ .catch(e=> {
+   res.render('error', e)
+ })
+})
 
 module.exports = router;
