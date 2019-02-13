38504d2f5cf48a872fba4acafa30f2171bf29767
CGuillotG <c.guillot@outlook.com>
2019-02-13 08:58:19 -0600

    Done with errors in axis

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,36 @@
-var express = require('express');
-var router = express.Router();
+let express = require("express");
+let router = express.Router();
+let axios = require("axios");
+let endpoint = "https://api.coindesk.com/v1/bpi/historical/close.json";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.post("/", (req, res, next) => {
+  let { fromDate, toDate, toCurrency } = req.body;
+  let search = `${endpoint}/?start=${fromDate}&end=${toDate}&currency=${toCurrency}`;
+  axios.get(search)
+    .then(results => {
+      let labels = Object.keys(results.data.bpi);
+      let data = Object.values(results.data.bpi);
+      let minValue = Math.min.apply(null, data);
+      let maxValue = Math.max.apply(null, data);
+      res.render("index", { labels, data, minValue, maxValue });
+    })
+    .catch(e => next(e));
+});
+
+router.get("/", function(req, res, next) {
+  axios
+    .get(endpoint)
+    .then(results => {
+      const labels = Object.keys(results.data.bpi);
+      const data = Object.values(results.data.bpi);
+      res.render("index", { labels, data });
+      console.log(labels);
+    })
+    .catch(e => next(e));
+});
+
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
