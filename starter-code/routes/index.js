9f66d32e249be708f857a3e2f5fb2441279e1548
Sebas Grossmann <sebasgrossmann@gmail.com>
2019-02-12 23:05:17 -0500

    not fully functional

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,58 @@
 var express = require('express');
 var router = express.Router();
 
+
+const axios = require('axios')
+const endpoints = 'http://api.coindesk.com/v1/bpi/historical/close.json'
+const moment = require('chart.js')
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+/* GET users listing. */
+let hoy = new Date()
+let dd = hoy.getDate();
+let mm = hoy.getMonth()+1; 
+let yyyy = hoy.getFullYear();
+if(dd<10) dd=`0${dd}`
+if(mm<10) mm=`0${mm}`
+hoy = `${yyyy}-${mm}-${dd}`
+hoyFrom = `${yyyy}-${mm}-01`
+
+router.post('/', (req, res, next) => {
+  const { fromDate, toDate} = req.body
+  if(!fromDate) fromDate = Date.now()
+  axios
+  .get(`${url}?start=${fromDate}&end=${toDate}`)
+    .then(results => {
+      let labels = Object.keys(results.data.bpi)
+      let data = Object.values(results.data.bpi)
+      let maxVal = Math.max(...Object.values(results.data.bpi))
+      let minVal = Math.min(...Object.values(results.data.bpi))
+      res.render('index', { labels, data,hoy, hoyFrom , maxVal, minVal })
+
+    })
+    .catch(e => next(e))
+})
+
+
+router.get('/',function(req, res, next) {
+  axios
+  .get(endpoints)
+ 
+  .then(response => {
+    const labels = Object.keys(response.data.bpi)
+    const data = Object.values(response.data.bpi)
+    const maxVal = Math.max(...Object.values(response.data.bpi))
+    const minVal = Math.min(...Object.values(response.data.bpi))
+    res.render("index",{labels,data,hoy, hoyFrom , maxVal, minVal})
+    
+  })
+  .catch(err => {
+    console.log(err);
+  });
 });
 
 module.exports = router;
--- starter-code/routes/users.js
+++ starter-code/routes/users.js
@@ -1,9 +1,22 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// const enpoints = 'http://api.coindesk.com/v1/bpi/historical/close.json'
+// /* GET users listing. */
+// router.get('/', function(req, res, next) {
+//   axios
+//   .get(endpoints)
+ 
+//   .then(response => {
+//     const {bpi} = response.data
+//     res.json(bpi)
+//     // const {results} = response.data
+//     // res.render('index', {results})
+    
+//   })
+//   .catch(err => {
+//     console.log(err);
+//   });
+// });
 
 module.exports = router;
