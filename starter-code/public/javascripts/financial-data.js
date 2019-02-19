3994f39ffd1a52c53a68c94d56ca44cce777c190
LiranGib <liran.gibor@gmail.com>
2019-02-19 14:03:33 -0500

    done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,42 @@
+axios.get('http://api.coindesk.com/v1/bpi/historical/close.json').then(chartData=>{
+ makeChart(chartData);
+})
+
+function changeDate(){
+   var start = document.getElementById("start").value;
+   var end = document.getElementById("end").value;
+
+ axios.get('http://api.coindesk.com/v1/bpi/historical/close.json?start=' + start+"&end=" + end).then(chartData=>{
+   makeChart(chartData);
+ })
+}
+
+function makeChart(chartData){
+ var ctx = document.getElementById("myChart").getContext('2d');
+ var myChart = new Chart(ctx, {
+     type: 'line',
+     data: {
+         labels: Object.keys(chartData.data.bpi),
+         datasets: [{
+             label: '# Bitcoin Price Chart',
+             data: Object.values(chartData.data.bpi),
+             backgroundColor: [
+                 'rgba(255, 99, 132, 0.2)',
+             ],
+             borderColor: [
+                 'rgba(255,99,132,1)',
+             ],
+             borderWidth: 1
+         }]
+     },
+     options: {
+         scales: {
+             yAxes: [{
+                 ticks: {
+                     beginAtZero:true
+                 }
+             }]
+         }
+     }
+ });
+}
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var axios = require('axios')
 var router = express.Router();
+const path = require('path')
+
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+
+
+  
 module.exports = router;
