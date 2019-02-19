ce798e6e8173994d682931839f8a2684b768a1a7
Manuel Garaboa <mannygaraboa@gmail.com>
2019-02-19 17:48:06 -0500

    Iteration 3 Complete

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,25 +1,33 @@
-const bitcoinPrice = [];
-let startDate = document.getElementById("startDate").value;
-let endDate = document.getElementById("endDate").value;
 
-axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-.then((res) =>{
+const baseURL = 'http://api.coindesk.com/v1/bpi/historical/close.json'
+function printTheChart(e)
+{
+    let startDate = document.getElementById("startDate").value;
+    let endDate = document.getElementById("endDate").value;
+    let url = `${baseURL}?start=${startDate}&end=${endDate}`
     
-console.log(res.data.bpi)
-
-    var ctx = document.getElementById("myChart").getContext('2d');
+    axios.get(url)
+    .then((res) =>{
+        console.log(res.data.bpi)
+        
+        var ctx = document.getElementById("myChart").getContext('2d');
         var myChart = new Chart(ctx, {
-        type: 'line',
-        data:
-        {
-            labels: Object.keys(res.data.bpi),
-            datasets: 
-            [{
-                label: "Bitcoin Price Index",
-                data: Object.values(res.data.bpi),
-                borderWidth: 1
-            }]
-        },
-    });
-})
+            type: 'line',
+            data:
+            {
+                labels: Object.keys(res.data.bpi),
+                datasets: 
+                [{
+                    label: "Bitcoin Price Index",
+                    data: Object.values(res.data.bpi),
+                    borderWidth: 1
+                }]
+            },
+        });
+    })
+}
+    
+document.getElementById('startDate').addEventListener("change", printTheChart);
+document.getElementById('endDate').addEventListener("change", printTheChart);
 
+printTheChart();
