23bb571ebfe03f7a68ce8b69acc0350e56db36c6
Abel Colmenares Napolitano <abelpronet@gmail.com>
2019-03-07 09:34:19 +0100

    not done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,25 @@
+axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
+    .then(response => printTheChart(response.data.bpi))
+    .catch(error => console.log(error));
+
+// 
+
+const printTheChart = stockData => {
+
+    const stockLabels = Object.keys(stockData);
+    const stockPrice = Object.values(stockData);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: stockLabels,
+            datasets: [{
+                label: "Whatever",
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: stockPrice,
+            }]
+        }
+    });
+};
