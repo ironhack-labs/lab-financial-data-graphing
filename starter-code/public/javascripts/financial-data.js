24e24cea7a5c7374b6d1528eb5a4d3f41dba30a1
Javier <javiermaker@gmail.com>
2019-03-07 09:39:13 +0100

    chart fix

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,20 +1,20 @@
 // const dateStart = "2018-02-15";
 // const dateEnd = "2019-01-15";
 
-const dateStart = document.getElementById('startDate');
-const dateEnd = document.getElementById('endDate');
+const dateStart = document.getElementById('startDate').value;
+const dateEnd = document.getElementById('endDate').value;
 
 
 //axios.get(`https://api.iextrading.com/1.0/stock/${stockTicket}/chart`)
 axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=<${dateStart}>&end<${dateEnd}>`)
     // console.log(response.data))
-    .then(response => printTheChart(response.data))
+    .then(response => printTheChart(response.data.bpi))
     .catch(error => console.log(error));
 
 const printTheChart = historicalData => {
 
-    const dates = historicalData.map(element => element.bpi.keys());
-    const prices = historicalData.map(element => element.bpi.values());
+    const dates = Object.keys(historicalData);
+    const prices = Object.values(historicalData);
 
     const ctx = document.getElementById('myChart').getContext('2d');
     new Chart(ctx, {
@@ -23,7 +23,7 @@ const printTheChart = historicalData => {
             labels: dates,
             datasets: [{
                 label: "Whatever",
-                backgroundColor: 'rgb(255, 99, 132)',
+                backgroundColor: 'rgb(20, 90, 89)',
                 borderColor: 'rgb(255, 99, 132)',
                 data: prices,
             }]
