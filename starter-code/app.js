94cd80c60a4dee3abb07d51879999356de60f937
Merle Jansen <merlevanjansen@gmail.com>
2019-04-05 11:45:58 +0200

    first upload

--- starter-code/app.js
+++ starter-code/app.js
@@ -10,6 +10,7 @@ var users = require('./routes/users');
 
 var app = express();
 
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -24,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,56 @@
+var url = 'http://api.coindesk.com/v1/bpi/historical/close.json'
+
+
+/* GET home page */
+  axios.get(url)
+    .then(result => {
+      //console.log(result.data.bpi)
+      printTheChart(result)
+    })
+    .catch(err => console.log(err))
+
+
+function printTheChart(something){
+      //get keys and values from Object:
+      const value = Object.values(something.data.bpi)
+      const date = Object.keys(something.data.bpi)
+      const ctx = document.getElementById('myChart').getContext('2d');
+      const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: date,
+          datasets: [{
+            label: "Bitcoin price index",
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: value,
+          }]
+        }
+      });
+    };
+
+    // Add a listener to this input to search the data every time that its values changes.
+     // Add the dates  to the API URL to get the correct data.
+    document.getElementById("submitDate").addEventListener("click", function(){
+      debugger
+
+      const start = document.getElementById("startDate").value;
+      const end = document.getElementById("endDate").value;
+      // var urlDate = 
+      // console.log(urlDate)
+      
+      axios.get('https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}')
+      .then(result => {
+        console.log(result.data.bpi)
+        printTheChart(result)
+      })
+      .catch(err => console.log(err))
+    });
+
+   
+
+  
+
+    
+
+ 
