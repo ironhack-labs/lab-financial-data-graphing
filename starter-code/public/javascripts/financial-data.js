c4c571c3728e382ffe2c73a5f9908abf4d42e24d
marianaLz <marianaglp15@gmail.com>
2019-04-05 14:24:56 -0600

    Done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,44 @@
+const base_url = 'http://api.coindesk.com/v1/bpi/historical/close.json'
+let ctx = document.getElementById('chart').getContext('2d');
+
+function setCharacter(e){
+  const value = Object.values(e.data.bpi)
+  const date = Object.keys(e.data.bpi)
+  let maxPrice = Math.max(...value);
+  let minPrice = Math.min(...value);
+  document.getElementById('max-price').innerHTML = maxPrice;
+  document.getElementById('min-price').innerHTML = minPrice;
+
+  const chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels: date,
+      datasets: [{
+        label: "Bitcoin price index",
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data: value,
+      }]
+    }
+  });
+};
+
+axios.get(base_url)
+.then(result => {
+  setCharacter(result)
+})
+.catch(err => console.log(err))
+
+document.getElementById("submitDate").addEventListener("click", function(){
+  const init = document.getElementById("initialDate").value;
+  const final = document.getElementById("finalDate").value;
+  let mon = document.getElementById("currency");
+  let currency = mon.options[mon.selectedIndex].value;
+  let urlDate = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${init}&end=${final}&currency=${currency}`
+  
+  axios.get(urlDate)
+  .then(result => {
+    setCharacter(result)
+  })
+  .catch(err => console.log(err))
+});
