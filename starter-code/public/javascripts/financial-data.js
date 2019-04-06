d1f2380655d18312643455e37b15f00b6874b88c
lockeas16 <lockeas16@gmail.com>
2019-04-05 21:10:20 -0600

    segunda iteracion finalizada

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,10 +1,39 @@
 const baseUrl = "https://api.coindesk.com/v1/bpi";
+const ctx = document.getElementById("chart").getContext("2d");
+const redCol = randomNum();
+const greenCol = randomNum();
+const blueCol = randomNum();
+
+function randomNum(maxNum = 256) {
+  return Math.floor(Math.random() * maxNum);
+}
 
 axios
   .get(`${baseUrl}/historical/close.json`)
   .then(res => {
-    console.log(res.data);
+    let bpi = res.data.bpi;
+    let bpiDates = [];
+    let bpiValues = [];
+    // loop through the properties in the object
+    for (let date in bpi) {
+      bpiDates.push(date);
+      bpiValues.push(bpi[date]);
+    }
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: bpiDates,
+        datasets: [
+          {
+            label: "Bitcoin Price Index",
+            backgroundColor: `rgb(${redCol}, ${greenCol}, ${blueCol})`,
+            borderColor: `rgb(${redCol}, ${greenCol}, ${blueCol})`,
+            data: bpiValues
+          }
+        ]
+      }
+    });
   })
   .catch(err => {
-    console - log("Error al recuperar datos de API de coindesk");
+    console.log("Error al recuperar datos de API de coindesk");
   });
