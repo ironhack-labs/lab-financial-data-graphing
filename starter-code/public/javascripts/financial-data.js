b4e7c9df7f654962d3e31d09456d5d5ba98d06a4
Fernando Lopez <fnandolopezsanchez@gmail.com>
2019-04-16 18:25:14 +0200

    4 iteration done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,45 +1,56 @@
-var ticket;
-    document.getElementById("applyDates").onchange = function (e) {
-      ticket = e.value
-      draw()
-    }
-    
-    
-    let start = document.querySelector("#dateStart")
-    let end = document.querySelector("#dateEnd")
-    
-    document.querySelector("#applyDates").onclick = function (){
-      draw();
-    }
-    
-    // var companiesColors = {
-    //   amzn: "#ffcc0099",
-    //   micr: "#0044ff99"
-    // }
-    function draw() {
-      axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start.value}&end=${end.value}`)
-        .then(bitCoinData => {
+let money = document.querySelector("#coinType");
+let coin;
 
-          bitCoinData = bitCoinData.data
-          const X1 = Object.keys(bitCoinData.bpi)
-          const Y1 = Object.values(bitCoinData.bpi)
-          console.log(X1,Y1)
-          const ctx = document.getElementById('bitcointchart').getContext('2d');
-          const chart = new Chart(ctx, {
-            type: 'line',
-            data: {
-              labels: X1,
-              datasets: [{
-                label: "Stock Chart",
-                borderWidth: 3,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgb(255, 99, 132, 0.5)',
-                data: Y1,
-              }]
+document.getElementById("coinType").onchange = function(e) {
+  coin = e.value;
+  draw();
+};
+
+var date;
+document.getElementById("applyDates").onchange = function(e) {
+  date = e.value;
+  draw();
+};
+
+let start = document.querySelector("#dateStart");
+let end = document.querySelector("#dateEnd");
+
+document.querySelector("#applyDates").onclick = function() {
+  draw();
+};
+
+// var companiesColors = {
+//   amzn: "#ffcc0099",
+//   micr: "#0044ff99"
+// }
+function draw() {
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?currency=${money.value}&?start=${start.value}&end=${end.value}`
+    )
+    .then(bitCoinData => {
+      bitCoinData = bitCoinData.data;
+      const X1 = Object.keys(bitCoinData.bpi);
+      const Y1 = Object.values(bitCoinData.bpi);
+      console.log(X1, Y1);
+      const ctx = document.getElementById("bitcointchart").getContext("2d");
+      const chart = new Chart(ctx, {
+        type: "line",
+        data: {
+          labels: X1,
+          datasets: [
+            {
+              label: "Stock Chart",
+              borderWidth: 3,
+              borderColor: "rgb(255, 99, 132)",
+              backgroundColor: "rgb(255, 99, 132, 0.5)",
+              data: Y1
             }
-          });
-        })
-        .catch(error => {
-          console.log(error);
-        });
-    }
+          ]
+        }
+      });
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
