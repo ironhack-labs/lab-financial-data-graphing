8222586704b6ccccc5398d5edb1ac10e48264eff
AnthonySBR <asbr1811@gmail.com>
2019-04-16 17:16:31 +0200

    Done. Missing bonus

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,18 +1,44 @@
-import Axios from "axios";
+let startDate = '2019-03-19';
+let endDate = '2019-03-25';
+let currency = 'USD';
 
-let startDate, endDate
-
-document.querySelector('#start').onchange(e)=function(){
+document.querySelector('#start').onchange = function (e) {
   startDate = e.target.value;
   drawChart();
 }
 
-document.querySelector('#end').onchange(e)=function(){
-  startDate = e.target.value;
+document.querySelector('#end').onchange = function (e) {
+  endDate = e.target.value;
   drawChart();
 }
 
-function drawChart (){
-  Axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
-    .then()
+document.querySelector('#currency').onchange = function (e) {
+  currency = e.target.value;
+  console.log(currency);
+  drawChart();
+}
+
+function drawChart() {
+  axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=${startDate}&end=${endDate}`)
+    .then(stockInfo => {
+      stockInfo = stockInfo.data
+      var stockDate = Object.keys(stockInfo.bpi);
+      var stockValue = stockDate.map(function (k) {
+        return stockInfo.bpi[k];
+      });
+      const ctx = document.querySelector('#chart').getContext('2d');
+      const chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: stockDate,
+          datasets: [{
+            label: "Bitcoins value",
+            borderWidth: 3,
+            borderColor: 'rgb(255, 99, 132)',
+            data: stockValue,
+          }]
+        }
+      });
+    }
+    )
 }
