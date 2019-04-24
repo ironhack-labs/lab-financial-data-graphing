818190ce4aa226230d693f920a399bcf521f541c
frankismael <ismaelfcom93@gmail.com>
2019-04-24 08:15:44 -0500

    avance

--- starter-code/app.js
+++ starter-code/app.js
@@ -5,6 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
 var index = require('./routes/index');
 var users = require('./routes/users');
 
@@ -21,6 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.use('/', index);
 app.use('/users', users);
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,32 @@
 var express = require('express');
 var router = express.Router();
+var axios = require('axios')
+var Chart = require('chart.js');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+
+  axios.get('https://api.coindesk.com/v1/bpi/historical/close.json') //  https://api.coindesk.com/v1/bpi/historical/close.json
+                                                                 //  https://api.coindesk.com/v1/bpi/currentprice.json
+    .then(response => {
+      const info = response.data.bpi 
+      console.log(info)
+      res.render('index', info);
+    })
 });
 
 module.exports = router;
+
+
+
+/* axios.get('https://rickandmortyapi.com/api/character/')
+  .then(response => response.data.results
+        .map(({name, image})=> {
+      var domName = document.createElement('p')
+      domName.innerHTML = name
+      var domImage = document.createElement('img')
+      domImage.src = image
+      var body = document.querySelector('body')
+      body.appendChild(domName)
+      body.appendChild(domImage)
+})) */
