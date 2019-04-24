88ed82f4dbec58266e9a26b363d6fea005640513
VictoriaPl <victoriapp15@gmail.com>
2019-04-23 22:56:35 -0500

    First commit

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -7,46 +7,4 @@ router.get('/', function(req, res, next) {
   res.render('index')
 });
 
-
-// axios.get('https://rickandmortyapi.com/api/character/')
-//   .then(response => response.data.results.map(
-//   char => console.log(char.name)))
-
-//   axios.get('https://rickandmortyapi.com/api/character/')
-//   .then(response => response.data.results.map(
-//     char => console.log(char => {
-//       const domName = document.createElement('p')
-//       domName.innerHTML = char.name
-//     }
-// )))
-
-// axios.get('https://rickandmortyapi.com/api/character/')
-//   .then(response => response.data.results.map(
-//     char => console.log(({name, image}) => {
-//       const domName = document.createElement('p')
-//       domName.innerHTML = name
-//       const domImage = document.createElement('img')
-//       domImage.src = image
-//       const body = document.querySelector('body')
-//       body.appendChild(domName)
-//       body.appendChild(domImage)
-//     }
-// )))
-// //srcipt in public javascripts
-// <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
-
-// //where?
-// axios.get('https://rickandmortyapi.com/api/character/')
-//   .then(response => response.data.results
-//         .map(({name, image}) => {
-//       var domName = document.createElement('p')
-//       domName.innerHTML = name
-//       var domImage = document.createElement('img')
-//       domImage.src = image
-//       var body = document.querySelector('body')
-//       body.appendChild(domName)
-//       body.appendChild(domImage)
-//     }
-// ))
-
 module.exports = router;
