de93cbc5f9a1a707f1684fb583e51441a2779e7d
pipepico <fpr29.10@gmail.com>
2019-04-24 01:53:21 -0500

    init

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const Axios = require('axios')
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => res.render('index'))
+
+axios.get('/user', {
+  params: {
+    ID: 12345
+  }
+})
+.then(function (response) {
+  console.log(response);
+})
+.catch(function (error) {
+  console.log(error);
 });
+
 
 module.exports = router;
