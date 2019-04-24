fb50e292ba7d2bae367613e1cf64212c13a9b976
gustavvopenna <gustavo.pema@gmail.com>
2019-04-24 01:33:15 -0500

    Add min-max values

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,31 +1,28 @@
+const minValue = document.getElementById('min-value')
+const maxValue = document.getElementById('max-value')
+
 
 function getData() {
   axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${setDate1()}&end=${setDate2()}&currency=${setCurrency()}`)
   .then(response => {
-    console.log(response.data.bpi)
+    let min = Math.min(...Object.values(response.data.bpi)).toFixed(2)
+    let max = Math.max(...Object.values(response.data.bpi)).toFixed(2) 
+    minValue.innerHTML = `Min: ${min}`
+    maxValue.innerHTML = `Max: ${max}`
+
     var ctx = document.getElementById('myChart');
     var myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: Object.keys(response.data.bpi),
             datasets: [{
-                label: '# of Votes',
+                label: 'Bitcoin Historic Price',
                 data: Object.values(response.data.bpi),
                 backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
+                    'rgba(255, 99, 132, 0.2)'
                 ],
                 borderColor: [
-                    'rgba(255, 99, 132, 1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
+                    'rgba(255, 99, 132, 1)'                    
                 ],
                 borderWidth: 1
             }]
@@ -40,6 +37,7 @@ function getData() {
             }
         }
     })
+    //minValue.innerText = Math.min(valuesArr).toString()
   })
   .catch(err => console.log(err)) 
 }
@@ -53,9 +51,11 @@ firstDate.addEventListener('change', setDate1)
 const lastDate = document.getElementById('last-date')
 lastDate.addEventListener('change', setDate2)
 
-
 const currency = document.getElementById('currency')
 currency.addEventListener('change', setCurrency)
+
+//const minValue = document.getElementById('min-value')
+//minValue.innerHTML = String(Math.min(Object.values(response.data.bpi)))
 
 
 function setDate1() {
@@ -73,4 +73,9 @@ function setDate2() {
 function setCurrency() {
   let cur = currency.value
   return cur
+}
+
+function getMax(arr) {
+  let num = Math.min(Object.values(arr.data.bpi))
+  return num 
 }
