75ed8503e59c0410efb780e16283ecf905811a57
letiescanciano <leticia.escanciano@gmail.com>
2019-05-21 17:03:25 +0200

    Bonus Currency&Max/Min Values

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,16 +1,30 @@
 const bitcoinAPI = axios.create({
-  baseURL: 'https://api.coindesk.com/v1/bpi/historical/close.json'
+  baseURL: 'http://api.coindesk.com/v1/bpi/historical'
 })
 
-console.log(bitcoinAPI)
-const getClosePrices = ()=>{
-  console.log('entro en getClosePrices')
-  bitcoinAPI.get()
-  .then(results => {
-    printTheChart(results.data.bpi);
 
-  })
-  .catch(err=>console.log(err))
+const getClosePrices = ()=>{
+  //console.log('entro en getClosePrices')
+
+  const dates = {
+    start: document.getElementById('start').value,
+    end: document.getElementById('end').value,
+  }
+  const currency = document.getElementById('currency').value
+
+  let query;
+
+  if(dates.start && dates.end && currency) {
+    query = `?start=${dates.start}&end=${dates.end}&currency=${currency}`
+  }
+  else if (currency){
+    query = `?currency=${currency}`
+  }
+  bitcoinAPI.get(`/close.json${query}`)
+      .then(results => {
+        printTheChart(results.data.bpi)
+      })
+      .catch(err => console.log(err))
 }
 
 const printTheChart = (data) =>{
@@ -19,6 +33,11 @@ const printTheChart = (data) =>{
 
   const ctx = document.getElementById('chart').getContext('2d')
 
+  console.log(prices)
+  document.getElementById('max').innerText= `${Math.max(...prices)} ${document.getElementById('currency').value}`
+  document.getElementById('min').innerText = `${Math.min(...prices)} ${document.getElementById('currency').value}`
+  
+  
   new Chart(ctx, {
     type: 'line',
     data: {
@@ -34,4 +53,9 @@ const printTheChart = (data) =>{
     }
   })
 }
+
 getClosePrices()
+document.getElementById('start').onchange = () => getClosePrices()
+document.getElementById('end').onchange = () => getClosePrices()
+document.getElementById('currency').onchange = () => getClosePrices()
+
