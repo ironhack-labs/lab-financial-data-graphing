ec1e35331de41d5c2a52a384dbae201386560666
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-05-21 17:33:42 +0200

    Iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,39 +1,57 @@
 const bitCoinInfo = axios.create({
-  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+  baseURL: "http://api.coindesk.com/v1/bpi/historical"
 })
+let start;
+let end;
 
 
-
-bitCoinInfo.get(`/chart`)
-
-  .then(response => {
-    printBitCoin(response.data.bpi)
-    console.log(response.data.bpi)
-  })
-  .catch(error => console.log(error))
-
-
-
-const printBitCoin = bitData => {
-
-  const stockLabels = Object.keys(bitData)
-  const stockPrice = Object.values(bitData)
-  const ctx = document.getElementById('chart').getContext('2d');
-
-
-  new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: stockLabels,
-      datasets: [{
-        label: "Stock Chart",
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
-        data: stockPrice,
-      }]
-    }
-  });
+document.getElementById("theButton").onclick = () => {
+  start = document.getElementById("startDate").value
+  end = document.getElementById("endDate").value
+  if (start && end) {
+    pepe(start, end)
+  }
 }
 
+const pepe = (start, end) => {
+  bitCoinInfo.get(`/clone.json?start=${start}&end=${end}`)
+
+    .then(response => {
+      printBitCoin(response.data.bpi)
+      console.log(response.data.bpi)
+    })
+    .catch(error => console.log(error))
+}
+
+const ctx = document.getElementById('chart').getContext('2d');
+
+
+let myChart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: undefined,
+    datasets: [{
+      label: "Stock Chart",
+      backgroundColor: 'rgb(255, 99, 132)',
+      borderColor: 'rgb(255, 99, 132)',
+      data: undefined,
+    }]
+  }
+});
+
+const printBitCoin = (bpi) => {
+
+  const stockLabels = Object.keys(bpi)
+  const stockPrice = Object.values(bpi)
+
+  myChart.data.labels = stockLabels
+  myChart.data.datasets[0].data = stockPrice
+
+  myChart.update();
+
+
+
+
+}
 
 
