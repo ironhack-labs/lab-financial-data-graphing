91e82bf961a23783d195d01bba797d12342d704f
letiescanciano <leticia.escanciano@gmail.com>
2019-05-21 15:49:15 +0200

    Iteration #2: Create chart

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -7,10 +7,31 @@ const getClosePrices = ()=>{
   console.log('entro en getClosePrices')
   bitcoinAPI.get()
   .then(results => {
-    console.log(results.data.bpi)
+    printTheChart(results.data.bpi);
 
   })
   .catch(err=>console.log(err))
 }
 
+const printTheChart = (data) =>{
+  const labels = Object.keys(data)
+  const prices = Object.values(data)
+
+  const ctx = document.getElementById('chart').getContext('2d')
+
+  new Chart(ctx, {
+    type: 'line',
+    data: {
+      labels,
+      datasets: [
+        {
+          label: "Bitcoin Chart",
+          backgroundColor: 'red',
+          borderColor: 'white',
+          data: prices
+        }
+      ]
+    }
+  })
+}
 getClosePrices()
