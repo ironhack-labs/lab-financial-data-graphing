10b6baaf70ac6f64ac34707afa01763ec80e3b84
Francho Garcia <franchog79@gmail.com>
2019-07-02 17:49:36 +0200

    Iteration 2 finished

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -2,12 +2,54 @@
 
 
 // }
-
+let labelsJson = []
+let dataJson = []
 axios
-    .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05")
+    .get("https://api.coindesk.com/v1/bpi/historical/close.json")
+    // .get("https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05")
     .then(JSONPayload => {
-        console.log(JSONPayload.data.bpi)
-        JSONPayload.data.bpi.forEach(element => {
-            console.log(element)
-        });
+        for (let date in JSONPayload.data.bpi) {
+            console.log("Property=" + date)
+            labelsJson.push(date)
+            dataJson.push(JSONPayload.data.bpi[date])
+            console.log("valor de property = " + JSONPayload.data.bpi[date])
+        }
     })
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: labelsJson,
+        datasets: [{
+            label: 'Bitcoin Price Index',
+            data: dataJson,
+            // backgroundColor: [
+            //     'rgba(255, 99, 132, 0.2)',
+            //     'rgba(54, 162, 235, 0.2)',
+            //     'rgba(255, 206, 86, 0.2)',
+            //     'rgba(75, 192, 192, 0.2)',
+            //     'rgba(153, 102, 255, 0.2)',
+            //     'rgba(255, 159, 64, 0.2)'
+            // ],
+            // borderColor: [
+            //     'rgba(255, 99, 132, 1)',
+            //     'rgba(54, 162, 235, 1)',
+            //     'rgba(255, 206, 86, 1)',
+            //     'rgba(75, 192, 192, 1)',
+            //     'rgba(153, 102, 255, 1)',
+            //     'rgba(255, 159, 64, 1)'
+            // ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        // scales: {
+        //     yAxes: [{
+        //         ticks: {
+        //             beginAtZero: true
+        //         }
+        //     }]
+        // }
+    }
+});
