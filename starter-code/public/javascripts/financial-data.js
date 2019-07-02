13d7b223cf0a336cbac23813d188ce9ed55f5d85
Francho Garcia <franchog79@gmail.com>
2019-07-02 20:03:39 +0200

    Iteration 4 bonus Finished

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,37 +1,15 @@
 let link = "https://api.coindesk.com/v1/bpi/historical/close.json"
 
-let initialDate = undefined;
-let finalDate = undefined
-
-let labelsJson = []
-let dataJson = []
 let myChart
 
-
-
-axios
-    .get("https://api.coindesk.com/v1/bpi/historical/close.json")
-    .then(JSONPayload => {
-        var ctx = document.getElementById('myChart').getContext('2d');
-        myChart = new Chart(ctx, {
-            type: 'line',
-            data: {
-                labels: Object.keys(JSONPayload.data.bpi),
-                datasets: [{
-                    label: 'Bitcoin Price Index',
-                    data: Object.values(JSONPayload.data.bpi),
-
-                    borderWidth: 1
-                }]
-            },
-            options: {}
-        });
-    })
+UpdateData()
 
 document.getElementById("getAPIInfo").onclick = function() {
-    initialDate = document.getElementById("InitialDate").value
-    finalDate = document.getElementById("FinalDate").value
-    link = link + "?start=" + initialDate + "&end=" + finalDate
+    let initialDate = document.getElementById("InitialDate").value
+    let finalDate = document.getElementById("FinalDate").value
+    let currency = document.getElementById("currency").value
+
+    link = link + "?start=" + initialDate + "&end=" + finalDate + "&currency=" + currency
     UpdateData()
 };
 
