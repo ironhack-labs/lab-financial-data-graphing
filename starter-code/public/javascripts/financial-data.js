b84316ec01b6d96190c758bd0d600f9576dcb49a
Miriloper <miriam.mad.ml@gmail.com>
2019-07-02 20:34:42 +0200

    finish without bonus but FREAKIN PERFECT

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,52 +1,37 @@
-axios
-  .get("https://api.coindesk.com/v1/bpi/historical/close.json")
+
+
+  PEPITO();
+
+  function PEPITO() {
+    let start = document.getElementById("startDate").value
+    let end = document.getElementById("endDate").value
+    axios
+  .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
   .then(JSONPayload => {
     console.log("Response from API is: ", JSONPayload.data.bpi);
-
+     var axisX = Object.keys(JSONPayload.data.bpi)
+    // var axisX = Object.keys(JSONPayload.data.bpi).filter(function(d, i) {
+    //   // return d >= this.document.getElementsById(`startDate`) && d <= this.);
+    //   return d >= 2019-06-03 && d <= 2019-07-01;
+    // });
     var ctx = document.getElementById("myChart").getContext("2d");
     var myChart = new Chart(ctx, {
       type: "line",
       data: {
-        labels: Object.keys(JSONPayload.data.bpi),
-        datasets: [{
-          label: '# financial data',
-          data: Object.values(JSONPayload.data.bpi),
-          backgroundColor: '#fabada',
-          borderColor: 'teal'
-
-          //movidas de colores y data
-        
-        }]
+        labels: axisX,
+        // labels: Object.keys(JSONPayload.data.bpi),
+        datasets: [
+          {
+            label: "# financial data",
+            data: Object.values(JSONPayload.data.bpi),
+            backgroundColor: "#fabada",
+            borderColor: "teal"
+          }
+        ]
       }
-      
-      
-      
-      
-      //JSONPayload.data.bpi,
-      // options: {xAxes: yAxes}
     });
-    myChart.update()
+
+    myChart.update();
   });
+  }
 
-
-
-// var chart = new Chart(ctx, {
-//   // The type of chart we want to create
-//   type: 'line',
-//   // The data for our dataset
-//   data: {
-//     labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-//     datasets: [{
-//       label: 'My First dataset',
-//       backgroundColor: 'rgb(255, 99, 132)',
-//       borderColor: 'rgb(255, 99, 132)',
-//       data: [0, 10, 205, 2, 20, 30, 45]
-//     }]
-//   },
-//   // Configuration options go here
-//   options: {}
-// });
-
-// chart.data.datasets[0].data[i] = chart.data.datasets[0].data[i] * Math.random()
-
-// chart.update();
