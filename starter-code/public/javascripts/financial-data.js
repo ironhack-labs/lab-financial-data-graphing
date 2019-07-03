8e5a11e5960878543ce5857dbd7af2cf8d4ba2e8
Daniel Orio <danielislas3@live.com>
2019-07-02 19:25:12 -0500

    iteracion 2 terminada - no mames si me salio 😁

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -3,6 +3,28 @@ const ctx = canvas.getContext('2d')
 
   axios.get(('http://api.coindesk.com/v1/bpi/historical/close.json'))
   .then(({data})=>{
-    const arr = Object.values(data.bpi)
-    console.log(arr)
+    const arrData = Object.values(data.bpi)
+    const arrKeys = Object.keys(data.bpi)
+    const myChart = new Chart(ctx, {
+      type: 'line',
+      data: {
+          labels: arrKeys,
+          datasets: [{
+              label: '# of Votes',
+              data: arrData,
+              borderWidth: 1
+          }]
+      },
+      options: {
+          scales: {
+              yAxes: [{
+                  ticks: {
+                      beginAtZero: true
+                  }
+              }]
+          }
+      }
+  })
   }).catch(err=>console.log(err))
+
+
