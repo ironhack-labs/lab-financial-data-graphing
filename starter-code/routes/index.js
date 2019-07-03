cba27b4d803e135498b9644222566158ac53badc
Dago Rodriguez <dagor@Dagos-MacBook-Pro.local>
2019-07-02 21:13:10 -0500

    finished iteration 2, i am halfway thorugh 3

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,12 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function(req, res, next) {
+  res.render("index", { title: "Express" });
+});
+router.get("/api/data", function(req, res, next) {
+  res.render("api-data");
 });
 
 module.exports = router;
