d1a8765a83dbfa91464cbe4d1a050b4acc7d477a
fabio <fabgmad@gmail.com>
2019-10-02 14:06:52 -0300

    Iteration 1: Done

--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,9 +1,17 @@
 var express = require('express');
 var router = express.Router();
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+// Get API
+
+axios
+  .get('http://api.coindesk.com/v1/bpi/historical/close.json')
+  .then(responseFromAPI => console.log("The response from API: ", responseFromAPI))
+  .catch(err => console.log("Error while getting the data: ", err));
 
 module.exports = router;
