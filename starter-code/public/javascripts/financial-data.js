e957ddd3e9bacd9c4fed4017dc8ee5531918f020
angelmc32 <angel.melendez.c@gmail.com>
2019-10-04 17:54:14 -0500

    Done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,17 +1,26 @@
 const ctx = document.getElementById("chart");
+const startDate = document.getElementById("from");
+const endDate = document.getElementById("to");
+const currency = document.getElementById("currency");
+const maxPrice = document.getElementById("max");
+const minPrice = document.getElementById("min");
+let query = "";
+
+const apiEndpoints = {
+  default: "https://api.coindesk.com/v1/bpi/historical/close.json",
+  date: "",
+  currency: "",
+};
+
+let getURL = `${apiEndpoints.default}${apiEndpoints.currency}${apiEndpoints.date}`;
 
 const getBitcoinPriceIndex = (url = "https://api.coindesk.com/v1/bpi/historical/close.json" ) => {
   
   axios.get(url)
   .then( res => {
-
     const { bpi } = res.data;
-    //const dates = Object.keys(bpi);
-    //const prices = dates.map( date => bpi[date] )
-
-    //console.log(typeof dates, typeof prices)
     printChart(bpi);
-    
+    getMaxMin(bpi);
   })
   .catch(err => console.log("Error while getting the data: ", err));
 
@@ -20,7 +29,7 @@ const getBitcoinPriceIndex = (url = "https://api.coindesk.com/v1/bpi/historical/
 const printChart = (bpiData) => {
 
   const dates = Object.keys(bpiData);
-  const prices = dates.map( date => bpiData[date] )
+  const prices = dates.map( date => bpiData[date] );
 
   const chart = new Chart(ctx, {
     type: "line",
@@ -29,8 +38,8 @@ const printChart = (bpiData) => {
       datasets: [
         {
           label: "Bitcoin Price Index",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          backgroundColor: "rgb(55, 55, 141)",
+          borderColor: "rgb(43, 43, 105)",
           data: prices
         }
       ]
@@ -39,4 +48,41 @@ const printChart = (bpiData) => {
 
 }
 
+const getMaxMin = (bpiData) => {
+
+  const dates = Object.keys(bpiData);
+  const prices = dates.map( date => bpiData[date] );
+
+  maxPrice.innerText = `Max: $${Math.max.apply(null, prices)}`;
+  minPrice.innerText = `Min: $${Math.min.apply(null, prices)}`;
+
+  console.log(prices);
+}
+
 getBitcoinPriceIndex();
+
+const getDataWithRange = () => {
+  if (startDate.value && endDate.value) {
+    apiEndpoints.date = `?start=${startDate.value}&end=${endDate.value}`;
+    getURL = `${apiEndpoints.default}${apiEndpoints.date}&&${apiEndpoints.currency}`;
+    getBitcoinPriceIndex(getURL);
+  }
+  else console.log(false);
+}
+
+const getDataWithCurrency = () => {
+  if (currency.value) {
+    apiEndpoints.currency = `?currency=${currency.value}`;
+    getURL = `${apiEndpoints.default}${apiEndpoints.date}&&${apiEndpoints.currency}`;
+    console.log(`Requesting currency: ${apiEndpoints.currency}`);
+    getBitcoinPriceIndex(getURL);
+  }
+  else console.log(false);
+}
+
+startDate.onchange = () => getDataWithRange();
+endDate.onchange = () => getDataWithRange();
+
+currency.onchange = () => getDataWithCurrency();
+
+console.log(getURL);
