2dafe590fb5387603bf88ca06ebc1fb03265b88d
Flor Urbinati <flor29600@hotmail.com>
2019-11-19 19:09:58 +0100

    iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,40 +1,57 @@
-// const key = "demo";
-// const functionName = "TIME_SERIES_DAILY";
-// const symbolName = "MSFT";
-const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+// const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+let apiUrl;
 
-axios
-    .get(apiUrl)
-    .then(responseFromAPI => {
-        console.log(responseFromAPI.data);
-        // res.json('index', responseFromAPI.data)
-        printTheChart(responseFromAPI.data);
-    })
-    .catch(err => {
-        console.log("Error while getting the data: ", err);
-    });
 
-    function printTheChart(stockData) {
-        const dailyData = stockData.bpi;
-        console.log(dailyData);
-        const dates = Object.keys(dailyData);
-        // console.log(x);
-        const values = Object.values(dailyData);
-        // console.log(y);
-        const ctx = document.getElementById("myChart").getContext("2d");
-        const chart = new Chart(ctx, {
-          type: "line",
-          data: {
+
+
+
+
+function printTheChart(stockData) {
+    const dailyData = stockData.bpi;
+    console.log(dailyData);
+    const dates = Object.keys(dailyData);
+    const values = Object.values(dailyData);
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const chart = new Chart(ctx, {
+        type: "line",
+        data: {
             labels: dates,
-            datasets: [
-              {
+            datasets: [{
                 label: "Stock Chart",
                 backgroundColor: "rgb(255, 99, 132)",
                 borderColor: "rgb(255, 99, 132)",
                 fill: false,
                 data: values
-              }
-            ]
-          }
-        }); 
-      } 
+            }]
+        }
+    });
+}
+
+const from = document.querySelector('#from');
+const to = document.querySelector('#to');
+
+
+function selectedDates() {
+    let minDate = from.value;
+    let maxDate = to.value
+    apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${minDate}&end=${maxDate}`
+    console.log(apiUrl)
+    axios
+        .get(apiUrl)
+        .then(responseFromAPI => {
+            printTheChart(responseFromAPI.data);
+        })
+        .catch(err => {
+            console.log("Error while getting the data: ", err);
+        });
+}
+
+
+from.onchange= () => {
+    selectedDates()
+}
+to.onchange= () => {
+    selectedDates()
+}
+
+window.onload = selectedDates()
