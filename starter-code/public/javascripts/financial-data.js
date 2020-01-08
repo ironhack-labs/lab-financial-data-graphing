efad634d9e4c3d4a3591497a1a145450966bf751
Juliane Arruda <juliane_ch.arruda@live.com>
2020-01-08 18:46:38 -0300

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,13 +1,13 @@
-const startDate = 'xxx';
-const endDate = 'xxx';
-const currencyType = 'xxx';
 
-const currency = `?currency=${currencyType}`;
-const period = `?start=${startDate}&end=${endDate}`;
-const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+// const currency = `?currency=${currencyType}`;
+// const period = `?start=${startDate}&end=${endDate}`;
 
-axios
-  .get(apiUrl)
+const bitcoinApi = axios.create({
+  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+});
+
+bitcoinApi
+  .get()
   .then(responseFromAPI => {
     printTheChart(responseFromAPI.data);
     // console.log("The response from API: ", responseFromAPI);
@@ -18,13 +18,13 @@ axios
 
 function printTheChart(coinData) {
   const dailyData = coinData.bpi;
-  console.log(dailyData);
+  // console.log(dailyData);
 
   const coinDates = Object.keys(dailyData)
-  console.log(coinDates);
+  // console.log(coinDates);
 
   const coinValues = coinDates.map((date) => dailyData[date])
-  console.log(coinValues);
+  // console.log(coinValues);
 
   const ctx = document.getElementById("myChart").getContext("2d");
   const chart = new Chart(ctx, {
@@ -42,7 +42,50 @@ function printTheChart(coinData) {
 
   const maxValue = Math.max(...coinValues);
   const minValue = Math.min(...coinValues);
-  document.getElementById("max-value").innerHTML = 'Max: ' + maxValue;
+  const money = document.getElementById('currency').value;
+  document.getElementById("max-value").innerHTML = 'Max: ' + maxValue + ' ' + money;
 
-  document.getElementById("min-value").innerHTML = 'Min: ' + minValue;
+  document.getElementById("min-value").innerHTML = 'Min: ' + minValue + ' ' + money;
 } // closes printTheChart()
+
+function getPeriodData(start, end) {
+  const period = `?start=${start}&end=${end}`;
+  console.log(period);
+  // bitcoinApi
+  axios
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json" + period)
+  .then(responseFromAPI => {
+    printTheChart(responseFromAPI.data);
+    // console.log("The response from API: ", responseFromAPI);
+  })
+  .catch(err => {
+    console.log("Error while getting the data: ", err);
+  });
+}
+
+
+function getCurrencyData(selectedCurrency) {
+  const currency = `?currency=${selectedCurrency}`;
+  // console.log(currency);
+  // bitcoinApi
+  axios
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json" + currency)
+  .then(responseFromAPI => {
+    printTheChart(responseFromAPI.data);
+    // console.log("The response from API: ", responseFromAPI);
+  })
+  .catch(err => {
+    console.log("Error while getting the data: ", err);
+  });
+}
+
+document.getElementsByClassName('period-dates').onchange = function() {
+  const startDate = document.getElementById('start-date').value;
+  const endDate = document.getElementById('end-date').value;
+  getPeriodData(startDate, endDate);
+};
+
+document.getElementById('currency').onchange = function() {
+  const currencyType = document.getElementById('currency').value;
+  getCurrencyData(currencyType);
+};
