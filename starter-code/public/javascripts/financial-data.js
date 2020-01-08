27d0c2fa14b21948ed3998fa65391c6df9ad9ca4
Fernando Lima Borrelli <flborrelli@gmail.com>
2020-01-08 18:27:01 -0300

    Done, remaining bonus

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,21 +1,31 @@
 
 //Axios Create
-const coinDeskApi = axios.create({
-  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
-});
+// const coinDeskApi = axios.create({
+//   baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+// });
+
+
+const refreshDate = () => {
+  const fromDate = document.getElementById('fromDate').value;
+  const toDate = document.getElementById('toDate').value;
+  const dateURL = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`;
+  refreshGraph(dateURL)
+}
 
 //Axios GET request
-
-coinDeskApi
-  .get()
-  .then(responseFromAPI => {
-    const { data } = responseFromAPI;
-    console.log(data.bpi)
-    printTheChart(data.bpi); // <== call the function here where you used to console.log() the response
-  })
-  .catch(err => {
-    console.log("Error while getting the data: ", err);
-  });
+function refreshGraph(myURL){
+  axios
+    .get(myURL)
+    .then(responseFromAPI => {
+      removeErrDiv()
+      const { data } = responseFromAPI;
+      console.log(data.bpi)
+      printTheChart(data.bpi); // <== call the function here where you used to console.log() the response
+    })
+    .catch(err => {
+      createDiv()
+      console.log("Error while getting the data: ", err);
+    })};
 
 //Print Chart Function Called in our Axios GET request
 function printTheChart(stockData) {
@@ -38,4 +48,18 @@ function printTheChart(stockData) {
     }
   }); // closes chart = new Chart()
   // closes printTheChart()
-};  
+}; 
+
+function createDiv() {
+  errDiv = document.createElement("div");
+  errDiv.setAttribute("id", "error");
+  document.body.appendChild(errDiv);
+}
+
+function removeErrDiv() {
+  if (document.getElementById("error")) {
+    const error = document.getElementById("error");
+    error.parentNode.removeChild(error);
+  }
+}
+
