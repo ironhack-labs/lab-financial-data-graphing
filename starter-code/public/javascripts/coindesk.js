efe38c39a52d7629ac515e4e8546e20041e15e9d
lpolon <lpolon@gmail.com>
2020-01-08 21:05:21 -0300

    currencies imported

--- starter-code/public/javascripts/coindesk.js
+++ starter-code/public/javascripts/coindesk.js
@@ -15,8 +15,14 @@ export const getData = async (path = '/historical/close.json', queryParams = '')
   }
 };
 
-export const getCurrencies = async () => {
-  const endpoint = 'http://api.coindesk.com/v1/bpi/supported-currencies.json';
-  const data = await axios.get(endpoint);
-  console.log(data)
-}
+// TODO: não consegui fazer funcionar. CORS. Consigo fazer esse get no browser, no postman, no wget... Por que o script não consegue pegar esse json? =(
+// export const getCurrencies = async () => {
+//   try {
+//     const endpoint = 'http://api.coindesk.com/v1/bpi/supported-currencies.json';
+//     const data = await fetch(endpoint);
+//     console.log(data)
+
+//   } catch (error) {
+//     console.log(error)
+//   }
+// }
--- starter-code/public/javascripts/getData.js
+++ starter-code/public/javascripts/getData.js
@@ -1,21 +1,22 @@
 // coinDesk required endPoint
-import {getData, getCurrencies} from './coindesk.js';
+import {getData} from './coindesk.js';
 import {drawLineChart} from './drawLineChart.js';
+import curr from './supported-currencies.js';
+const {currencies} = curr;
 
 const ctx = document.getElementById('chart').getContext('2d');
 // TODO: onload
 const data = getData();
 drawLineChart(ctx, data);
 
-// getCurrencies()
 
 document.getElementById('update-chart').addEventListener('click', (event) => {
   const startDate = document.getElementById('startDate').value;
   const endDate = document.getElementById('endDate').value;
-  if(startDate && endDate) {
-    const data = getData(undefined,`?start=${startDate}&end=${endDate}`)
+  if (startDate && endDate) {
+    const data = getData(undefined, `?start=${startDate}&end=${endDate}`);
     drawLineChart(ctx, data);
   } else {
-    console.log('preencha ambos os campos!')
+    console.log('preencha ambos os campos!');
   }
 });
