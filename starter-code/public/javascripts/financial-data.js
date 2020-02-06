76440dbb58348531e98c4ee87abb598e9d21b9a4
stifamajstor <stjepan.crncic@gmail.com>
2020-02-06 16:23:09 +0100

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,52 +1,44 @@
-let graph;
-axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-  .then(response => {
-    graph = response.data.bpi;
-    let result = Object.keys(graph).map(function(key, value) {
-      return [graph(key), graph[value]];
-    });
-    console.log(result);
-  });
+const refresh = () => {
+  const fromDate = document.getElementById("from").value;
+  const toDate = document.getElementById("to").value;
 
-let ctx = document.getElementById("myChart");
-let myChart = new Chart(ctx, {
-  type: "bar",
-  data: {
-    labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-    datasets: [
-      {
-        label: "# of Votes",
-        data: [4, 4, 5, 6],
-        backgroundColor: [
-          "rgba(255, 99, 132, 0.2)",
-          "rgba(54, 162, 235, 0.2)",
-          "rgba(255, 206, 86, 0.2)",
-          "rgba(75, 192, 192, 0.2)",
-          "rgba(153, 102, 255, 0.2)",
-          "rgba(255, 159, 64, 0.2)"
-        ],
-        borderColor: [
-          "rgba(255, 99, 132, 1)",
-          "rgba(54, 162, 235, 1)",
-          "rgba(255, 206, 86, 1)",
-          "rgba(75, 192, 192, 1)",
-          "rgba(153, 102, 255, 1)",
-          "rgba(255, 159, 64, 1)"
-        ],
-        borderWidth: 1
-      }
-    ]
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`
+    )
+    .then(response => {
+      let graph = response.data.bpi;
+
+      let dates = Object.keys(graph);
+      let values = Object.values(graph);
+
+      let ctx = document.getElementById("myChart");
+      let myChart = new Chart(ctx, {
+        type: "bar",
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: "Bitcoin chart",
+              data: values,
+
+              borderWidth: 1
+            }
+          ]
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true
+                }
+              }
+            ]
           }
         }
-      ]
-    }
-  }
-});
+      });
+    });
+};
+
+window.onload = refresh();
