add4f4347173ea633c7ecf0009c7895f9b6cd081
borndead <shaulitto1@gmail.com>
2020-02-06 14:15:19 +0100

    almost done

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,33 @@
+axios
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+  .then(response => {
+    const dataBitcoin = response.data.bpi;
+    console.log(dataBitcoin);
+    const ctx = document.getElementById("myChart").getContext("2d");
+    new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: Object.keys(dataBitcoin),
+        datasets: [
+          {
+            label: "Bitcoin Price Index",
+            data: Object.values(dataBitcoin)
+          }
+        ]
+      }
+    });
+  });
+
+const input = document.getElementsByTagName("input");
+input.onchange = () => {
+  const firstDate = document.getElementById("first-date");
+  const secondDate = document.getElementById("second-date");
+
+  if (firstDate.value !== undefined && secondDate.value !== undefined) {
+    axios
+      .get(
+        `http://api.coindesk.com/v1/bpi/historical/close.json?start=${firstDate.value}&end=${secondDate.value}`
+      )
+      .then(response => {});
+  }
+};
