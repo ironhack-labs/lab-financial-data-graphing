9db2b47fa85125f3c30e837ca76e231a70bced57
FrankyMolina <fjmolinatorrejon@gmail.com>
2020-02-11 17:48:02 +0100

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,24 +1,41 @@
 
-
-
-
 const currency = `USD`;
-const dateStart = `2013-12-12`;
-const dateEnd = `2014-12-12`;
 
-const bitCoin = axios.create({
-    baseURL: `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=${dateStart}&end=${dateEnd}`
-});
+const dateStartDOMEl = document.getElementById("start-date"); 
+const dateEndDOMEl = document.getElementById("end-date");
+let dateStart = document.getElementById("start-date").value; 
+let dateEnd = document.getElementById("end-date").value;
+
+window.onload/(drawAll())
+dateStartDOMEl.addEventListener('change', ()=>{
+    drawAll()
+})
+
+dateEndDOMEl.addEventListener('change', ()=>{
+    drawAll()
+})
 
 
-bitCoin.get()
-    .then(res => {
 
-        printTheChart(res.data.bpi);
-    })
-    .catch(err => {
-        console.log('Error while getting the data: ', err);
+function drawAll(){
+    dateStart = document.getElementById("start-date").value; 
+    dateEnd = document.getElementById("end-date").value;
+    
+    const bitCoin = axios.create({
+        baseURL: `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=${dateStart}&end=${dateEnd}`
     });
+    
+    
+    bitCoin.get()
+        .then(res => {
+            printTheChart(res.data.bpi);
+        })
+        .catch(err => {
+            console.log('Error while getting the data: ', err);
+        });
+
+}
+
 
 
 
