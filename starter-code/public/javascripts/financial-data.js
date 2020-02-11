d0a2127f8977d72fce1dccbe290cffe3d4322485
Borja del Amo <borjadao@gmail.com>
2020-02-11 21:07:58 +0100

    completada iteracion2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,8 +1,36 @@
 const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
-axios.get(baseURL)
-.then(dataPayload => {
-  console.log(dataPayload.data.bpi)
-   const myKeys = Object.keys(dataPayload.data.bpi);
-   const myValues = Object.values(dataPayload.data.bpi);
-})
-.catch(err => console.log(err))
+axios
+  .get(baseURL)
+  .then(dataPayload => {
+    console.log(dataPayload.data.bpi);
+    const myKeys = Object.keys(dataPayload.data.bpi);
+    const myValues = Object.values(dataPayload.data.bpi);
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: myKeys,
+        datasets: [
+          {
+            label: "Bitcoin Price Index",
+            data: myValues,
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                stepSize: 500
+              }
+            }
+          ]
+        }
+      }
+    });
+  })
+  .catch(err => console.log(err));
+
+
