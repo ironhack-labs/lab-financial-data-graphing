499b23188aa2011f00c686274e53889a16456da1
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-02-11 21:54:00 +0100

    iteration 2

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -5,8 +5,34 @@ const coinDeskApi = axios.create({
 function getCoinInfo() {
   coinDeskApi
     .get()
-    .then(responseFromAPI => console.log("Response from API is: ", responseFromAPI.data))
-    .catch(err => console.log("Error is: ", err));
+    .then(responseFromAPI => {
+      let data = responseFromAPI.data;
+      printTheChart(data);
+    })
+    .catch(e => console.log(e));
 }
 
 getCoinInfo();
+
+function printTheChart(data) {
+  const dailyData = data["bpi"];
+
+  const dates = Object.keys(dailyData);
+  const coinValues = dates.map(date => dailyData[date]);
+
+  const ctx = document.getElementById("coinChart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Bitcoin Price Index",
+          backgroundColor: "rgba(255, 99, 132, 0.5)",
+          borderColor: "rgb(255, 99, 132)",
+          data: coinValues
+        }
+      ]
+    }
+  });
+}
