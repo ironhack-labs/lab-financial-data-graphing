7c434ecc9c9a0484ec67c631f5819c6d11dea288
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-02-11 20:12:49 +0100

    iteration-1 finish

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,9 @@
+const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
+axios
+  .get(baseURL)
+  .then(dataPayload => {
+    console.log(dataPayload.data.bpi);
+    const myKeys = Object.keys(dataPayload.data.bpi);
+    const myValues = Object.values(dataPayload.data.bpi);
+  })
+  .catch(err => console.log(err));
