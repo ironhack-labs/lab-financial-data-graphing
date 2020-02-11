fc574bf2dd78d6b021845d9243dd437a30ea7d86
mario010203 <mariojose1490@hotmail.com>
2020-02-11 22:45:58 +0100

    first iteration

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,7 @@
+const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
+axios
+  .get(apiUrl)
+  .then(responseFromAPI => console.log("The response from API: ", responseFromAPI))
+  .catch(err => console.log("Error while getting the data: ", err));
+
