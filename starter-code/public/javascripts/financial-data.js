1d63e5934ec53944c79e06d20b5490c74dc3a0e9
Grecia Patricia <grecia.busquets@gmail.com>
2020-02-11 21:07:47 +0100

    Painting chart on server, modify values to get data

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -6,5 +6,30 @@ axios
     console.log(dataPayload.data.bpi);
     const myKeys = Object.keys(dataPayload.data.bpi);
     const myValues = Object.values(dataPayload.data.bpi);
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const myChart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: myKeys,
+        datasets: [
+          {
+            label: "Bitcoin Price Index",
+            data: myValues,
+            borderWidth: 1
+          }
+        ]
+      },
+      options: {
+        scales: {
+          yAxes: [
+            {
+              ticks: {
+                stepSize: 500
+              }
+            }
+          ]
+        }
+      }
+    });
   })
   .catch(err => console.log(err));
