068df257e786ce9e87287bef2008f68cdec9b260
Jaime Hidalgo Fernandez <jaimehidalgofernandez@gmail.com>
2020-02-11 17:20:56 +0100

    done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -6,29 +6,31 @@ const timeEndElm = document.querySelector('#end')
 let timeStart, timeEnd
 
 timeStartElm.addEventListener('change', function () {
-    timeStart = document.querySelector('#start').value
+  drawAll()
 })
 timeEndElm.addEventListener('change', function () {
+    drawAll()
+})
+
+window.onload(drawAll())
+
+function drawAll(){
+    timeStart = document.querySelector('#start').value
     timeEnd = document.querySelector('#end').value
-})
-
-
-
-const apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${timeStart}end=${timeEnd}`;
-// const apiUrl = `https://www.alphavantage.co/query?function=${functionName}&symbol=${symbolName}&apikey=${key}`;
-
-
-
-
-axios
-  .get(apiUrl)
-  .then(responseFromAPI => {
-    printTheChart(responseFromAPI.data); // <== call the function here where you used to console.log() the response
-})
-  .catch(err => {
-    console.log("Error while getting the data: ", err);
-  });
-  
+   
+    const apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${timeStart}&end=${timeEnd}`;
+    console.log(apiUrl)
+    // const apiUrl = `https://www.alphavantage.co/query?function=${functionName}&symbol=${symbolName}&apikey=${key}`;
+    
+    axios
+      .get(apiUrl)
+      .then(responseFromAPI => {
+        printTheChart(responseFromAPI.data); // <== call the function here where you used to console.log() the response
+    })
+      .catch(err => {
+        console.log("Error while getting the data: ", err);
+      });
+}
 
   
 function printTheChart(data) {
@@ -52,3 +54,4 @@ function printTheChart(data) {
     }
   }); // closes chart = new Chart()
 } // closes printTheChart()
+
