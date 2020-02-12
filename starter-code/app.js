34ad2095d2650ca34c27c40bd9a6e9a0846cc173
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-12 17:51:08 +0100

    Prepare project by installing dependencies and declaring enviroment variables

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,14 +1,16 @@
-var express = require('express');
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
+require("dotenv").config();
 
-var index = require('./routes/index');
-var users = require('./routes/users');
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-var app = express();
+const index = require('./routes/index');
+const users = require('./routes/users');
+
+const app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
