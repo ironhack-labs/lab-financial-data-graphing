3178c13cde66ff7d304c21edd2d2284a24d63ef9
Ana Segarra <anasegarraber@gmail.com>
2020-02-12 14:16:08 +0100

    Style chart

--- starter-code/public/javascripts/chart.js
+++ starter-code/public/javascripts/chart.js
@@ -7,23 +7,52 @@ const printChart = (values, dates) => {
 			labels: dates,
 			datasets: [
 				{
-					label: 'Bitcoin Price Index',
+					label: 'BPI',
 					data: values,
 					backgroundColor: 'rgba(255, 179, 195, 0.3)',
-					borderColor: 'rgb(255, 99, 132)'
+					borderColor: 'rgb(255, 99, 132)',
+					pointBackgroundColor: 'rgb(255, 99, 132)'
 				}
 			]
 		},
 		options: {
 			title: {
 				display: true,
-				text: 'Bitcoin Price Index (BPI)'
+				text: 'Bitcoin Price Index (BPI)',
+				fontSize: 22,
+				fontColor: 'rgb(145, 33, 76)'
 			},
 			elements: {
 				line: {
 					tension: 0.2,
 					borderWidth: 1
 				}
+			},
+			scales: {
+				yAxes: [
+					{
+						scaleLabel: {
+							display: true,
+							labelString: 'Bitcoin values',
+							padding: { bottom: 7 },
+							fontSize: 15,
+							fontColor: 'rgb(145, 33, 76)',
+							fontStyle: 'bold'
+						}
+					}
+				],
+				xAxes: [
+					{
+						scaleLabel: {
+							display: true,
+							labelString: 'Times',
+							padding: { top: 10 },
+							fontSize: 15,
+							fontColor: 'rgb(145, 33, 76)',
+							fontStyle: 'bold'
+						}
+					}
+				]
 			}
 		}
 	});
