7d03cc3adf6bba9ed60ece790dd6b132b0e11bc0
Luismi Feijoo <luism.feijoo26@gmail.com>
2020-02-12 18:50:32 +0100

    Add chart.js

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,6 +1,6 @@
 axios({
-  method: "GET",
-  url: "http://api.coindesk.com/v1/bpi/historical/close.json",
+  method: 'GET',
+  url: 'http://api.coindesk.com/v1/bpi/historical/close.json'
 })
   .then(response => {
     // Here we can do something with the response object
@@ -9,7 +9,54 @@ axios({
     // Here we catch the error and display it
   });
 
-document.querySelector('#button').onclick = function () {
-  const response = axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-  response.then(res => console.log(res))
+document.querySelector('#button').onclick = function() {
+  const response = axios.get(
+    `http://api.coindesk.com/v1/bpi/historical/close.json`
+  );
+  response.then(res => console.log(res));
 };
+
+const ctx = document.getElementById('chart').getContext('2d');
+
+const chart = new Chart(ctx, {
+  type: 'line',
+  data: {
+    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    datasets: [
+      {
+        label: '# of Votes',
+        data: [12, 19, 3, 5, 2, 3],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }
+    ]
+  },
+  options: {
+    responsive: true,
+    maintainAspectRatio: false,
+    scales: {
+      yAxes: [
+        {
+          ticks: {
+            beginAtZero: true
+          }
+        }
+      ]
+    }
+  }
+});
