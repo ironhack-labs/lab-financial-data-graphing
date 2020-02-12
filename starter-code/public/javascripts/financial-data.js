a4353572bcf1604f817abfbea3c6d1b60db682a1
militroncho7 <victor@MacBook-Air-de-Victor.local>
2020-02-12 18:59:20 +0100

    happy coding ♥️

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,83 +1,32 @@
 const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
 axios
-  .get(baseURL)
-  .then(dataPayload => {
-    console.log(dataPayload.data);
-    printTheChart(dataPayload.data);
-  })
-  .catch(err => console.log(err));
+    .get(baseURL)
+    .then(dataPayload => {
+        console.log(dataPayload.data);
+        printChart(dataPayload.data);
+    })
+    .catch(err => console.log(err));
 
-function printTheChart(data) {
-  const dailyData = data["bpi"];
-  const myKeys = Object.keys(dailyData);
-  const myValues = Object.values(dailyData);
-  console.log(myValues);
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const chart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: myKeys,
-      datasets: [
-        {
-          label: "Value",
-          backgroundColor: "rgba(247,255,255, .7)",
-          borderColor: "rgb(247,0,255)",
-          fill: true,
-          data: myValues
+function printChart(data) {
+    const dailyData = data["bpi"];
+    const myKeys = Object.keys(dailyData);
+    const myValues = Object.values(dailyData);
+    console.log(myValues);
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: myKeys,
+            datasets: [
+                {
+                    label: "Value",
+                    backgroundColor: "rgba(247,255,255, .5)",
+                    borderColor: "rgb(247,0,255)",
+                    fill: true,
+                    data: myValues
+                }
+            ]
         }
-      ]
-    }
-  });
+    });
 }
-
-
-// const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
-
-// axios
-//     .get(baseURL)
-//     .then(dataPayload => {
-//         console.log(dataPayload.data.bpi);
-//         const myKeys = Object.keys(dataPayload.data.bpi);
-//         const myValues = Object.values(dataPayload.data.bpi);
-//         console.log(myKeys);
-//         console.log(myValues);
-//     })
-//     .catch(err => console.log(err));
-
-// const ctx = document.getElementById('myChart').getContext('2d');
-// const myChart = new Chart(ctx, {
-//     type: 'line',
-//     data: {
-//         labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-//         datasets: [{
-//             label: '# of Votes',
-//             data: [12, 19, 3, 5, 2, 3],
-//             backgroundColor: [
-//                 'rgba(255, 99, 132, 0.2)',
-//                 'rgba(54, 162, 235, 0.2)',
-//                 'rgba(255, 206, 86, 0.2)',
-//                 'rgba(75, 192, 192, 0.2)',
-//                 'rgba(153, 102, 255, 0.2)',
-//                 'rgba(255, 159, 64, 0.2)'
-//             ],
-//             borderColor: [
-//                 'rgba(255, 99, 132, 1)',
-//                 'rgba(54, 162, 235, 1)',
-//                 'rgba(255, 206, 86, 1)',
-//                 'rgba(75, 192, 192, 1)',
-//                 'rgba(153, 102, 255, 1)',
-//                 'rgba(255, 159, 64, 1)'
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {
-//         scales: {
-//             yAxes: [{
-//                 ticks: {
-//                     beginAtZero: true
-//                 }
-//             }]
-//         }
-//     }
-// });
