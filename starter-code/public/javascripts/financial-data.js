c58e2f207b5286dbe275bd2bd215a7d4aff7215e
Borja del Amo <borjadao@gmail.com>
2020-02-13 21:04:56 +0100

    iteracion3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,36 +1,44 @@
-const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
-axios
-  .get(baseURL)
-  .then(dataPayload => {
-    console.log(dataPayload.data.bpi);
-    const myKeys = Object.keys(dataPayload.data.bpi);
-    const myValues = Object.values(dataPayload.data.bpi);
-    const ctx = document.getElementById("myChart").getContext("2d");
-    const myChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: myKeys,
-        datasets: [
-          {
-            label: "Bitcoin Price Index",
-            data: myValues,
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                stepSize: 500
-              }
-            }
-          ]
+const dateFromId = document.querySelector("#dateFrom");
+const dateToId = document.querySelector("#dateTo");
+function chartMaster() {
+    const dateFrom = dateFromId.value;
+    const dateTo = dateToId.value;
+    const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`;
+    function getDataAndPrint(baseURL) {
+        axios
+            .get(baseURL)
+            .then(dataPayload => {
+                //console.log(dataPayload.data.bpi);
+                printChart(dataPayload.data);
+            })
+            .catch(err => console.log(err));
+
+        function printChart(data) {
+            const dailyData = data["bpi"];
+            const myKeys = Object.keys(dailyData);
+            const myValues = myKeys.map(value => {
+                return dailyData[value];
+            });
+            const ctx = document.getElementById("myChart").getContext("2d");
+            const chart = new Chart(ctx,{
+                type: "line",
+                data:{
+                    labels: myKeys,
+                    datasets:[
+                        {
+                            label: "Value",
+                            backgroundColor: "rgba(33, 136, 69, .1)",
+                            borderColor: "rgb (159, 229, 179)",
+                            fill: true,
+                            data: myValues
+                        }
+                    ]
+                }
+            });
         }
-      }
-    });
-  })
-  .catch(err => console.log(err));
-
-
+    }
+getDataAndPrint(baseURL);
+}
+dateFromId.addEventListener("change", chartMaster);
+dateToId.addEventListener("change", chartMaster);
+chartMaster();
