f71b7aa971f4f7560f1afb16f5d5cf610fdb507f
Gonzalo Hernández <gonzalohernandez@icloud.com>
2020-02-13 10:40:24 +0100

    iteration 4

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,42 +1,66 @@
+//const bodyParser = require("body-parser");
+
+let chart;
+
 // get request with Axios
-function getCoinInfo(start = "2020-02-01", end = "2020-02-11") {
+function getCoinInfo(start, end, currency, cb) {
   axios
     .get("http://api.coindesk.com/v1/bpi/historical/close.json", {
       params: {
         start,
-        end
+        end,
+        currency
       }
     })
     .then(responseFromAPI => {
-      let data = responseFromAPI.data;
-      printTheChart(data);
+      const dailyData = responseFromAPI.data["bpi"];
+      const dates = Object.keys(dailyData);
+      const coinValues = dates.map(date => dailyData[date]);
+      cb(dates, coinValues);
     })
     .catch(e => console.log(e));
 }
 
 // print chart
-function printTheChart(data) {
+function printTheChart(label, data) {
   Chart.defaults.global.defaultFontColor = "rgb(169, 169, 169)";
-  const dailyData = data["bpi"];
-
-  const dates = Object.keys(dailyData);
-  const coinValues = dates.map(date => dailyData[date]);
-
   const ctx = document.getElementById("coinChart").getContext("2d");
-  const chart = new Chart(ctx, {
+  chart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: dates,
+      labels: label,
       datasets: [
         {
           label: "Bitcoin Price Index",
           backgroundColor: "rgba(30, 144, 255, 0.5)",
           borderColor: "rgb(30, 144, 255)",
-          data: coinValues
+          data: data
         }
       ]
     }
   });
 }
 
-getCoinInfo();
+// update chart values
+function updateChart(label, data) {
+  chart.data.labels = label;
+  chart.data.datasets.forEach(dataset => {
+    dataset.data = data;
+  });
+  chart.update();
+}
+
+// get dates values and refresh the chart
+const inputs = document.querySelectorAll(".input");
+const dateFrom = document.querySelector("#dateFrom");
+const dateTo = document.querySelector("#dateTo");
+inputs.forEach(input => {
+  input.addEventListener("input", event => {
+    // api request and call the update function
+    if (dateFrom.value && dateTo.value)
+      getCoinInfo(dateFrom.value, dateTo.value, currency.value, updateChart);
+    else getCoinInfo(null, null, currency.value, updateChart);
+  });
+});
+
+getCoinInfo(null, null, "USD", printTheChart); // api request and print the chart the first time
