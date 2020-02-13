6cabab71ccd109395954e49f4f0320de2a16ab55
Ana Segarra <anasegarraber@gmail.com>
2020-02-13 02:27:23 +0100

    Include dates filter

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,13 +1,33 @@
-const callApi = async () => {
+const callApi = async (initialDate, endDate) => {
 	try {
-		const response = await axios.get('https://api.coindesk.com/v1/bpi/historical/close.json');
+		let response;
+		if (initialDate && endDate) {
+			response = await axios.get(
+				`https://api.coindesk.com/v1/bpi/historical/close.json?start=${initialDate}&end=${endDate}`
+			);
+		} else {
+			response = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`);
+		}
 		const values = Object.values(response.data.bpi);
 		const dates = Object.keys(response.data.bpi);
-
+		console.log(`data from ${initialDate} to ${endDate}`);
 		printChart(values, dates);
 	} catch (error) {
 		console.log(error);
 	}
 };
+callApi();
 
-window.onload = callApi;
+let initialDate = document.getElementById('initial-date').value;
+let endDate = document.getElementById('last-date').value;
+
+function dateListener(id) {
+	document.getElementById(id).addEventListener('change', e => {
+		e.target.name === 'initial-date' ? (initialDate = e.target.value) : (endDate = e.target.value);
+		callApi(initialDate, endDate);
+	});
+}
+
+// Set up event listeners
+dateListener('initial-date');
+dateListener('last-date');
