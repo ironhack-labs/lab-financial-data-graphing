79a5ca71dde4f57503d30c65fe34050ad8657926
Pilar <p.garciacampo@gmail.com>
2020-02-13 20:02:51 +0100

    Add iteration 1

---
+++ starter-code/public/javascripts/financial-data.js
@@ -0,0 +1,18 @@
+/*jshint esversion: 6 */
+const restCoinDeskApi = axios.create({
+  baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+});
+
+function getCoinDeskInfo() {
+  restCoinDeskApi
+    .get()
+    .then(responseFromAPI =>
+      console.log("HEY! Response from API is: ", responseFromAPI.data)
+    )
+    .catch(err => console.log("Error is: ", err));
+}
+
+document.getElementById("theButton").onclick = function() {
+  //const country = document.getElementById("theInput").value;
+  getCoinDeskInfo();
+};
