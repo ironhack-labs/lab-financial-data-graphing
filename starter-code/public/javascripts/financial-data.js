9bb55439622fc4e15a0110bef49d8bed9893b1bf
Ignacio Garcia Barrero <igarciabarrero@outlook.com>
2020-02-13 20:35:05 +0100

    Getting data from API and displayin it in console

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,13 +1,14 @@
-axios({
-  method: "GET",
-  url: "/http://api.coindesk.com/v1/bpi/historical/close.json",
-  params: "URL parameters to be sent with the request"
-})
-  .then(response => {
-    console.log(response)
-    // Here we can do something with the response object
-  })
-  .catch(err => {
-    console.log(error)
-    // Here we catch the error and display it
-  });
+const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
+axios
+  .get(apiUrl)
+  .then(responseFromAPI => console.log("The response from API: ", responseFromAPI))
+  .catch(err => console.log("Error while getting the data: ", err));
+
+
+
+
+
+
+
+
