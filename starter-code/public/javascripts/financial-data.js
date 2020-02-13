35bd3c8cb17ed72b9138cf25dcab6cae36af97f7
David Garcia Bravo <54409809+dgarciabravo13@users.noreply.github.com>
2020-02-13 17:57:41 +0100

    print dates and bitCoins in the chart

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -2,29 +2,32 @@ const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 
 axios
   .get(apiUrl)
-  .then(responseFromAPI =>
-    console.log("The response from API: ", responseFromAPI)
-  )
-  .catch(err => console.log("Error while getting the data: ", err));
+  .then(responseFromAPI => {
+    console.log("The response from API: ", responseFromAPI.data.bpi);
+    const chartDates = Object.keys(responseFromAPI.data.bpi);
+    const chartCoin = Object.values(responseFromAPI.data.bpi);
 
-var ctx = document.getElementById("myChart").getContext("2d");
-var chart = new Chart(ctx, {
-  // The type of chart we want to create
-  type: "line",
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: "line",
+   // The data for our dataset
+    data: {
+      labels: chartDates,
+      datasets: [
+        {
+          label: "BitCoin",
+          backgroundColor: "teal",
+          borderColor: "lightblue",
+          data: chartCoin
+        }
+      ]
+    },
 
-  // The data for our dataset
-  data: {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
-    datasets: [
-      {
-        label: "My First dataset",
-        backgroundColor: "rgb(255, 99, 132)",
-        borderColor: "rgb(255, 99, 132)",
-        data: [0, 10, 5, 2, 20, 30, 45]
-      }
-    ]
-  },
+    // Configuration options go here
+    options: {}
+  });
 
-  // Configuration options go here
-  options: {}
-});
+  }).catch(err => console.log("Error while getting the data: ", err));
+
+  
