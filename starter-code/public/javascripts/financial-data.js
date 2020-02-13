232532f9a3f119180efbf3a3952eb000fe984218
Grecia Patricia <grecia.busquets@gmail.com>
2020-02-13 20:20:33 +0100

    Finished iteration3, there is a problem after selecting the dates. When I move the mouse over the chart the data changes to default and does not recognize the dates

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,35 +1,51 @@
-const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
+const dateFromDomEl = document.querySelector("#fromDate");
+const dateToDomEl = document.querySelector("#toDate");
 
-axios
-  .get(baseURL)
-  .then(dataPayload => {
-    console.log(dataPayload.data.bpi);
-    const myKeys = Object.keys(dataPayload.data.bpi);
-    const myValues = Object.values(dataPayload.data.bpi);
-    const ctx = document.getElementById("myChart").getContext("2d");
-    const myChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: myKeys,
-        datasets: [
-          {
-            label: "Bitcoin Price Index",
-            data: myValues,
-            borderWidth: 1
-          }
-        ]
-      },
-      options: {
-        scales: {
-          yAxes: [
-            {
-              ticks: {
-                stepSize: 500
+function chartMaster() {
+  const dateFrom = dateFromDomEl.value;
+  const dateTo = dateToDomEl.value;
+  const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`;
+
+  function getDataAndPrint(baseURL) {
+    axios
+      .get(baseURL)
+      .then(dataPayload => {
+        console.log(dataPayload.data.bpi);
+        const myKeys = Object.keys(dataPayload.data.bpi);
+        const myValues = myKeys.map(date => {
+          return dataPayload.data.bpi[date];
+        });
+
+        const ctx = document.getElementById("myChart").getContext("2d");
+        const myChart = new Chart(ctx, {
+          type: "line",
+          data: {
+            labels: myKeys,
+            datasets: [
+              {
+                label: "Bitcoin Price Index",
+                data: myValues,
+                borderWidth: 1
               }
+            ]
+          },
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    stepSize: 500
+                  }
+                }
+              ]
             }
-          ]
-        }
-      }
-    });
-  })
-  .catch(err => console.log(err));
+          }
+        });
+      })
+      .catch(err => console.log(err));
+  }
+  getDataAndPrint(baseURL);
+}
+dateFromDomEl.addEventListener("change", chartMaster);
+dateToDomEl.addEventListener("change", chartMaster);
+chartMaster();
