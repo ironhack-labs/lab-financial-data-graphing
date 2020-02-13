bc6e38e42b291d737a172b2e3ee86f40c200d1f1
Ruben Vaquero <rvaquero@elpublicamaleon.com>
2020-02-13 20:00:10 +0100

    iteration 3 end

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,31 +1,50 @@
-const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
-axios
-  .get(baseURL)
-  .then(dataPayload => {
-    console.log(dataPayload.data);
-    printTheChart(dataPayload.data);
-  })
-  .catch(err => console.log(err));
+const dateFromId = document.querySelector("#dateFrom");
+const dateToId = document.querySelector("#dateTo");
 
-function printTheChart(data) {
-  const dailyData = data["bpi"];
-  const myKeys = Object.keys(dailyData);
-  const myValues = Object.values(dailyData);
-  console.log(myValues);
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const chart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: myKeys,
-      datasets: [
-        {
-          label: "Value",
-          backgroundColor: "rgba(247,255,255, .7)",
-          borderColor: "rgb(247,0,255)",
-          fill: true,
-          data: myValues
-        }
-      ]
-    }
-  });
+function chartMaster() {
+  const dateFrom = dateFromId.value;
+  const dateTo = dateToId.value;
+  const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}`;
+
+  function getDataAndPrint(baseURL) {
+    axios
+      .get(baseURL)
+      .then(dataPayload => {
+        console.log(dataPayload.data);
+        printTheChart(dataPayload.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function printTheChart(data) {
+    const dailyData = data["bpi"];
+    const myKeys = Object.keys(dailyData);
+    const myValues = myKeys.map(value => {
+      return dailyData[value];
+    });
+    const ctx = document.getElementById("myChart").getContext("2d");
+    const chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: myKeys,
+        datasets: [
+          {
+            label: "Value",
+            backgroundColor: "rgba(247,255,255, .7)",
+            borderColor: "rgb(247,0,255)",
+            fill: true,
+            data: myValues
+          }
+        ]
+      }
+    });
+  }
+
+  getDataAndPrint(baseURL);
 }
+
+dateFromId.addEventListener("change", chartMaster);
+
+dateToId.addEventListener("change", chartMaster);
+
+chartMaster();
