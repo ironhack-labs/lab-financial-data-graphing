f35a7565a41e7bcf94588001d5af3e4116f85e3b
Ana Segarra <anasegarraber@gmail.com>
2020-02-13 14:15:43 +0100

    Include currency filter

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,16 +1,18 @@
-const callApi = async (initialDate, endDate) => {
+const callApi = async (initialDate, endDate, currency = 'USD') => {
 	try {
 		let response;
 		if (initialDate && endDate) {
 			response = await axios.get(
-				`https://api.coindesk.com/v1/bpi/historical/close.json?start=${initialDate}&end=${endDate}`
+				`https://api.coindesk.com/v1/bpi/historical/close.json?start=${initialDate}&end=${endDate}&currency=${currency}`
 			);
+			console.log(`data from ${initialDate} to ${endDate} in ${currency}`);
 		} else {
-			response = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json`);
+			response = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?`);
+			console.log('data from last 31 days');
 		}
 		const values = Object.values(response.data.bpi);
 		const dates = Object.keys(response.data.bpi);
-		console.log(`data from ${initialDate} to ${endDate}`);
+		console.log('data:', response.data);
 		printChart(values, dates);
 	} catch (error) {
 		console.log(error);
@@ -20,13 +22,20 @@ callApi();
 
 let initialDate = document.getElementById('initial-date').value;
 let endDate = document.getElementById('last-date').value;
+let currencyInput = document.getElementById('currency').value;
 
 function dateListener(id) {
 	document.getElementById(id).addEventListener('change', e => {
 		e.target.name === 'initial-date' ? (initialDate = e.target.value) : (endDate = e.target.value);
-		callApi(initialDate, endDate);
+		currencyInput === '' ? callApi(initialDate, endDate) : callApi(initialDate, endDate, currencyInput);
 	});
 }
+
+document.getElementById('currency').addEventListener('change', e => {
+	currencyInput = e.target.value;
+	console.log('currency changed to', e.target.value);
+	callApi(initialDate, endDate, currencyInput);
+});
 
 // Set up event listeners
 dateListener('initial-date');
