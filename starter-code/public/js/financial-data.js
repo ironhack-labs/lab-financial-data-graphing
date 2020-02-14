8798b5d2cd7f35ab51cf71ac40d3a401847bc738
vctorChamizo <victor.chamizo96@gmail.com>
2020-02-14 20:36:57 +0100

    Add all exersice and bonus

--- starter-code/public/js/financial-data.js
+++ starter-code/public/js/financial-data.js
@@ -1,30 +1,62 @@
+let axiosInstance, ctx, chart;
+const dateFrom = document.getElementById("from-input");
+const dateTo = document.getElementById("to-input");
+const currency = document.getElementById("currency-input");
+
 window.onload = () => {
-    axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
-      .then(res => {
-        res = res.data.bpi
-        const keys = Object.keys(res);
-        const values = keys.map(k => res[k])
-        generateChart(keys, values);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
+  ctx = document.getElementById("financial-chart").getContext("2d");
+  axiosInstance = axios.create({
+    baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
+  });
+
+  requestAPI(null, null, null, loadChart);
+};
+
+async function requestAPI(start, end, currency, fn) {
+  try {
+    console.log(start)
+    console.log(end)
+    const response = await axiosInstance.get("", { params: { start, end, currency } });
+    const keys = Object.keys(response.data.bpi);
+    const values = keys.map(k => response.data.bpi[k]);
+    putExtremPoints(Math.min(...values), Math.max(...values))
+    fn(keys, values);
+  } catch (error) {
+    console.log("ERROR: ", error);
+  }
 }
 
-function generateChart(labels, data) {
-    const ctx = document.getElementById('financial-chart').getContext('2d');
-    new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: labels,
-            datasets: [{
-                data: data,
-                backgroundColor: 'rgba(28,55,92, 0.4)',
-                borderColor: 'rgba(28,55,92)',
-                borderWidth: 2
-            }]
+function loadChart(labels, data) {
+  chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: "Bitcoin Price",
+          data: data,
+          backgroundColor: "rgba(28,55,92, 0.4)",
+          borderColor: "rgba(28,55,92)",
+          borderWidth: 2
         }
-    });
-
+      ]
+    }
+  });
 }
 
+function updateChart(labels, data) {
+  chart.data.labels = labels;
+  chart.data.datasets[0].data = data;
+  chart.update();
+}
+
+function putExtremPoints(min, max) {
+  document.getElementById("min-val").innerHTML = min;
+  document.getElementById("max-val").innerHTML = max;
+}
+
+function listeners() {
+  console.log("hola")
+  if (dateTo.value && dateFrom.value) requestAPI(dateFrom.value, dateTo.value, currency.value, updateChart) 
+  else requestAPI(null, null, currency.value, updateChart) 
+}
