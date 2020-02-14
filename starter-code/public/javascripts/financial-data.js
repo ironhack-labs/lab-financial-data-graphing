5d907803a713767acf229945db301b59595ee5ed
Borja Domínguez <borja.d.atienza@gmail.com>
2020-02-14 21:02:34 +0100

    3º Interation DONE

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,63 +1,58 @@
-const baseURL = "http://api.coindesk.com/v1/bpi/historical/close.json";
+let chart;
+const dateFrom = document.querySelector("#dateFrom");
+const dateTo = document.querySelector("#dateTo");
 
-axios
-  .get(baseURL)
-  .then(showData => {
-    console.log(showData.data["bpi"]);
-    showGraphChart(showData);
-  })
-  .catch(err => console.log(err));
+console.log(dateFrom, dateTo);
 
-function showGraphChart(data) {
-  const dataApi = data["bpi"];
-  const keys = Object.keys(dataApi);
-  const values = Object.values(dataApi);
+function goDataShow() {
+  const dateFromValue = dateFrom.value;
+  const dateToValue = dateTo.value;
+  const baseURL = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFromValue}&end=${dateToValue}`;
 
-  const ctx = document.getElementById("myChart").getContext("2d");
-  const chart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: keys,
-      datasets: [
-        {
-          label: "Value",
-          backgroundColor: "rgb(255, 154, 0)",
-          borderColor: "rgb(255, 255, 255)",
-          fill: true,
-          data: values
-        }
-      ]
-    }
-  });
+  function getDataAndShow(baseURL) {
+    axios
+      .get(baseURL)
+      .then(showData => {
+        console.log(showData.data["bpi"]);
+        showGraphChart(showData.data);
+      })
+      .catch(err => console.log(err));
+  }
+
+  function showGraphChart(data) {
+    const dataApi = data["bpi"];
+    const keys = Object.keys(dataApi);
+    const values = Object.values(dataApi);
+    console.log("hola");
+
+    const ctx = document.getElementById("theChart").getContext("2d");
+    chart = new Chart(ctx, {
+      type: "line",
+      data: {
+        labels: keys,
+        datasets: [
+          {
+            label: "Value",
+            backgroundColor: "rgb(255, 154, 0)",
+            borderColor: "rgb(255, 255, 255)",
+            fill: true,
+            data: values
+          }
+        ]
+      }
+    });
+  }
+  getDataAndShow(baseURL);
 }
 
-// axios
-//   .get(baseURL)
-//   .then(dataPayload => {
-//     console.log(dataPayload.data);
-//     printTheChart(dataPayload.data);
-//   })
-//   .catch(err => console.log(err));
+dateFrom.addEventListener("change", () => {
+  chart.destroy();
+  goDataShow();
+});
 
-// function printTheChart(data) {
-//   const dailyData = data["bpi"];
-//   const myKeys = Object.keys(dailyData);
-//   const myValues = Object.values(dailyData);
-//   console.log(myValues);
-//   const ctx = document.getElementById("myChart").getContext("2d");
-//   const chart = new Chart(ctx, {
-//     type: "line",
-//     data: {
-//       labels: myKeys,
-//       datasets: [
-//         {
-//           label: "Value",
-//           backgroundColor: "rgba(247,255,255, .7)",
-//           borderColor: "rgb(247,0,255)",
-//           fill: true,
-//           data: myValues
-//         }
-//       ]
-//     }
-//   });
-// }
+dateTo.addEventListener("change", () => {
+  chart.destroy();
+  goDataShow();
+});
+
+goDataShow();
