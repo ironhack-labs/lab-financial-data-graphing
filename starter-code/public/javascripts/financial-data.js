d28ace61eebd4ffe35ca0f35fd01d114c6f8a726
alberto gomez marin <albertogomezmarin@albertos-MacBook-Air.local>
2020-02-14 21:30:08 +0100

    iteration 2 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,3 +1,35 @@
 const coinDeskApi = axios.create({
   baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
 });
+function getHistorialInfo() {
+  coinDeskApi
+    .get()
+    .then(responseFromAPI => {
+      printTheChart(responseFromAPI.data);
+    })
+    .catch(err => console.log("Error is: ", err));
+}
+getHistorialInfo();
+
+function printTheChart(data) {
+  const dailyData = data["bpi"];
+
+  const dates = Object.keys(dailyData);
+  const coin = dates.map(date => dailyData[date]);
+
+  const ctx = document.getElementById("historialChart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: dates,
+      datasets: [
+        {
+          label: "Bitcoin Prices",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: coin
+        }
+      ]
+    }
+  });
+}
