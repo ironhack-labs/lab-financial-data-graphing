8417315cb42aea502aa86dddf1f755c785ef019d
carballo <carballet@gmail.com>
2020-02-14 19:50:32 +0100

    Iteration 2 Done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,11 +1,10 @@
 
 
-function getData(apiCoindesk){
+function getDataPrintChart(){
   axios
   .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-  .then(response => {
-      console.log(response.data);
-    // Aquí tendría que pintar la grafica
+  .then(responseCoin => {
+      printChart(responseCoin.data) // Llamo a la función que dibuja la gráfica porque meterlo todo aquí es un lío de la leche
   })
   .catch(err => {
     console.log("Error recibiendo los datos: ", err);
@@ -13,5 +12,33 @@ function getData(apiCoindesk){
 }
 
 
-// Llamo a getData en la Iteracion #1 para ver por consola que esto chuta pero tiene pinta de que esto se va fuera
-getData()
+// Llamo a getData para que pida los datos y los pinte
+getDataPrintChart()
+
+
+// Pinto la gráfica
+
+function printChart(data) {
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const dayData = data["bpi"]; // Los datos están dentro de un objeto llamado bpi fecha:precio
+const priceDate = Object.keys(dayData); // Saco la fecha que es la key de cada cosa para usarla de labels
+const price = Object.values(dayData); // Saco el precio que es el value de cada cosa para usarla en data
+
+const myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: priceDate,
+      datasets: [
+        {
+          label: "BTC",
+          backgroundColor: "rgb(68, 166, 199)",
+          borderColor: "rgb(103, 199, 68)",
+          fill: false,
+          data: price
+        }
+      ]
+    }
+  }); 
+}
