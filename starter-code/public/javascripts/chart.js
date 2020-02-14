b5978cb0cc019b145ba17e6e0d9001a6bb41ccc6
Román Méndez <mendezbeck@gmail.com>
2020-02-14 21:04:43 +0100

    working on destroying chart to repaint

--- starter-code/public/javascripts/chart.js
+++ starter-code/public/javascripts/chart.js
@@ -1,5 +1,5 @@
 const ctx = document.getElementById("myChart").getContext("2d");
-const myChart = (labels, data, title) => {
+let myChart = (labels, data, title) => {
   new Chart(ctx, {
     type: "line",
     data: {
@@ -8,11 +8,50 @@ const myChart = (labels, data, title) => {
         {
           label: title,
           data: data,
-          backgroundColor: ["rgba(255, 99, 132, 0.2)", "rgba(54, 162, 235, 0.2)", "rgba(255, 206, 86, 0.2)", "rgba(75, 192, 192, 0.2)", "rgba(153, 102, 255, 0.2)", "rgba(255, 159, 64, 0.2)"],
-          borderColor: ["rgba(255, 99, 132, 1)", "rgba(54, 162, 235, 1)", "rgba(255, 206, 86, 1)", "rgba(75, 192, 192, 1)", "rgba(153, 102, 255, 1)", "rgba(255, 159, 64, 1)"],
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(255, 99, 132, 1)"],
           borderWidth: 1
         }
       ]
     }
   });
+};
+
+let date = new Date();
+const today = date.toJSON().slice(0, 10);
+date.setDate(date.getDate() - 30);
+const minusThirty = date.toJSON().slice(0, 10);
+
+const chartHistoricalBPI = (start = minusThirty, end = today) => {
+  axios
+    .get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${start}&end=${end}`)
+    .then(res => {
+      const chartDates = Object.keys(res.data.bpi);
+      const chartValues = Object.values(res.data.bpi);
+      return myChart(chartDates, chartValues, "Title");
+    })
+    .catch(err => console.log("ERROR", err));
+};
+
+document.addEventListener("DOMContentLoaded", chartHistoricalBPI());
+
+const dateFrom = document.getElementById("dateFrom");
+const dateTo = document.getElementById("dateTo");
+const dateFromValue = (dateFrom.onchange = () => {
+  dateFrom.value;
+});
+const dateToValue = (dateTo.onchange = () => {
+  dateTo.value;
+});
+
+dateFrom.setAttribute("value", minusThirty);
+dateTo.setAttribute("value", today);
+
+dateFrom.onchange = () => {
+  myChart.destroy();
+  chartHistoricalBPI(dateFrom.value);
+};
+dateTo.onchange = () => {
+  myChart.destroy();
+  chartHistoricalBPI(minusThirty, dateTo.value);
 };
