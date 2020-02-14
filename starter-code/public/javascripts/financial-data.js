7c8929f4e602e59f3b97f9f1b10d2172342ef313
Ana Segarra <anasegarraber@gmail.com>
2020-02-14 14:13:27 +0100

    Add date restrictions using momentjs

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,7 +1,7 @@
 // Global variables
 const baseURL = 'https://api.coindesk.com/v1/bpi/historical/close.json?';
-let initialDate = document.getElementById('initial-date').value;
-let endDate = document.getElementById('last-date').value;
+let initialDateInput = document.getElementById('initial-date').value;
+let endDateInput = document.getElementById('last-date').value;
 let currencyInput = document.getElementById('currency').value;
 
 // Fetch API data
@@ -11,9 +11,16 @@ const callApi = async (initialDate, endDate, currency = 'USD') => {
 		if (initialDate && endDate) {
 			response = await axios.get(`${baseURL}start=${initialDate}&end=${endDate}&currency=${currency}`);
 			console.log(`data from ${initialDate} to ${endDate} in ${currency}`);
-		} else {
+		} else if (!initialDate && !endDate) {
 			response = await axios.get(`${baseURL}?currency=${currency}`);
-			console.log('data from last 31 days');
+			console.log(`data from last 31 days`);
+		} else {
+			const fixEndDate = fixDate('end-date', initialDateInput);
+			const fixInitialDate = fixDate('initial-date', endDateInput);
+			response = await axios.get(
+				`${baseURL}start=${initialDate || fixInitialDate}&end=${endDate || fixEndDate}&currency=${currency}`
+			);
+			console.log(`data from ${initialDate || fixInitialDate} to ${endDate || fixEndDate}`);
 		}
 		const values = Object.values(response.data.bpi);
 		const dates = Object.keys(response.data.bpi);
@@ -36,14 +43,24 @@ function handleChange(id) {
 	document.getElementById(id).addEventListener('change', e => {
 		e.target.name === 'currency'
 			? (currencyInput = e.target.value)
-			: e.target.name === 'initial-date' ? (initialDate = e.target.value) : (endDate = e.target.value);
-		currencyInput === '' ? callApi(initialDate, endDate) : callApi(initialDate, endDate, currencyInput);
+			: e.target.name === 'initial-date' ? (initialDateInput = e.target.value) : (endDateInput = e.target.value);
+		currencyInput === ''
+			? callApi(initialDateInput, endDateInput)
+			: callApi(initialDateInput, endDateInput, currencyInput);
 	});
 }
 
 function printValues(variable, id, apiValues, currency) {
 	id === 'max' ? (variable = Math.max(...apiValues)) : (variable = Math.min(...apiValues));
 	document.getElementById(id).textContent = `${variable} ${currency}`;
+}
+
+function fixDate(id, date) {
+	if (id === 'end-date') {
+		return moment(date).add(31, 'days').format('YYYY-MM-DD');
+	} else {
+		return moment(date).subtract(31, 'days').format('YYYY-MM-DD');
+	}
 }
 
 // Set up event listeners
