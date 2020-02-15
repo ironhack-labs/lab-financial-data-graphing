9bc309300b275aed7b43cd662f38084beb6e898f
Iván Herranz <ihcuesta@gmail.com>
2020-02-15 09:27:22 +0100

    Iteration 3: done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,59 +1,66 @@
-let input1 = document.getElementById("date1");
-let input2 = document.getElementById("date2");
+let dateFromId = document.getElementById("date1");
+let dateToId = document.getElementById("date2");
+let currencyId = document.getElementById("currency");
+let chart;
 
-let date1 = document.getElementById("date1").value;
-let date2 = document.getElementById("date2").value;
 
-const restCoinDeskApi = axios.create({
-    baseURL: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${date1}&end=${date2}`
-  });
-  
-  function getCoinDeskInfo() {
+const mainChart = () => {
+  let dateFrom = dateFromId.value;
+  let dateTo = dateToId.value;
+  let currency = currencyId.value;
+  const restCoinDeskApi = axios.create({
+    baseURL: `http://api.coindesk.com/v1/bpi/historical/close.json?start=${dateFrom}&end=${dateTo}&currency=${currency}`
+  })
+
+  function getCoinDeskInfo(restCoinDeskApi) {
     restCoinDeskApi
-      .get()
-      .then(responseFromAPI => printTheChart(responseFromAPI.data))
-      .catch(err => console.log("Error is: ", err));
+    .get()
+    .then(responseFromAPI => printTheChart(responseFromAPI.data))
+    .catch(err => console.log("Error is: ", err));
   }
-  
-  
 
-// Print function
+  // Print function
   function printTheChart(data) {
     const bpi = data["bpi"];
-  
+
     const axisX = Object.keys(bpi);
     const axisY = axisX.map(e => bpi[e]);
-  
+
     const ctx = document.getElementById("myChart").getContext("2d");
-    const chart = new Chart(ctx, {
+    // if (chart) chart.destroy();
+    chart = new Chart(ctx, {
       type: "line",
       data: {
         labels: axisX,
-        datasets: [
-          {
-            label: "Data",
-            backgroundColor: "#FF0000",
-            borderColor: "rgb(255, 99, 132)",
-            data: axisY
-          }
-        ]
+        datasets: [{
+          label: "Data",
+          backgroundColor: "#FF0000",
+          borderColor: "rgb(255, 99, 132)",
+          data: axisY
+        }]
       }
     }); // closes chart = new Chart()
-    
+
   } // closes printTheChart()
+
+  getCoinDeskInfo(restCoinDeskApi);
+
+}
 
 //   document.getElementById("theButton").onclick = function() {
 //     getCoinDeskInfo();
 //   };
 
-  
-  input1.addEventListener("change", function() {
-   
-    getCoinDeskInfo();
+dateFromId.addEventListener("change", function () {
+  chart.destroy();
+  mainChart();
 });
 
-input2.addEventListener("change", function() {
-    
-    getCoinDeskInfo();
+dateToId.addEventListener("change", function () {
+  chart.destroy();
+  mainChart();
 });
 
+
+
+mainChart();
