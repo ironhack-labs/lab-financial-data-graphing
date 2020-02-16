cf6b7d9608e2f94f77c7186dae4d56e349b132c4
Kevin Medina <kmedinakm@hotmail.com>
2020-02-15 21:59:22 -0500

    complete iteration 3

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,41 +1,45 @@
-const coinDeskApi = axios.create({
-    baseURL: "http://api.coindesk.com/v1/bpi/historical/close.json"
-  });
-  
-  function getCoinDeskInfo() {
-    coinDeskApi
-      .get()
-      .then(responseFromAPI => {
-        console.log("Response from API is: ", responseFromAPI);
-        printTheChart(responseFromAPI.data);
-      })
-      .catch(err => console.log("Error is: ", err));
-  }
-  
-  document.getElementById("theInput").onchange = function() {
-    getCoinDeskInfo();
-  };
+const baseUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 
-function printTheChart(stockData) {
-    const dailyData = stockData["bpi"];
-  
-    const stockDates = Object.keys(dailyData);
-    const stockPrices = stockDates.map(date => dailyData[date]);
-  
-    const ctx = document.getElementById("myChart").getContext("2d");
-  
-    new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: stockDates,
-        datasets: [
-          {
-            label: "Bitcoin prices",
-            backgroundColor: "rgb(255, 99, 132)",
-            borderColor: "rgb(255, 99, 132)",
-            data: stockPrices
-          }
-        ]
-      }
-    }); 
-  } 
+const getCoinDeskInfo = (firstDate, secondDate) => {
+  axios
+    .get(`${baseUrl}?start=${firstDate}&end=${secondDate}`)
+    .then(responseFromAPI => {
+      console.log("Response from API is: ", responseFromAPI);
+      printTheChart(responseFromAPI.data);
+    })
+    .catch(err => console.log("Error is: ", err));
+}
+
+const printTheChart = stockData => {
+  const dailyData = stockData["bpi"];
+  const stockDates = Object.keys(dailyData);
+  const stockPrices = stockDates.map(date => dailyData[date]);
+  const ctx = document.getElementById("myChart").getContext("2d");
+
+  const sortedArray = stockPrices.sort((a,b) => a-b);
+  const max = sortedArray[sortedArray.length-1];
+  const min = sortedArray[0];
+  document.getElementById('max').innerHTML = `Max: ${max}`;
+  document.getElementById('min').innerHTML = `Min: ${min}`;
+
+  new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: stockDates,
+      datasets: [
+        {
+          label: "Bitcoin prices",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: stockPrices
+        }
+      ]
+    }
+  });
+}
+
+document.getElementById("second-date").onchange = function() {
+  let firstDate = document.getElementById("first-date").value;
+  let secondDate = document.getElementById("second-date").value; 
+  getCoinDeskInfo(firstDate, secondDate);
+};
--- starter-code/routes/index.js
+++ starter-code/routes/index.js
@@ -1,12 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const axios = require('axios');
 
 // const baseUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'BPI' });
+    res.render('index', { title: 'BPI Prices' });
 });
 
 module.exports = router;
