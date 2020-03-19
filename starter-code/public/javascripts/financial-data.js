a73eceb9c2d44e5cf5bf885c3c512fbfe9579b26
Sybille Kasslatter <sybille.kasslatter@hotmail.com>
2020-03-19 16:44:35 +0100

    Iteration 3 done

--- starter-code/public/javascripts/financial-data.js
+++ starter-code/public/javascripts/financial-data.js
@@ -1,27 +1,54 @@
-let $chart = document.getElementById("myChart");
-let fromDate = document.getElementById("from-date");
-let toDate = document.getElementById("to-date");
+let apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+let $update = document.getElementById("update");
 
-axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-  .then(apiResponse => {
-    const dataBitcoin = apiResponse.data.bpi;
-    console.log(dataBitcoin);
+getAxios(apiUrl)
 
-    let myChart = new Chart($chart, {
-      type: "line",
-      data: {
-        labels: Object.keys(dataBitcoin),
-        datasets: [{
-            label: "Bitcoin Price Index",
-            data: Object.values(dataBitcoin),
-            backgroundColor: "rgba(255, 99, 132, 0.2)",
-            borderColor: "rgba(255, 99, 132, 1)",
-            borderWidth: 1,
-            pointBackgroundColor: "rgb(128, 128, 128)",
-            pointHoverBackgroundColor: "rgb(20, 19, 19)",
-          }
-        ]
-      }
+//EVENT LISTENER
+$update.addEventListener("click", () => {
+  debugger
+  let fromDate = document.getElementById("from-date");
+  let toDate = document.getElementById("to-date");
+
+  if (fromDate.value >= toDate.value) {
+    let $error = document.getElementById("error");
+    $error.innerText = `The end date can not be smaller than the start date`;
+  }
+  else {
+    newApiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate.value}&end=${toDate.value}`;
+    getAxios(newApiUrl)
+  }
+});
+
+//////
+function getAxios(url){
+  axios
+    .get(url)
+    .then(responseFromAPI => {
+      printTheChart(responseFromAPI.data.bpi);
+    })
+    .catch(err => {
+      console.log("Error while getting the data: ", err);
     });
-  });
+}
+
+
+function printTheChart(data) {
+  let $chart = document.getElementById("myChart").getContext("2d");
+  let myChart = new Chart($chart, {
+    type: "line",
+    data: {
+      labels: Object.keys(data),
+      datasets: [
+        {
+          label: "Bitcoin Price Index",
+          data: Object.values(data),
+          backgroundColor: "rgba(255, 99, 132, 0.2)",
+          borderColor: "rgba(255, 99, 132, 1)",
+          borderWidth: 1,
+          pointBackgroundColor: "rgb(128, 128, 128)",
+          pointHoverBackgroundColor: "rgb(20, 19, 19)"
+        }
+      ]
+    }
+  }); 
+} 
