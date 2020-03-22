ca202f022b62700af7b14f1527d8b021f8d17550
Emelie Sundgren <emelie.sundgren@hotmail.com>
2020-03-22 12:16:40 +0100

    done

--- script.js
+++ script.js
@@ -3,84 +3,42 @@ var $toDate = document.getElementById('dataTo');
 var toDate = $toDate.value;
 var fromDate = $fromDate.value;
 
-//Graph from-date
-$fromDate.addEventListener('change', () => {
+render(fromDate, toDate);
+
+$fromDate.addEventListener('change', event => {
   fromDate = $fromDate.value;
+  render(fromDate, toDate);
+});
+
+$toDate.addEventListener('change', event => {
   toDate = $toDate.value;
+  render(fromDate, toDate);
+});
+
+function render(fromD, toD) {
   axios
-    .get( `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`)
+    .get(
+      `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`
+    )
     .then(bpiData => {
       var ctx = document.getElementById('myChart').getContext('2d');
       var chart = new Chart(ctx, {
-
         type: 'line',
 
         data: {
           labels: Object.keys(bpiData.data.bpi),
-          datasets: [{
+          datasets: [
+            {
               label: 'My First dataset',
               backgroundColor: 'rgb(255, 99, 132)',
               borderColor: 'rgb(255, 99, 132)',
               data: Object.values(bpiData.data.bpi),
-            },],
+            },
+          ],
         },
       });
     })
     .catch(error => {
       console.log('Error is: ', error);
     });
-});
-
-//Graph to-date
-$toDate.addEventListener('change', () => {
-  toDate = $toDate.value;
-  fromDate = $fromDate.value;
-  axios
-    .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`)
-    .then(bpiData => {
-      var ctx = document.getElementById('myChart').getContext('2d');
-      var chart = new Chart(ctx, {
-
-        type: 'line',
-
-        data: {
-          labels: Object.keys(bpiData.data.bpi),
-          datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: Object.values(bpiData.data.bpi),
-            },],
-        },
-      });
-    })
-    .catch(error => {
-      console.log('Error is: ', error);
-    });
-});
-
-
-//Graph showing
-axios
-  .get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`)
-  .then(bpiData => {
-    var ctx = document.getElementById('myChart').getContext('2d');
-    var chart = new Chart(ctx, {
-
-      type: 'line',
-
-      data: {
-        labels: Object.keys(bpiData.data.bpi),
-        datasets: [
-          {
-            label: 'My First dataset',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: Object.values(bpiData.data.bpi),
-          },],
-      },
-    });
-  })
-  .catch(error => {
-    console.log('Error is: ', error);
-  });
+}
