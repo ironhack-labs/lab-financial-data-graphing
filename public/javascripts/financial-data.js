0a761130025c27a89a9bb11885b7688b479fef88
Daniel Hoogerwerf <45287356+danielhoogerwerf@users.noreply.github.com>
2020-03-26 21:07:17 +0100

    Iteration 2 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,5 +1,27 @@
-axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-    .then(results => {
-        console.log(results.data)
-    })
-    .catch(err => console.log("Error while getting the data: ", err));
+axios
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+  .then(results => {
+    priceChart(results.data.bpi);
+  })
+  .catch(err => console.log("Error while getting the data: ", err));
+
+const priceChart = results => {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  const stockDates = Object.keys(results);
+  const stockPrices = stockDates.map(date => results[date]);
+
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: stockDates,
+      datasets: [
+        {
+          label: "Stock Chart",
+          backgroundColor: "rgb(251, 123, 32)",
+          borderColor: "rgb(255, 99, 132)",
+          data: stockPrices
+        }
+      ]
+    }
+  });
+};
