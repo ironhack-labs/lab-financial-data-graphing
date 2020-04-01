9d55919a968a953e61f6ac25d0314d09059806b1
Fabio Menozzi <f.menozzi@yahoo.com>
2020-04-01 17:43:51 -0300

    iterations 1-4 Done

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,66 @@
+let apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+let fromDate;
+let toDate;
+
+// API call
+ function apiCall() {
+    axios
+        .get(apiUrl)
+        .then(responseFromAPI => {
+            printTheChart(responseFromAPI.data); // <== call the function here where you used to console.log() the response
+            console.log(responseFromAPI.data)
+        })
+        .catch(err => console.log("Error while getting the data: ", err));
+} 
+
+
+
+
+//graph 
+function printTheChart(bitCoinData) {
+    const dailyData = bitCoinData['bpi'];
+
+    const bitcoinDates = Object.keys(dailyData);
+    const bitcoinPrices = Object.values(dailyData);
+    
+    const ctx = document.getElementById("myChart").getContext("2d")
+    const chart = new Chart(ctx, {
+        type: "line",
+        data: {
+            labels: bitcoinDates,
+            datasets: [{
+                label: "Bitcoin Chart",
+                backgroundColor: "rgb(255, 99, 132)",
+                borderColor: "rgb(255, 99, 132)",
+                data: bitcoinPrices
+            }]
+        }
+    }); //closing chart
+
+} // closing function printTheChart
+
+apiCall()
+
+
+document.getElementById('fromDate').addEventListener('input', (event) => {
+    fromDate = event.target.value
+    toDate= document.getElementById('toDate').value
+    apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`
+    apiCall()
+})
+
+document.getElementById('toDate').addEventListener('input', (event) => {
+    toDate = event.target.value
+    fromDate= document.getElementById('fromDate').value
+    apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}`
+    apiCall()
+})
+       
+document.getElementById('currency').addEventListener('change', (event) => {
+    currency = event.target.value
+   
+    apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`
+    apiCall()
+})
+
+
