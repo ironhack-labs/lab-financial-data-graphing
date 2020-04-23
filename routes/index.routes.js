ccf47de383e1cd8f5c760b2e3b051599c5273faa
Vidal Diaz <vgdiaz@hotmail.com>
2020-04-23 00:43:51 -0500

    Se suben primeras tres iteraciones

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,10 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express')
+const router = express.Router()
+const axios = require('axios')
 
 /* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
-
-module.exports = router;
+router.get('/', async (req, res, next) => {
+  const { data } = await axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+  res.render('index', data)
+})
+module.exports = router
