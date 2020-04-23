ae262b34a4465049ef3b9070c2b6cae6b7f468ba
donovanftl <donovan_torreslan@outlook.com>
2020-04-22 23:47:48 -0500

    iteracion 3 done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,7 +5,7 @@ const axios = require('axios')
 
 /* GET home page. */
 router.get('/', async(req, res, next ) => {
-  const {data} = await axios.get('https://api.kanye.rest')
+  const {data} = await axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
   res.render('index', data)
 })    
 module.exports = router;
