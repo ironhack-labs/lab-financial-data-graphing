b325064eca2d2d688f1f2929d85e154286ff5e8c
Armando del Rio <d-rio@outlook.com>
2020-04-23 09:29:30 -0500

    graph done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
-
+router.get('/', async(req, res, next ) => {
+ const {data} = await axios.get('http://api.coindesk.com/v1/bpi/historical/close.json')
+ res.render('index', data)
+})    
 module.exports = router;
