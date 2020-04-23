8555ad0b54d7dcdebddeaea8bde38390a70fc3a3
FranciscoEP <francisco.eugp@gmail.com>
2020-04-23 01:38:01 -0500

    iterations done missing styles and bonuses

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,56 @@
+const ctx = document.getElementById('blockChain').getContext('2d')
+
+const filterLoad = () => {
+  const initDate = document.getElementById('start-date').value
+  const finalDate = document.getElementById('final-date').value
+  const chartUrl =
+    `http://api.coindesk.com/v1/bpi/historical/close.json?start=` +
+    `${initDate}` +
+    '&end=' +
+    `${finalDate}`
+
+  axios
+    .get(chartUrl)
+    .then(({ data }) => {
+      const dates = Object.keys(data.bpi)
+      const values = Object.values(data.bpi)
+      const blockChain = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: dates,
+          datasets: [
+            {
+              label: 'Block Chain behavior figures',
+              data: values,
+              backgroundColor: ['rgba(94, 240, 200, 1)'],
+              borderColor: [
+                'rgba(255, 100, 132, 1)',
+                'rgba(54, 299, 235, 1)',
+                'rgba(255, 359, 86, 1)',
+                'rgba(75, 100, 192, 1)',
+                'rgba(153, 120, 120, 1)',
+                'rgba(105, 159, 64, 1)',
+                'rgba(55, 229, 204, 1)',
+                'rgba(255, 50, 100, 1)',
+              ],
+              borderWidth: 1,
+            },
+          ],
+          options: {
+            scales: {
+              yAxes: [
+                {
+                  ticks: {
+                    beginAtZero: true,
+                  },
+                },
+              ],
+            },
+          },
+        },
+      })
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+}
