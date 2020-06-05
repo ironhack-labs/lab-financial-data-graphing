cd7795b2f6c39a4075642dec9debce0f17663aff
Alba Olveros <x8a.018@gmail.com>
2020-06-05 19:29:30 +0200

    Lab done with bonus

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const favicon = require('serve-favicon');
 const logger = require('morgan');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const cors = require("cors")
+
 
 const app = express();
 
@@ -14,6 +16,7 @@ app.set('view engine', 'hbs');
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,50 +1,63 @@
-// Filter by dates
-// ?start=<VALUE>&end=<VALUE> (YYY-MM-DD)
+const startDate = document.getElementById("startDate");
+const endDate = document.getElementById("endDate");
+const currency = document.getElementById("currency");
 
-// Change the currency
-// ?currency=<VALUE>
-const startDate = document.getElementById('startDate');
-const endDate = document.getElementById('endDate');
-const currency =  document.getElementById('currency');
+const elementsArray = document.querySelectorAll("input");
 
-const arrayofInput = [startDate, endDate, currency]
-
-/*
 axios
-.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-.then(response => {
-    printTheChart(response.data.bpi)
-})
-*/
+  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
+  .then((response) => {
+    printTheChart(response.data.bpi, currency.value);
+  });
 
-arrayofInput.addEventListener('input', [startDate, endDate, currency] => {
-   
+elementsArray.forEach((elem) => {
+  elem.addEventListener("input", () => {
+    console.log("Event listening");
     axios
-    .get(`http://api.coindesk.com/v1/bpi/historical/close.json/?start=${startDate.value}&end=${endDate.value}?currency=${currency.value}`)
-    
-    .then(response => {
-        console.log(response.data)
-    printTheChart(response.data.bpi)
-    })
+      .get(
+        "http://api.coindesk.com/v1/bpi/historical/close.json?currency=" +
+          currency.value +
+          "&start=" +
+          startDate.value +
+          "&end=" +
+          endDate.value
+      )
+
+      .then((response) => {
+        console.log(response.data);
+        printTheChart(response.data.bpi, currency.value);
+      });
+  });
 });
 
-function printTheChart(stockData) {
-    const date = Object.keys(stockData)
-    const price = Object.values(stockData)
-  
-    const ctx = document.getElementById('my-chart').getContext('2d');
-    const chart = new Chart(ctx, {
-      type: 'line',
-      data: {
-        labels: date,
-        datasets: [
-          {
-            label: 'BitCoin Price Index',
-            backgroundColor: 'rgb(255, 99, 132)',
-            borderColor: 'rgb(255, 99, 132)',
-            data: price
-          }
-        ]
-      }
-    }); // closes chart = new Chart()
-  } // closes printTheChart()*/
+function printTheChart(stockData, currency) {
+  const date = Object.keys(stockData);
+  const price = Object.values(stockData);
+
+  const max = Math.max(...price);
+  const min = Math.min(...price);
+
+  const ctx = document.getElementById("my-chart").getContext("2d");
+  const chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: date,
+      datasets: [
+        {
+          label: "BitCoin Price Index",
+          backgroundColor: "rgb(255, 99, 132)",
+          borderColor: "rgb(255, 99, 132)",
+          data: price,
+        },
+      ],
+    },
+  }); // closes chart = new Chart()
+
+  let str = `
+        <h3>Values</h3>
+         <div><b>Min: </b>${min} ${currency}</div>
+         <div><b>Max: </b>${max} ${currency}</div>
+
+   `;
+  document.getElementsByClassName("container")[0].innerHTML = str;
+} // closes printTheChart()
