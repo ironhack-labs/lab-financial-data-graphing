3ce9313063a123e477da649a7c35546695cce7d6
Juli Ramon <juli@nexumlabs.com>
2020-06-10 01:04:20 +0200

    Iteration 3,4,5 - Done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,17 +1,40 @@
-const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+console.log('JS Connected');
 
-axios
-    .get(apiUrl)
-    .then(apiResponse => {
-        console.log(apiResponse);
-        printChart(apiResponse.data.bpi)
-    })
-    .catch(err => console.log('Error getting data from API', err))
+window.addEventListener('load', getElements)
+const showBtn = document.getElementById('showBtn');
+showBtn.addEventListener('click', getElements)
 
+function getElements(){
+    const startDate = document.getElementById('startDate').value;
+    const endDate = document.getElementById('endDate').value;
+    const currency = document.getElementById('currencySelector').value;
+    getData(startDate, endDate, currency);
+}
 
-const printChart = graphData => {
+function getData(startDate, endDate, currency) {
+    let apiUrl = ``;
+
+    if(!startDate || !endDate){
+        apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`
+    } else {
+        apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
+    }
+    
+    axios
+        .get(apiUrl)
+        .then(apiResponse => printChart(apiResponse.data.bpi, currency))
+        .catch(err => console.log('Error getting data from API', err))
+}
+
+function printChart(graphData, currency) {
     const dates = Object.keys(graphData);
-    const prices = Object.values(graphData)
+    const prices = Object.values(graphData);
+
+    const maxValue = document.getElementById('maxValue');
+    maxValue.innerHTML = Math.max(...prices) + " " + currency;
+    const minValue = document.getElementById('minValue');
+    minValue.innerHTML = Math.min(...prices) + " " + currency;
+
     const ctx = document.getElementById('myChart').getContext('2d');
     const chart = new Chart(ctx, {
         type: 'line',
