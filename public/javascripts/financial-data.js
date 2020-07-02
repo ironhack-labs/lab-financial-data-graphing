f315a4102f3d66e0cc9ab0c0f58246b8bd4541f5
carlosziegler <carlos.ziegler@gmail.com>
2020-07-02 14:53:18 +0200

    Add chart.js

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -15,6 +15,54 @@ const fetchHistoricalData = async () => {
 const getHistoricalData = async () => {
     const result = await fetchHistoricalData()
     console.log(result)
+    return result
 }
 
-getHistoricalData()
+
+const ctx = document.getElementById('myChart').getContext('2d');
+
+const printChart = async () => {
+    const data = await getHistoricalData()
+    const dailyData = "Coins"
+    //data for x
+    const timeData = Object.keys(data.bpi)
+    //data for y
+    const coinsValue = Object.values(data.bpi)
+    console.log('timeData', timeData)
+    console.log('coinsValue', coinsValue)
+
+
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: timeData,
+            datasets: [{
+                label: 'Biticoin Price Index',
+                data: coinsValue,
+                backgroundColor: [
+
+                    'rgba(54, 162, 235, 0.2)',
+
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+
+                ],
+                borderWidth: 1
+            }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        }
+    });
+
+
+
+}
+printChart()
