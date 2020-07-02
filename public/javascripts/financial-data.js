057c44e9181c5c24f30c4adc610df95f773d3248
SiEr0212 <simonertel@gmx.de>
2020-07-02 21:04:13 +0200

    Iteration 2 done, parts of 3

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,44 +1,45 @@
 const key = "demo";
 const functionName = "TIME_SERIES_DAILY";
 const symbolName = "MSFT";
-const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.jso`;
+const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=2020-01-01&end=2020-07-01&currency=USD`;
 
 axios
     .get(apiUrl)
     .then(response => {
-        console.log(response.data);
-        printTheChart(response.data)
+        //console.log(response.data);
+
+        const bitcoinData = response.data.bpi;
+
+        //console.log(bitcoinData);
+        
+        const dates = Object.keys(bitcoinData)
+        const values = Object.values(bitcoinData)
+        
+        printTheChart(dates, values);
     })
     .catch(err => {
         console.log('Error while getting the data', err);
     })
 
-    function printTheChart(stockData) {
-        const dailyData = stockData['Time Series (Daily)'];
-        // console.log('daily data:', dailyData);
-        // this is the data for the x axis
-        const stockDates = Object.keys(dailyData);
-        console.log(stockDates);
-        // this is the data for the y axis
-        const stockPrices = stockDates.map(date => {
-            return dailyData[date]['4. close'];
-        });
-        console.log('stock prices: ', stockPrices);
-    
+    function printTheChart(dates, values) {
         const ctx = document.getElementById('myChart').getContext('2d');
-    
-        new Chart(ctx, {
+        ctx.clearRect(0, 0, 700, 400);
+        const chart = new Chart(ctx, {
             type: 'line',
             data: {
-                labels: stockDates,
+                labels: dates,
                 datasets: [
                     {
-                        label: 'Bitcoin Stock Chart',
+                        label: 'Stock Chart',
                         backgroundColor: 'rgb(255, 99, 132)',
                         borderColor: 'rgb(255, 99, 132)',
-                        data: stockPrices
+                        data: values
                     }
                 ]
             }
-        })
-    }
+        }); // closes chart = new Chart()
+    }; // closes printTheChart()
+    
+    //document.getElementById('canvas-update').addEventListener('click', () => {
+        //getBitcoinValue();
+   
