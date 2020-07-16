c20150f6c6f701e56d3d16e7d4d3627500f092ae
Irma Blanco <irblancofdz@gmail.com>
2020-07-16 20:28:49 +0200

    Iteration 5

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -7,87 +7,118 @@ const currencySelect = document.getElementById('currency')
 const endpoint = 'https://api.coindesk.com/v1/bpi/historical/close.json';
 
 function getDates() {
-    const dateFrom = inputDateFrom.value;
-    const dateTo = inputDateTo.value;
-    const currency = currencySelect.value
-    return `${endpoint}?start=${dateFrom}&end=${dateTo}&currency=${currency}`;
+  const dateFrom = inputDateFrom.value;
+  const dateTo = inputDateTo.value;
+  const currency = currencySelect.value
+  return `${endpoint}?start=${dateFrom}&end=${dateTo}&currency=${currency}`;
+}
+
+function getMaxAndMin(response) {
+
+  const bitcoinValues = Object.values(response.data.bpi);
+
+  const bitcoinMax = arrayMax(bitcoinValues);
+  const bitcoinMin = arrayMin(bitcoinValues);
+
+  document.getElementById('min').innerHTML = `${bitcoinMin} ${currencySelect.value}`;
+  document.getElementById('max').innerHTML = `${bitcoinMax} ${currencySelect.value}`;
+
+  currencySelect.addEventListener('change', () => {
+    document.getElementById('min').innerHTML = `${bitcoinMin} ${currencySelect.value}`;
+    document.getElementById('max').innerHTML = `${bitcoinMax} ${currencySelect.value}`;
+  });
+}
+
+
+function arrayMin(arr) {
+  return arr.reduce(function (p, v) {
+    return (p < v ? p : v);
+  });
+}
+
+function arrayMax(arr) {
+  return arr.reduce(function (p, v) {
+    return (p > v ? p : v);
+  });
 }
 
 function formatDate(date) {
-    var d = new Date(date),
-        month = '' + (d.getMonth() + 1),
-        day = '' + d.getDate(),
-        year = d.getFullYear();
+  var d = new Date(date),
+    month = '' + (d.getMonth() + 1),
+    day = '' + d.getDate(),
+    year = d.getFullYear();
 
-    if (month.length < 2)
-        month = '0' + month;
-    if (day.length < 2)
-        day = '0' + day;
+  if (month.length < 2)
+    month = '0' + month;
+  if (day.length < 2)
+    day = '0' + day;
 
-    console.log('formated date', [year, month, day].join('-'));
-    return [year, month, day].join('-');
+  console.log('formated date', [year, month, day].join('-'));
+  return [year, month, day].join('-');
 }
 
 function printChart(response) {
-    const labels = Object.keys(response.data.bpi)
-    const ctx = document.getElementById('myChart').getContext('2d');
-    const data = Object.values(response.data.bpi)
-    const chart = new Chart(ctx, {
-        // The type of chart we want to create
-        type: 'line',
+  const labels = Object.keys(response.data.bpi)
+  const ctx = document.getElementById('myChart').getContext('2d');
+  const data = Object.values(response.data.bpi)
+  const chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
 
-        // The data for our dataset
-        data: {
-            labels: labels,
-            datasets: [{
-                label: 'Bitcoin Price',
-                backgroundColor: 'rgb(255, 99, 132)',
-                borderColor: 'rgb(255, 99, 132)',
-                data
-            }]
-        },
+    // The data for our dataset
+    data: {
+      labels: labels,
+      datasets: [{
+        label: 'Bitcoin Price',
+        backgroundColor: 'rgb(255, 99, 132)',
+        borderColor: 'rgb(255, 99, 132)',
+        data
+      }]
+    },
 
-        // Configuration options go here
-        options: {}
-    });
+    // Configuration options go here
+    options: {}
+  });
 }
 
+
 axios.get('https://api.coindesk.com/v1/bpi/historical/close.json')
-    .then(responseFromApi => {
-        printChart(responseFromApi);
-    })
-    .catch(error => console.error(error))
+  .then(responseFromApi => {
+    getMaxAndMin(responseFromApi);
+    printChart(responseFromApi);
+  })
+  .catch(error => console.error(error))
 
 inputDateFrom.addEventListener('change', () => {
 
-    if (inputDateTo.value !== '') {
-        axios
-            .get(getDates())
-            .then((responseFromApi) => {
-                printChart(responseFromApi);
-            })
-            .catch(err => console.log('err', err));
-    };
+  if (inputDateTo.value !== '') {
+    axios
+      .get(getDates())
+      .then((responseFromApi) => {
+        printChart(responseFromApi);
+      })
+      .catch(err => console.log('err', err));
+  };
 });
 
 inputDateTo.addEventListener('change', () => {
-    if (inputDateFrom.value !== '') {
-        axios
-            .get(getDates())
-            .then((responseFromApi) => {
-                printChart(responseFromApi);
-            })
-            .catch(err => console.log('err', err));
-    };
+  if (inputDateFrom.value !== '') {
+    axios
+      .get(getDates())
+      .then((responseFromApi) => {
+        printChart(responseFromApi);
+      })
+      .catch(err => console.log('err', err));
+  };
 });
 
 currencySelect.addEventListener('change', () => {
-    if (inputDateFrom.value !== '' && inputDateTo !== '') {
-        axios
-            .get(getDates())
-            .then((responseFromApi) => {
-                printChart(responseFromApi);
-            })
-            .catch(err => console.log('err', err));
-    };
+  if (inputDateFrom.value !== '' && inputDateTo !== '') {
+    axios
+      .get(getDates())
+      .then((responseFromApi) => {
+        printChart(responseFromApi);
+      })
+      .catch(err => console.log('err', err));
+  };
 });
