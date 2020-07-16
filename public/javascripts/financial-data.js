acc2247c24594a8ff14518b1daa4485b5b84510b
TL Garcia <tomas.bl.garcia@gmail.com>
2020-07-16 13:21:01 +0200

    All iterations done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,35 +1,53 @@
 const dateFromInput = document.getElementById('date-from');
 const dateToInput = document.getElementById('date-to');
 const currencyInput = document.getElementById('select-currency');
+const minValue = document.getElementById('min-value');
+const maxValue = document.getElementById('max-value');
 let from = dateFromInput.value;
 let to = dateToInput.value;
 let currency = currencyInput.value;
 
-window.addEventListener('load', getInfo(from, to));
 
+window.addEventListener('load', getInfo(from, to, currency));
 dateFromInput.addEventListener('input', () => {
   from = dateFromInput.value;
-  getInfo(from, to);
+  getInfo(from, to, currency);
 });
 
 dateToInput.addEventListener('input', () => {
   to = dateToInput.value;
-  getInfo(from, to);
+  getInfo(from, to, currency);
+});
+
+currencyInput.addEventListener('input', () => {
+  currency = currencyInput.value;
+  getInfo(from, to, currency);
 });
 
 
-function getInfo(from, to) {
-    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${from}&end=${to}` )
-        .then(response => printChart(response.data))
+function getInfo(from, to, currency) {
+    axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}&start=${from}&end=${to}` )
+        .then(response => {
+          findMinMax(response.data.bpi);
+          printChart(response.data);
+        })
         .catch(error => console.error(error));
+};
+
+function findMinMax(bpi) {
+  const values = Object.values(bpi);
+  const min = Math.min(...values);
+  const max = Math.max(...values);
+
+  minValue.innerText = min + currency;
+  maxValue.innerText = max + currency;
 }
 
 
 const ctx = document.getElementById('myChart').getContext('2d');
 
 function printChart(bpiData) {
-  const dailyData = bpiData['bpi'];
-  console.log('Daily Data: ' + dailyData)
+  const dailyData = bpiData.bpi;
  
   const bpiDates = Object.keys(dailyData);
   const ratePrices = bpiDates.map(date => dailyData[date]);
@@ -48,5 +66,5 @@ function printChart(bpiData) {
         }
       ]
     }
-  }); // closes chart = new Chart()
-} // closes printTheChart()
+  }); 
+}
