9920203479c9b5e582f3c29b4cc312d638db4079
Michel Ribbens <michelribbens@gmail.com>
2020-08-18 20:42:17 +0100

    iteration 1

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,8 @@
+const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+
+axios
+  .get(apiUrl)
+  .then(responseFromAPI => {
+    console.log(responseFromAPI)
+  })
+  .catch(err => console.log('Error while getting the data: ', err));
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
+router.get('/', (req, res) => res.render('index', { title: 'LAB | Financial Data Graphing' }));
 
 module.exports = router;
