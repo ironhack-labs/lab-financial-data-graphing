9efe9fccfcae08be191ce0199d98819bd7a425b8
Michel Ribbens <michelribbens@gmail.com>
2020-08-18 20:57:12 +0100

    iteration 2

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -3,6 +3,35 @@ const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
 axios
   .get(apiUrl)
   .then(responseFromAPI => {
-    console.log(responseFromAPI)
+    console.log(responseFromAPI); // Iteration 1
+    printTheChart(responseFromAPI.data); 
   })
   .catch(err => console.log('Error while getting the data: ', err));
+
+
+function printTheChart(bitcoinData) {
+  const dailyData = bitcoinData.bpi;
+
+  const dates = Object.keys(dailyData);
+  const bitcoinPrices = dates.map(date => dailyData[date]);
+
+var ctx = document.getElementById('myChart').getContext('2d');
+var chart = new Chart(ctx, {
+    // The type of chart we want to create
+    type: 'line',
+
+    // The data for our dataset
+    data: {
+        labels: dates,
+        datasets: [{
+            label: 'Bitcoin Price index',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: bitcoinPrices
+        }]
+    },
+
+    // // Configuration options go here
+    // options: {}
+  });
+}
