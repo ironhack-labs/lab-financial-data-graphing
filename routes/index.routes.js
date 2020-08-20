8fae0a82f19f69a3fab353112823d0269f0b8fb0
“Debora <debora.crescenzo@gmail.com>
2020-08-20 13:17:54 +0100

    done

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,7 +1,9 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
+router.get("/", (req, res) =>
+  res.render("index", { title: "LAB | Financial Data Graphing" })
+);
 
 module.exports = router;
