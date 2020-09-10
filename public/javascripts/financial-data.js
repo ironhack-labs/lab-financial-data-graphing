36166ab936880d2fd276af9bf32d368f1e0c8f6a
Diana <d.thueroff@gmail.com>
2020-09-10 21:16:21 +0200

    all iterations without bonus

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,68 @@
+const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+
+const printTheChart = stockData => {
+    const dailyData = stockData.bpi;
+    console.log({ dailyData });
+//This is the data for the x axis:
+    const stockDates = Object.keys(dailyData);
+    console.log({ stockDates });
+    //This is the data for the y axis
+
+    const bitcoinPrice = stockDates.map(date => dailyData[date]);
+    console.log(bitcoinPrice);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: stockDates,
+            datasets: [
+                {
+                    label: 'Bitcoin Chart',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: bitcoinPrice
+                }
+            ]
+        }
+    });
+
+};
+function changeDate(){
+    const userInputStart = document.getElementById('start').value;
+    const userInputEnd = document.getElementById('end').value;
+
+    console.log('Startdatum: ', userInputStart);
+    console.log('Enddatum: ', userInputEnd);
+
+    axios
+.get(`${apiUrl}?start=${userInputStart}&end=${userInputEnd}`)
+.then(response => {
+    console.log(response.data);
+    printTheChart(response.data);
+})
+.catch(err => {
+    console.log('error while getting the data', err);
+});
+}
+
+
+
+document.getElementById("start").addEventListener('change', changeDate);
+document.getElementById("end").addEventListener('change', changeDate);
+
+
+
+//
+
+axios
+.get(apiUrl)
+.then(response => {
+    console.log(response.data);
+    printTheChart(response.data);
+})
+.catch(err => {
+    console.log('error while getting the data', err);
+});
+
+
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 
+
+
 /* GET home page. */
 router.get('/', (req, res) => res.render('index', { title: 'Express' }));
 
