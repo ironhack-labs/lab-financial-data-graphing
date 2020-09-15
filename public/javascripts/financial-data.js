877773f33be92693de2a4f17c0d8d75b71d673b6
anna dodo <anna.dorenskaya@gmail.com>
2020-09-15 22:18:59 +0200

    3rd iteration

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -4,88 +4,28 @@ let ctx = document.getElementById("myChart").getContext("2d");
 axios
   .get(url)
   .then((data) => {
-   // console.log(data.data.bpi);
+    // console.log(data.data.bpi);
     let keys = Object.keys(data.data.bpi);
-    let values =  Object.values(data.data.bpi);
-   // console.log(keys);
-   // console.log(values)
+    let values = Object.values(data.data.bpi);
+    // console.log(keys);
+    // console.log(values)
     createChart(keys, values);
   })
   .catch((err) => {
     console.log(err);
   });
 
-  function createChart(xaxis, yaxis) {
-    let myChart = new Chart(ctx, {
-        type: "line",
-        data: {
-            //x Axis
-          labels: xaxis,
-          datasets: [
-            {
-              label: "Bitcoin Prica Index",
-              //y axis
-              data: yaxis,
-/*               backgroundColor: [
-                "rgba(255, 99, 132, 0.2)",
-                "rgba(54, 162, 235, 0.2)",
-                "rgba(255, 206, 86, 0.2)",
-                "rgba(75, 192, 192, 0.2)",
-                "rgba(153, 102, 255, 0.2)",
-                "rgba(255, 159, 64, 0.2)",
-              ],
-              borderColor: [
-                "rgba(255, 99, 132, 1)",
-                "rgba(54, 162, 235, 1)",
-                "rgba(255, 206, 86, 1)",
-                "rgba(75, 192, 192, 1)",
-                "rgba(153, 102, 255, 1)",
-                "rgba(255, 159, 64, 1)",
-              ], */
-              borderWidth: 1,
-            },
-          ],
-        },
-        options: {
-          scales: {
-            yAxes: [
-              {
-                ticks: {
-                  beginAtZero: true,
-                },
-              },
-            ],
-          },
-        },
-      });
-  }
-
- 
-
-/*   let myChart = new Chart(ctx, {
-    type: "bar",
+function createChart(xaxis, yaxis) {
+  let myChart = new Chart(ctx, {
+    type: "line",
     data: {
-      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      //x Axis
+      labels: xaxis,
       datasets: [
         {
-          label: "# of Votes",
-          data: [1, 2, 3],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
+          label: "Bitcoin Prica Index",
+          //y axis
+          data: yaxis,
           borderWidth: 1,
         },
       ],
@@ -101,4 +41,38 @@ axios
         ],
       },
     },
-  }); */
+  });
+}
+
+/* 3rd iteration */
+const startDate = document.getElementById("start-date");
+const finishDate = document.getElementById("finish-date");
+
+startDate.addEventListener("change", (event) => {
+  if (finishDate.value) {
+    filterData();
+  }
+});
+
+finishDate.addEventListener("change", (event) => {
+  if (startDate.value) {
+    filterData();
+  }
+});
+
+function filterData() {
+  const urlFilter = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate.value}&end=${finishDate.value}`;
+  console.log(urlFilter);
+  axios
+    .get(urlFilter)
+    .then((data) => {
+        let keys = Object.keys(data.data.bpi);
+        let values = Object.values(data.data.bpi);
+        createChart(keys, values);
+    })
+    .catch((err) => {
+      console.log(err);
+     // console.log(error.response.data)
+    });
+}
+
