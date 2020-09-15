1261676bad158ccc472a6bfe03092225227ebdad
anna dodo <anna.dorenskaya@gmail.com>
2020-09-15 21:12:55 +0200

    2nd iteration

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,9 +1,104 @@
-const url = `http://api.coindesk.com/v1/bpi/historical/close.json`
+const url = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+let ctx = document.getElementById("myChart").getContext("2d");
 
-axios.get(url)
-    .then(data => {
-        console.log(data)
-    })
-    .catch(err => {
-        console.log(err)
-    })
+axios
+  .get(url)
+  .then((data) => {
+   // console.log(data.data.bpi);
+    let keys = Object.keys(data.data.bpi);
+    let values =  Object.values(data.data.bpi);
+   // console.log(keys);
+   // console.log(values)
+    createChart(keys, values);
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+  function createChart(xaxis, yaxis) {
+    let myChart = new Chart(ctx, {
+        type: "line",
+        data: {
+            //x Axis
+          labels: xaxis,
+          datasets: [
+            {
+              label: "Bitcoin Prica Index",
+              //y axis
+              data: yaxis,
+/*               backgroundColor: [
+                "rgba(255, 99, 132, 0.2)",
+                "rgba(54, 162, 235, 0.2)",
+                "rgba(255, 206, 86, 0.2)",
+                "rgba(75, 192, 192, 0.2)",
+                "rgba(153, 102, 255, 0.2)",
+                "rgba(255, 159, 64, 0.2)",
+              ],
+              borderColor: [
+                "rgba(255, 99, 132, 1)",
+                "rgba(54, 162, 235, 1)",
+                "rgba(255, 206, 86, 1)",
+                "rgba(75, 192, 192, 1)",
+                "rgba(153, 102, 255, 1)",
+                "rgba(255, 159, 64, 1)",
+              ], */
+              borderWidth: 1,
+            },
+          ],
+        },
+        options: {
+          scales: {
+            yAxes: [
+              {
+                ticks: {
+                  beginAtZero: true,
+                },
+              },
+            ],
+          },
+        },
+      });
+  }
+
+ 
+
+/*   let myChart = new Chart(ctx, {
+    type: "bar",
+    data: {
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+      datasets: [
+        {
+          label: "# of Votes",
+          data: [1, 2, 3],
+          backgroundColor: [
+            "rgba(255, 99, 132, 0.2)",
+            "rgba(54, 162, 235, 0.2)",
+            "rgba(255, 206, 86, 0.2)",
+            "rgba(75, 192, 192, 0.2)",
+            "rgba(153, 102, 255, 0.2)",
+            "rgba(255, 159, 64, 0.2)",
+          ],
+          borderColor: [
+            "rgba(255, 99, 132, 1)",
+            "rgba(54, 162, 235, 1)",
+            "rgba(255, 206, 86, 1)",
+            "rgba(75, 192, 192, 1)",
+            "rgba(153, 102, 255, 1)",
+            "rgba(255, 159, 64, 1)",
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true,
+            },
+          },
+        ],
+      },
+    },
+  }); */
