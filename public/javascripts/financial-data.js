3663f53042870335d88f7df65b1289138c807e02
allecramarcella <allecramarcella.dev@gmail.com>
2020-09-16 15:42:19 +0200

    incl bonus iterations

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -24,6 +24,9 @@ function drawChart(xData, yData) {
 const selectStartDate = document.getElementById('start-date')
 const selectEndDate = document.getElementById('end-date')
 const dateFilter = document.getElementById('date')
+const currencyFilter = document.getElementById('currency')
+const currentMaxValue = document.getElementById('max-value')
+const currentMinValue = document.getElementById('min-value')
 
 // const startDate = '2016-12-01'
 // const endDate = '2017-02-05'
@@ -32,22 +35,38 @@ const dateFilter = document.getElementById('date')
 function showChart(){
     const startDate = selectStartDate.value
     const endDate = selectEndDate.value
+    const currency = currencyFilter.value
 
-    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`)
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
         .then(response => {
             console.log(response)
 
             const xAxis = Object.keys(response.data.bpi) 
             const yAxis = Object.values(response.data.bpi)
             drawChart(xAxis, yAxis)
+
+            // min and max value
+            const minValue = Math.min(...yAxis)
+            const maxValue = Math.max(...yAxis)
+
+            currentMaxValue.innerHTML = `Max: ${maxValue} ${currency}`
+            currentMinValue.innerHTML = `Min: ${minValue} ${currency}`
+
+            
         })
         .catch(err => { console.log(err)})
 }
 
+showChart()
 
 dateFilter.addEventListener('change', event => {
     event.preventDefault()
     showChart()
 })
 
-showChart()
+currencyFilter.addEventListener('change',  event => {
+    event.preventDefault()
+    showChart()
+})
+
+
