8c9c8f7e511a85d7e2238977351769d2e5adb090
Thrashskin <jmontegonz@gmail.com>
2020-12-06 21:12:58 +0100

    Bonus iteration 5 complete

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -22,6 +22,11 @@ const getFinancialData = urlApi => {
     .then(response => {
         const dates = Object.keys(response.data.bpi) 
         const values = dates.map(date => response.data.bpi[date]);
+        //const values = dates.map(date => Number(response.data.bpi[date]*(100/100)));
+
+        document.getElementById('min').innerHTML = Math.min(...values);
+        document.getElementById('max').innerHTML = Math.max(...values);
+
 
         const data = {
             labels: [...dates],
@@ -36,7 +41,8 @@ const getFinancialData = urlApi => {
         let lineChart = new Chart(ctx, {
             type: 'line',
             data: data,
-        });
+        });        
+        
     })
     .catch( err => {
         console.log(err);
@@ -52,8 +58,9 @@ document.getElementById('input-start-date').addEventListener('change', () => {
     let endDate = document.getElementById('input-end-date').value;
     let currency = document.getElementById('input-currency').value;
     let url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
-
     getFinancialData(url);
+
+    
 })
 
 document.getElementById('input-end-date').addEventListener('change', () => {
