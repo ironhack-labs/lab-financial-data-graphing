ac10507cfa0405089e9a3d2f988807124c0f2afc
Santiago Contreras <santiagoc213@gmail.com>
2020-12-16 16:42:43 -0500

    iteration 4

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -38,20 +38,10 @@ async function bpiChart(apiUrl) {
                     label: 'Bitcoin Prices',
                     data: prices,
                     backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
+                        'rgba(255, 99, 132, 0.2)'
                     ],
                     borderColor: [
-                        'rgba(255, 99, 132, 1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
+                        'rgba(255, 99, 132, 1)'
                     ],
                     borderWidth: 1
                 }]
@@ -64,12 +54,19 @@ async function bpiChart(apiUrl) {
       }
 }
 
-document.querySelector('#end-date').addEventListener('change', () => {
+function updateChart() {
     const { value: startDate } = document.querySelector('#start-date');
     const { value: endDate } = document.querySelector('#end-date');
-    const newApiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`;
+    const { value: currency } = document.querySelector('#currency');
+    const newApiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`;
     bpiChart(newApiUrl)
-})
+}
+
+
+
+document.querySelector('#end-date').addEventListener('change', updateChart)
+document.querySelector('#start-date').addEventListener('change', updateChart)
+document.querySelector('#currency').addEventListener('change', updateChart)
 
 bpiChart(apiUrl)
 
