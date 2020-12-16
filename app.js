d465aee9708628d781fdb603a72f40e34cbe5790
Tomy Tubert <tomytubert@gmail.com>
2020-12-16 09:45:10 +0100

    Done without bonus, but i dont like it the way ive done it

--- app.js
+++ app.js
@@ -40,4 +40,5 @@ app.use((err, req, res) => {
   res.render('error');
 });
 
+
 module.exports = app;
--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,6 +1,78 @@
-const startDate = document.querySelector(".startDate").value
-const endDate = document.querySelector(".endDate").value
 
-const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`
+const startDate = document.querySelector("#startDate").value
+const endDate = document.querySelector("#endDate").value
 
-console.log(apiUrl);
+const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+
+async function updateChart(){
+    const startDate = document.querySelector("#startDate").value
+    const endDate = document.querySelector("#endDate").value
+    const apiUrl =`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+    const {data} = await axios.get(apiUrl);
+    //console.log(data);
+    const bpi = data["bpi"];
+    //console.log(bpi);
+    const arrLabel = Object.keys(bpi)
+    //console.log(arrLabel);
+    const arrValues = Object.values(bpi)
+    //console.log(arrValues);
+    const ctx = document.querySelector("#canvas").getContext("2d");
+        const chart = new Chart(ctx,{
+            type:"line",
+            data: {
+                labels: arrLabel,
+                datasets: [
+                    {
+                        label:"BTC",
+                        backgroundColor:"yellow",
+                        borderColor:"black",
+                        data: arrValues
+                    }
+                ]
+            }
+        })
+}
+const endDateTag = document.querySelector("#endDate")
+
+endDateTag.addEventListener("change", updateChart)
+
+// async function getAll(){
+//     try{
+//         const {data} = await axios.get(apiUrl);
+//         //console.log("Data",data);
+//     }catch(err){
+//         console.error(err);
+//     }
+// }
+
+async function showChart(){
+    try{
+        const {data} = await axios.get(apiUrl);
+        //console.log(data);
+        const bpi = data["bpi"];
+        //console.log(bpi);
+        const arrLabel = Object.keys(bpi)
+        //console.log(arrLabel);
+        const arrValues = Object.values(bpi)
+        //console.log(arrValues);
+        const ctx = document.querySelector("#canvas").getContext("2d");
+        const chart = new Chart(ctx,{
+            type:"line",
+            data: {
+                labels: arrLabel,
+                datasets: [
+                    {
+                        label:"BTC",
+                        backgroundColor:"yellow",
+                        borderColor:"black",
+                        data: arrValues
+                    }
+                ]
+            }
+        })
+    }catch(err){
+        console.error(err);
+    }
+}
+
+showChart();
