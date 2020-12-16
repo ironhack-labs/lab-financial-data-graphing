9bf744764f281b23f0e91947f260ed442e95f3af
Tomy <tomytubert@gmail.com>
2020-12-16 08:21:05 +0100

    Npm not found in Imac

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,6 @@
+const startDate = document.querySelector(".startDate").value
+const endDate = document.querySelector(".endDate").value
+
+const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`
+
+console.log(apiUrl);
