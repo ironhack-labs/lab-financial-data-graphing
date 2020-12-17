70553516cbccea2c8032cd66e4efcf1c043bfb1e
Pablo Fernandez <pablo_fz@hotmail.com>
2020-12-17 16:03:42 +0100

    terminado

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,8 +1,18 @@
-const chartBtn = document.querySelector("#get-bitcoinChart-btn");
-chartBtn.addEventListener("click", getData);
+
 let inicio ="2013-09-01";
 let fin = "2013-09-04";
 let moneda = "USD";
+var ctx = document.getElementById('my-chart').getContext('2d');
+
+const chartBtn = document.querySelector("#get-bitcoinChart-btn");
+chartBtn.addEventListener("click", async ()=>{
+    const data = await getData();
+    const ejeX = Object.keys(data);
+    const ejeY = Object.values(data);
+    console.log(data)
+    console.log(ejeX,ejeY)
+    printChart(ejeX,ejeY);
+});
 
 const start = document.querySelector("#start");
 start.addEventListener("change", (event)=>{ inicio =event.target.value
@@ -41,4 +51,26 @@ async function getData() {
 
 function mostrarDatos(){
     console.log(inicio,fin);
+}
+
+function printChart(X,Y){
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: X,
+            datasets: [{
+                label: 'Bitcoin',
+                data: Y,
+                backgroundColor: [
+                    'rgba(255, 99, 132, 0.2)',
+                   
+                ],
+                borderColor: [
+                    'rgba(255, 99, 132, 1)',
+     
+                ],
+                borderWidth: 1
+            }]
+        }
+    });
 }
