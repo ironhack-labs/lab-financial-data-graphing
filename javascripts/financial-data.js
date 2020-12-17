0f6673a35801ffa0bbf4c31be02f9a73b00f17d2
Marc Lopez Pujol <marclopez@MacBook-Pro-de-Marc-2.local>
2020-12-17 08:50:56 +0100

    All done

--- javascripts/financial-data.js
+++ javascripts/financial-data.js
@@ -1,8 +1,9 @@
 const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
-async function getInfo() {
+async function getInfo(start, end, currency) {
     try {
-    const { data } = await axios.get(apiUrl)
+    const endpoint = `?start=${start}&end=${end}&currency=${currency}`
+    const { data } = await axios.get(apiUrl+endpoint)
         return data;
     } catch (err) {
         console.error(err)
@@ -11,19 +12,27 @@ async function getInfo() {
 
 async function createGraph() {
     try {
-        const data = await getInfo();
-        const stocks = data["bpi"]
-        console.log(data)
-        console.log(stocks)
+        const start = document.querySelector("#inicio").value;
+        const end = document.querySelector("#fin").value;
+        const currency = document.querySelector("#moneda").value;
+        const min = document.querySelector("#min");
+        const max = document.querySelector("#max");
+        const valuesDiv = document.querySelector("#values")
+        const data = await getInfo(start, end, currency);
+        const stocks = data["bpi"];
         const labels = Object.keys(stocks);
         const stocksInfo = Object.values(stocks);
+        const ordenados = stocksInfo.sort(ordena);
+        values.style.display = "block"
+        max.innerText = "Max: " + ordenados[ordenados.length - 1] + " " + currency
+        min.innerText = "Min: " + ordenados[0] + " " + currency
         const canvas = document.querySelector("#grafico").getContext("2d");
         const grafico = new Chart(canvas, {
             type: "line",
             data: {
                 labels: labels,
                 datasets: [{
-                    label: 'BPI in Dollars',
+                    label: `BPI in ${currency}`,
                     data: [12, 19, 3, 5, 2, 3],
                     backgroundColor:
                         'rgba(255, 99, 132, 0.2)',
@@ -38,4 +47,10 @@ async function createGraph() {
     }
 }
 
-createGraph();
+const btn = document.querySelector("#create")
+
+btn.addEventListener("click", createGraph);
+
+function ordena(a, b) {
+  return a - b;
+}
