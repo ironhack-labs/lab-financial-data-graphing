d8e0d53623ae9f39d54792bb8a9ce6dfdad2555d
Javier Repi <xavirepi@hotmail.com>
2021-02-10 13:55:15 +0100

    finish iteration 4

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,19 +1,18 @@
 const apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json`
-//const dateFilterUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
 //const currencyUrl = `https://api.coindesk.com/v1/bpi/currentprice/<CODE>.json`
 
 // Axios Get Request
-const getBPI = (url) => {
+const getBPI = (url, chartColor) => {
     axios.get(url)
     .then(responseFromAPI => {
         console.log('responseFromAPI: ', responseFromAPI);
-        printTheChart(responseFromAPI.data)
+        printTheChart(responseFromAPI.data, chartColor)
     })
     .catch(err => console.log('Error while getting the data: ', err))
 }
 
 // Printing the Chart
-function printTheChart(data) {
+function printTheChart(data, color) {
     const bpiHistory = data['bpi'];
     
     const bpiDates = Object.keys(bpiHistory);
@@ -27,8 +26,8 @@ function printTheChart(data) {
             datasets: [
                 {
                     label: 'Bitcoin Price',
-                    backgroundColor: 'rgba(22, 105, 122, 0.75)',
-                    borderColor: 'rgba(22, 105, 122, 0.75)',
+                    backgroundColor: color,
+                    borderColor: color,
                     data: bpiPrices
                 }
             ]
@@ -38,11 +37,28 @@ function printTheChart(data) {
 
 // Filtering by date
 
-document.onload = (getBPI(apiUrl));
+document.onload = (getBPI(apiUrl, 'rgba(22, 105, 122, 0.75)'));
 
 document.getElementById('filter-by-date-btn').addEventListener('click', () => {
     let startDate = document.getElementById('start-date').value;
     let endDate = document.getElementById('end-date').value;
-    console.log(startDate, endDate);
     getBPI(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`);
+})
+
+document.getElementById('currency').addEventListener('input', () => {
+    let currency = document.getElementById('currency').value;
+    switch(currency) {
+        case 'USD':
+            chartColor = 'rgba(22, 105, 122, 0.75)';
+            break;
+        case 'EUR':
+            chartColor = 'rgba(219, 100, 0, 0.75)';
+            break;
+        case 'GBP':
+            chartColor = 'rgba(239, 79, 79, 0.75)';
+            break;
+        default:
+            chartColor = 'rgba(187, 187, 187, 0.75)';
+    }
+    getBPI(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`, chartColor);
 })
