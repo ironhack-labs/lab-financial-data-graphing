7aa5aee121028bd707ef5f1c948d9ad47fdbbd23
Javier Repi <xavirepi@hotmail.com>
2021-02-10 21:05:35 +0100

    finish bonus iteration 5

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,17 +1,18 @@
 const apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json`
 //const currencyUrl = `https://api.coindesk.com/v1/bpi/currentprice/<CODE>.json`
 
-// Axios Get Request
+// Iteration 1 - Axios Get Request
 const getBPI = (url, chartColor) => {
     axios.get(url)
     .then(responseFromAPI => {
         console.log('responseFromAPI: ', responseFromAPI);
-        printTheChart(responseFromAPI.data, chartColor)
+        printTheChart(responseFromAPI.data, chartColor);
+        getMaxAndMin(responseFromAPI.data);
     })
     .catch(err => console.log('Error while getting the data: ', err))
 }
 
-// Printing the Chart
+// Iteration 2 - Printing the Chart
 function printTheChart(data, color) {
     const bpiHistory = data['bpi'];
     
@@ -35,16 +36,17 @@ function printTheChart(data, color) {
     })
 }
 
-// Filtering by date
-
 document.onload = (getBPI(apiUrl, 'rgba(22, 105, 122, 0.75)'));
 
+
+// Iteration 3 - Filtering by date
 document.getElementById('filter-by-date-btn').addEventListener('click', () => {
     let startDate = document.getElementById('start-date').value;
     let endDate = document.getElementById('end-date').value;
     getBPI(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`);
 })
 
+// Iteration 4 - Currency selection
 document.getElementById('currency').addEventListener('input', () => {
     let currency = document.getElementById('currency').value;
     switch(currency) {
@@ -62,3 +64,17 @@ document.getElementById('currency').addEventListener('input', () => {
     }
     getBPI(`https://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`, chartColor);
 })
+
+
+// Iteration 5 - Max/Min values
+function getMaxAndMin(data) {
+    const bpiHistory = data['bpi'];
+    const bpiDates = Object.keys(bpiHistory);
+    const bpiPrices = bpiDates.map(date => bpiHistory[date]);
+
+    document.getElementById('max-value').innerHTML = Math.max(...bpiPrices).toFixed(2);
+    document.getElementById('min-value').innerHTML = Math.min(...bpiPrices).toFixed(2);
+
+    const currencyValue = document.getElementsByClassName('currency-value');
+    [...currencyValue].forEach(element => element.innerHTML = currency.value)
+}
