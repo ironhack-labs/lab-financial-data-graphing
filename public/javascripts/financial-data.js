328b7367bd0a348707ac45d3a5dc3bb268b81988
Ángela Herrador <angelaherrador1991@gmail.com>
2021-02-13 00:40:28 +0100

    Add iterations 2, 3 and 4

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,8 +1,9 @@
 const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`
 
 //GET data from API
-axios
-    .get(apiUrl)
+const getDataInfo = (url) => {
+    axios
+    .get(url)
     .then((response) => {
         //console.log(response.data.bpi)
         const data = response.data.bpi
@@ -10,5 +11,48 @@ axios
         //console.log(xAxis)
         const yAxis = Object.values(data)
         //console.log(yAxis)
+        
+        paintData(xAxis, yAxis)
     })
-    .catch((e)=> console.log(e))
+    .catch((e) => console.log(e))
+}
+
+const paintData = (xAxis, yAxis) => {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xAxis,
+            datasets: [{
+                data: yAxis,
+                label: 'Bitcoin Price Index',
+                borderColor: "green",
+                backgroundColor: "transparent"
+            }]
+        }, 
+    })
+}
+
+document.onload = getDataInfo(apiUrl)
+
+document.getElementById('getValueBtn').addEventListener('click', () => {
+    let startDate = document.getElementById('startValue').value
+    let endDate = document.getElementById('endValue').value
+    const datesFilterUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}`
+    getDataInfo(datesFilterUrl)
+    //console.log(datesFilterUrl)
+    //console.log(startDate)
+})
+
+document.getElementById('currency').addEventListener('input', () => {
+    let currency = document.getElementById('currency').value
+    let startDate = document.getElementById('startValue').value
+    let endDate = document.getElementById('endValue').value
+    const currencyUrl = `https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`
+    getDataInfo(currencyUrl)
+    //console.log(currencyUrl)
+})
+
+
+
+    
