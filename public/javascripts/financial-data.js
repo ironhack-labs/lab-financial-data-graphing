0cfe0ca17ef5d4eeb5d83c24f38ca79457d4bec1
Pedro Frias <plfriasf@gmail.com>
2021-02-12 13:23:20 -0800

    Iteration #2: Create a chart done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -5,7 +5,32 @@ const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json"
 // get
 
 axios.get(apiUrl)
-    .then((data) => {
-        console.log(data)
+    .then((response) => {
+        const {
+            data
+        } = response
+        const xAxis = Object.keys(data.bpi)
+        const yAxis = Object.values(data.bpi);
+        paintChart(xAxis, yAxis)
+        console.log(yAxis)
     })
     .catch((e) => console.log("Error geting data", e))
+
+
+const paintChart = (xAxis, yAxis) => {
+    const ctx = document.getElementById("my-chart").getContext("2d");
+    const myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: xAxis,
+            datasets: [{
+                data: yAxis,
+                label: "Price",
+                borderColor: "blue",
+                backgroundColor: "transparent"
+
+            }]
+        }
+    })
+}
+
