fd5870a0016c5f88b81a534a4b2052029107a5f9
Pedro Frias <plfriasf@gmail.com>
2021-02-12 14:36:18 -0800

    Iteration #3: Dates Filter done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,4 +1,5 @@
 const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json"
+let myChart;
 
 
 
@@ -19,13 +20,13 @@ axios.get(apiUrl)
 
 const paintChart = (xAxis, yAxis) => {
     const ctx = document.getElementById("my-chart").getContext("2d");
-    const myChart = new Chart(ctx, {
+    myChart = new Chart(ctx, {
         type: 'line',
         data: {
             labels: xAxis,
             datasets: [{
                 data: yAxis,
-                label: "Price",
+                label: "BTC",
                 borderColor: "blue",
                 backgroundColor: "transparent"
 
@@ -34,3 +35,44 @@ const paintChart = (xAxis, yAxis) => {
     })
 }
 
+
+
+const query = () => {
+    const from = document.getElementById("dateFrom").value
+    const to = document.getElementById("dateTo").value;
+    const queryUrl = `${apiUrl}?start=${from}&end=${to}`;
+   
+    myChart.data.labels = [];
+    myChart.data.datasets = [];
+   
+
+   //get
+    axios.get(queryUrl)
+       .then((response) =>{
+           const { data } = response;
+           const xAxis = Object.keys(data.bpi);
+           const yAxis = Object.values(data.bpi);
+           updateData(myChart, xAxis, yAxis);
+           
+       }) 
+
+    
+
+}
+
+const button = document.getElementById("home-button").addEventListener("click", query)
+
+
+const updateData = (chart, label, yAxis) => {
+  chart.data.labels = label;
+  chart.data.datasets = [
+    {
+      label: "BTC",
+      borderColor: "blue",
+      backgroundColor: "transparent",
+      data: yAxis,
+    },
+  ];
+
+  chart.update();
+};
