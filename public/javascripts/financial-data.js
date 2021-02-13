0b08bfaa343796b7f7513bbe7b0722065b7c7943
Alicia Parra <alicia@redparra.com>
2021-02-13 14:58:44 +0100

    Do iteration 5 Lab done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -3,6 +3,8 @@ const apiUrl = `https://api.coindesk.com/v1/bpi/historical/close.json`
 let from;
 let to;
 let currency;
+let min;
+let max;
 
 const apiRequest = (url) => {
     axios.get(url)
@@ -10,17 +12,27 @@ const apiRequest = (url) => {
         const {data} = response
         let xAxis = Object.keys(data.bpi);
         let yAxis = Object.values(data.bpi);
+        
         drawChart(xAxis,yAxis)
+        limitValues(yAxis)
+        document.getElementById('max__price').innerHTML = max
+        document.getElementById('min__price').innerHTML = min
       })
       .catch((error) => {
         console.log(error);
       })
 }
 
+//MIN-MAX VALUES
+
+const limitValues = (pricesArr) => {
+    min = Math.min(...pricesArr).toFixed(2)
+    max = Math.max(...pricesArr).toFixed(2)
+}
 
 
-  //DRAW CHART
-  const drawChart= (xAxis,yAxis) => {
+//DRAW CHART
+const drawChart= (xAxis,yAxis) => {
     const ctx = document.getElementById("myChart").getContext("2d")
 
     const chart = new Chart(ctx, {
@@ -37,28 +49,28 @@ const apiRequest = (url) => {
         // Configuration options go here
         options: {}
     })
-  }
+}
 
-  //Show default dates at the beggining
-  apiRequest(apiUrl) 
+//Show default dates at the beggining
+apiRequest(apiUrl) 
 
-  //Change values when change input field
-  document.getElementById('from__date').addEventListener('change', () => {
-     from = document.getElementById('from__date').value
-     refreshChart()
-  })
-  document.getElementById('to__date').addEventListener('change', () => {
-     to = document.getElementById('to__date').value  
-     refreshChart()
-  })
-  document.getElementById('currency').addEventListener('change', () => {
+//Change values when change input field
+document.getElementById('from__date').addEventListener('change', () => {
+    from = document.getElementById('from__date').value
+    refreshChart()
+})
+document.getElementById('to__date').addEventListener('change', () => {
+    to = document.getElementById('to__date').value  
+    refreshChart()
+})
+document.getElementById('currency').addEventListener('change', () => {
     currency = document.getElementById('currency').value
     console.log(currency)
     refreshChart()
- })
+})
 
-  //Refresh chart with new values if both inputs are changed
-  const refreshChart = () => {
+//Refresh chart with new values if both inputs are changed
+const refreshChart = () => {
     if(to != undefined && from !=undefined){
         if(!currency){
             apiRequest(`${apiUrl}?start=${from}&end=${to}`)
@@ -72,7 +84,7 @@ const apiRequest = (url) => {
             apiRequest(`${apiUrl}?currency=${currency}`) 
         }   
     }
-  }
+}
   
 
 
