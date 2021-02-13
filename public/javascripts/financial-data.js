4b9c06a904191388741514df38dd123e25372112
Alicia Parra <alicia@redparra.com>
2021-02-13 11:20:57 +0100

    Do iteration 2

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -3,8 +3,30 @@ const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`
 axios.get(apiUrl)
   .then((response) => {
     const {data} = response
-    console.log(data);
+    //console.log(data);
+    const xAxis = Object.keys(data["bpi"]);
+    const yAxis = Object.values(data["bpi"]);
+    drawChart(xAxis,yAxis)
   })
   .catch((error) => {
     console.log(error);
   })
+
+  const drawChart= (xAxis,yAxis) => {
+    const ctx = document.getElementById("myChart").getContext("2d")
+
+    const chart = new Chart(ctx, {
+        type: 'line',
+        // The data for our dataset
+        data: {
+            labels: xAxis,
+            datasets: [{
+                label: 'Date',
+                borderColor: 'rgb(255, 99, 132)',
+                data: yAxis
+            }]
+        },
+        // Configuration options go here
+        options: {}
+    })
+  }
