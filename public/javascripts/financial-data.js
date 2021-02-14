f53bad99da66c9e42117f1e645da12c816f448a2
Pedro <pedroscampoy@gmail.com>
2021-02-15 00:24:56 +0100

    Iteration2

---
+++ public/javascripts/financial-data.js
@@ -0,0 +1,31 @@
+const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
+axios.get(apiUrl)
+  .then((response) => {
+    const { data } = response
+    const xAxis = Object.keys(data["bpi"]);
+    const yAxis = Object.values(data["bpi"]);
+    drawChart(xAxis, yAxis)
+  })
+  .catch((error) => {
+    console.log(error)
+  })
+
+const drawChart = (xAxis, yAxis) => {
+  const ctx = document.getElementById("myChart").getContext("2d")
+
+  const chart = new Chart(ctx, {
+    type: 'line',
+    // The data for our dataset
+    data: {
+      labels: xAxis,
+      datasets: [{
+        label: 'Date',
+        borderColor: 'rgb(255, 99, 132)',
+        data: yAxis
+      }]
+    },
+    // Configuration options go here
+    options: {}
+  })
+} 
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 /* GET home page. */
-router.get('/', (req, res) => res.render('index', { title: 'Express' }));
+router.get('/', (req, res) => res.render('index', { title: 'lab-financial-data-graphing' }));
 
 module.exports = router;
