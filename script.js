085fcc05d1185b004de1d7d053cfb2ccaef48a4a
luisga32 <luisga32@hotmail.com>
2021-02-14 10:27:05 +0100

    Iteration 2 done

--- script.js
+++ script.js
@@ -3,6 +3,32 @@ const myUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`
 
 axios.get(myUrl)
 .then((response) =>{
-   console.log(response.data)
+  
+   const datesSerie = Object.keys(response.data.bpi);
+   const valuesSerie = Object.values(response.data.bpi);
+   paintBitcoinHistoric(datesSerie,valuesSerie);
+ 
 })
 .catch((e) => console.log(`Error getting data: `, e));
+
+const paintBitcoinHistoric = (xAxis,yAxis) =>{
+
+    const ctx = document.getElementById("my-chart").getContext("2d");
+
+    const myLineChart = new Chart(ctx, {
+        type: 'line',
+         data: {
+            labels: xAxis,
+            datasets: [{
+                data: yAxis,
+                label:`Bitcoin Price Index`,
+                borderColor:'grey',
+                backgroundColor: 'transparent'
+            }
+
+            ]
+        },
+
+     
+    });
+}
