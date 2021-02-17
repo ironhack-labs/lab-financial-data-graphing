5a2c9acc39c79864b65c74143e91b59b56de7fa9
Pedro <pedroscampoy@gmail.com>
2021-02-17 10:32:35 +0100

    Iteration3

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,31 +1,74 @@
 const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+let startDate
+let endDate
 
-axios.get(apiUrl)
-  .then((response) => {
-    const { data } = response
-    const xAxis = Object.keys(data["bpi"]);
-    const yAxis = Object.values(data["bpi"]);
-    drawChart(xAxis, yAxis)
-  })
-  .catch((error) => {
-    console.log(error)
-  })
+const renderData = (url, start, end) => {
+  if (start && end) {
+    url = `${url}?start=${start}&end=${end}`
+  }
+  axios.get(url)
+    .then((response) => {
+      console.log(url)
+      const { data } = response
+      const xAxis = Object.keys(data["bpi"]);
+      const yAxis = Object.values(data["bpi"]);
+      renderChart(xAxis, yAxis)
+    })
+    .catch((error) => {
+      console.log(error)
+    })
+}
 
-const drawChart = (xAxis, yAxis) => {
+const renderChart = (xAxis, yAxis) => {
   const ctx = document.getElementById("myChart").getContext("2d")
 
   const chart = new Chart(ctx, {
     type: 'line',
-    // The data for our dataset
     data: {
       labels: xAxis,
       datasets: [{
-        label: 'Date',
-        borderColor: 'rgb(255, 99, 132)',
-        data: yAxis
+        label: 'Bitcoin Price Index (BPI)',
+        borderColor: 'gray',
+        data: yAxis,
+        backgroundColor: 'transparent',
+        lineTension: 0.1
       }]
     },
-    // Configuration options go here
-    options: {}
+    options: {
+      scales: {
+        yAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'BPI'
+          }
+        }],
+        xAxes: [{
+          scaleLabel: {
+            display: true,
+            labelString: 'Date'
+          }
+        }]
+      }
+    }
   })
-} 
+}
+
+renderData(apiUrl)
+
+//Iteration3
+const startDateInput = document.getElementById('start-date')
+const endDateInput = document.getElementById('end-date')
+
+startDateInput.addEventListener('change', () => {
+  startDate = startDateInput.value
+  console.log(startDate, endDate)
+  renderData(apiUrl, startDate, endDate)
+})
+endDateInput.addEventListener('change', () => {
+  endDate = endDateInput.value
+  console.log(startDate, endDate)
+  renderData(apiUrl, startDate, endDate)
+})
+
+console.log(startDate)
+
