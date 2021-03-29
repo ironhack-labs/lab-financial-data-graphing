e59b37f58892f4b251f6b8d96482a59ee6c64548
frankie.eckersley-carr <frankie.eckersley-carr@outlook.com>
2021-03-29 12:17:04 +0200

    complete iteration 3 and 4

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -2,37 +2,77 @@ let bitCoinData;
 let myLineChart;
 
 let ctx = document.getElementById("myChart").getContext("2d");
+let fromDate = document.getElementById("from-date");
+let toDate = document.getElementById("to-date");
+let currency = document.getElementById("currency");
+let dateWarning = document.getElementById("date-warning");
+
+fromDate.addEventListener("change", updateChart);
+toDate.addEventListener("change", updateChart);
+currency.addEventListener("change", updateChart);
 
 axios
   .get("http://api.coindesk.com/v1/bpi/historical/close.json")
   .then((response) => {
-    // console.log(response.data.bpi);
     return response.data.bpi;
   })
   .then((bitCoinData) => {
-    // first get the values as arrays
-    let bitCoinValue = Object.values(bitCoinData);
-    let bitCoinDates = Object.keys(bitCoinData);
-
-    drawChart(bitCoinDates, bitCoinValue);
+    drawChart(bitCoinData);
   })
   .catch((error) => {
     console.log(error);
   });
 
-function drawChart(chartLabels, chartData) {
+function drawChart(bitCoinData) {
+  let bitCoinValue = Object.values(bitCoinData);
+  let bitCoinDates = Object.keys(bitCoinData);
   myLineChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: chartLabels,
+      labels: bitCoinDates,
       datasets: [
         {
           label: "My First dataset",
           backgroundColor: "rgb(255, 99, 132)",
           borderColor: "rgb(255, 99, 132)",
-          data: chartData,
+          data: bitCoinValue,
         },
       ],
     },
   });
+}
+
+function updateChart() {
+  let fromDateString = fromDate.value;
+  let toDateString = toDate.value;
+  let currencyString = currency.value;
+  let requestUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDateString}&end=${toDateString}&currency=${currencyString}`;
+  let requestUrlCurrencyOnly = `http://api.coindesk.com/v1/bpi/historical/close.json?currency=${currencyString}`;
+  if (fromDateString && toDateString && toDateString > fromDateString) {
+    axios
+      .get(requestUrl)
+      .then((response) => {
+        return response.data.bpi;
+      })
+      .then((bitCoinData) => {
+        drawChart(bitCoinData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    dateWarning.innerHTML = "";
+  } else {
+    dateWarning.innerHTML = "Please enter valid From and To dates";
+    axios
+      .get(requestUrlCurrencyOnly)
+      .then((response) => {
+        return response.data.bpi;
+      })
+      .then((bitCoinData) => {
+        drawChart(bitCoinData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }
 }
