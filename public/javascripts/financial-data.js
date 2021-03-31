aab1bbc4cdd5e00c2c5627f0d0bea9cc96e158d1
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-03-31 14:30:44 -0300

    add iteration 2

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,12 +1,31 @@
-const getData = () => {
-    axios
-        .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-        .then(response => {
-          console.log(response);
-        })
-        .catch(err => {
-          next(error)
-        });
+const createChart = async () => {
+  try {
+    const response = await axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`);
+
+    const bpiDates = Object.keys(response.data.bpi);
+    const bpiPrices = Object.values(response.data.bpi);
+
+    const ctx = document.getElementById('myChart').getContext('2d');
+    
+    new Chart(ctx, {
+      type: 'line',
+      data: {
+        labels: bpiDates,
+        datasets: [{
+          label: 'Stock price',
+          backgroundColor: 'rgb(224, 224, 210)',
+          borderColor: 'rgb(171, 171, 135)',
+          data: bpiPrices
+        }]
+      }
+    })
+  }
+
+  catch (error) {
+    console.log(error);
+  }
+      
 }
 
-getData()
+createChart();
+
