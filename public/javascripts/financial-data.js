e0294c177260fc4abf85acc23ea8af140c70f02c
luisjhc <ljoaquincuenca@gmail.com>
2021-03-31 14:28:34 +0200

    bonus iterations 4 and 5 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,21 +1,70 @@
-let startDate = "";
+const baseUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+
+const currencyInput = document.getElementById("currency");
+let currency = currencyInput.value;
+
+const selectMaxValue = document.getElementById("maxValue");
+const selectMinValue = document.getElementById("minValue");
+const selectMaxCurrency = document.getElementById("maxCurrency");
+const selectMinCurrency = document.getElementById("minCurrency");
+
+
+//dates filter
+//getting the start date 
+let startDate;
 const startingInput = document.getElementById("StartDate");
 startingInput.addEventListener("input", (event) => {
-  console.log(event.target.value);
   startDate = event.target.value;
+  getHistoricalData();
 });
 
-let endDate = "";
+//getting the end date
+let endDate;
 const endInput = document.getElementById("EndDate");
 endInput.addEventListener("input", (event) => {
-  console.log(event.target.value);
   endDate = event.target.value;
+  getHistoricalData();
 });
 
+//getting the value of the currency
+currencyInput.onchange = (event) => {
+  currency = event.target.value;
+  bitcoinPriceTracker();
+};
+
+//function for date filter
+function getHistoricalData() {
+  //both dates, end and start, are needed to proceed with the request
+  if (!endDate || !startDate) {
+    return;
+  }
+  //start date must be lower than end date
+  const start = new Date (startDate);
+  const end = new Date (endDate);
+  if (end < start) {
+    return;
+  }
+  axios.get(`${baseUrl}?start=${startDate}&end=${endDate}`).then((response2) => {
+    const labels = Object.keys(response2.data.bpi);
+    const data = Object.values(response2.data.bpi);
+    drawChart(labels, data);
+  });
+}
+
+//function to request the values with the currency selected
+function bitcoinPriceTracker() {
+  axios.get(`${baseUrl}?currency=${currency}`).then((axiosResponse) => {
+    const labels = Object.keys(axiosResponse.data.bpi);
+    const data = Object.values(axiosResponse.data.bpi);
+    maxAndMinValues(data, currency);
+    drawChart(labels, data);
+  });
+}
+
+//default request
 axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json?start="+startDate+"&end="+endDate)
+  .get(baseUrl)
   .then((response) => {
-    console.log("response:", response.data);
     const labels = Object.keys(response.data.bpi);
     const data = Object.values(response.data.bpi);
     drawChart(labels, data);
@@ -23,6 +72,8 @@ axios
   .catch((err) => {
     console.log("err:", err.message);
   });
+
+  //function to draw the chart with the values selected
 function drawChart(labels, data) {
   var ctx = document.getElementById("myChart").getContext("2d");
   var myChart = new Chart(ctx, {
@@ -37,3 +88,18 @@ function drawChart(labels, data) {
     },
   });
 }
+
+//function to show the max/min values and adding the currency
+function maxAndMinValues(data, currency) {
+
+  const maxValue = Math.max.apply(null, data);
+  selectMaxValue.innerText = maxValue;
+  selectMaxCurrency.innerText = currency;
+
+  const minValue = Math.min.apply(null, data);
+  selectMinValue.innerText = minValue;
+  selectMinCurrency.innerText = currency;
+}
+
+//calling the currency function
+bitcoinPriceTracker();
