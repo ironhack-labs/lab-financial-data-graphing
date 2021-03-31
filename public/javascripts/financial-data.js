710d3db4fa45f3007cd3b8df200831c12ec7e9b3
Aldo <aldo.pena.93@hotmail.com>
2021-03-31 22:06:21 +0200

    done iteration 3

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,71 +1,65 @@
+const apiURL = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 const fromDate = document.getElementById("from-date");
 const toDate = document.getElementById("to-date");
-//const log = document.getElementById("values");
 const actionButton = document.getElementById("action-button");
 let historyDates = [];
 let historyPrices = [];
+let fromDateValue;
+let toDateValue;
 
-//function getDefaultData() {
-axios
-  .get(`http://api.coindesk.com/v1/bpi/historical/close.json`)
-  .then((response) => {
-    console.log("***response: ", response.data.bpi);
-    let retrievedData = response.data.bpi;
-    historyDates = Object.keys(retrievedData);
-    historyPrices = Object.values(retrievedData);
-    //drawChart(historyDates, historyPrices);
-  })
-  .catch((err) => console.log("error****", err));
-//}
+fromDate.addEventListener("change", (event) => {
+  fromDateValue = event.target.value;
+  getRangedData();
+});
 
-function getRangedData(fromDateValue, toDateValue) {
+toDate.addEventListener("change", (event) => {
+  toDateValue = event.target.value;
+  getRangedData();
+});
+
+function getDefaultData() {
   axios
-    .get(
-      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDateValue}&end=${toDateValue}`
-    )
+    .get(apiURL)
     .then((response) => {
-      console.log("***response: ", response.data.bpi);
-      // let retrievedData = response.data.bpi;
-      // historyDates = Object.keys(retrievedData);
-      // historyPrices = Object.values(retrievedData);
-      // drawChart(historyDates, historyPrices);
+      let retrievedData = response.data.bpi;
+      historyDates = Object.keys(retrievedData);
+      historyPrices = Object.values(retrievedData);
+      drawChart(historyDates, historyPrices, "rgb(5, 128, 56)");
+    })
+    .catch((err) => console.log("axios error****", err));
+}
+getDefaultData();
+
+function getRangedData() {
+  if (!fromDateValue || !toDateValue) {
+    return;
+  }
+  axios
+    .get(`${apiURL}?start=${fromDateValue}&end=${toDateValue}`)
+    .then((response) => {
+      let retrievedData = response.data.bpi;
+      historyDates = Object.keys(retrievedData);
+      historyPrices = Object.values(retrievedData);
+      drawChart(historyDates, historyPrices, "rgb(3, 19, 252)");
     })
     .catch((err) => console.log("error****", err));
 }
 
-// function drawChart(historyDates, historyPrices) {
-//   var ctx = document.getElementById("myChart").getContext("2d");
-//   var chart = new Chart(ctx, {
-//     type: "line",
-//     data: {
-//       labels: historyDates,
-//       datasets: [
-//         {
-//           label: "Historical BPI",
-//           backgroundColor: "rgb(255,215,0)",
-//           borderColor: "rgb(5, 128, 56)",
-//           data: historyPrices,
-//         },
-//       ],
-//     },
-//     options: {},
-//   });
-
-//   actionButton.addEventListener("click", () => {
-//     console.log(fromDateValue, toDateValue);
-//     getRangedData(fromDateValue, toDateValue);
-//   });
-// }
-// drawChart();
-
-actionButton.addEventListener("click", () => {
-  console.log("working");
-  //console.log(fromDate.value, toDate.value);
-  const fromDateValue = fromDate.value;
-  const toDateValue = toDate.value;
-  console.log(fromDateValue, toDateValue);
-  getRangedData(fromDateValue, toDateValue);
-  return;
-  //console.log(fromDateValue, toDateValue);
-  //getRangedData(fromDateValue, toDateValue);
-});
+function drawChart(historyDates, historyPrices, color) {
+  var ctx = document.getElementById("myChart").getContext("2d");
+  var chart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels: historyDates,
+      datasets: [
+        {
+          label: "Historical BPI",
+          backgroundColor: "rgb(255,215,0)",
+          borderColor: color,
+          data: historyPrices,
+        },
+      ],
+    },
+    options: {},
+  });
+}
