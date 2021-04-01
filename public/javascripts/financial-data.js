a8d95fd6db2701de0e2042c548f0ae3f0e387247
Aleksandra Pietraszek <ola.pietraszek@gmail.com>
2021-04-01 18:08:51 +0200

    Iteration 3&4 done

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,23 +1,63 @@
-let axisX;
-let axisY;
+const apiUrl = "http://api.coindesk.com/v1/bpi/historical/close.json";
+const startDateInput = document.getElementById("start");
+const endDateInput = document.getElementById("end");
+const currencyInput = document.getElementById("currency");
+let start;
+let end;
+let currency = currencyInput.value;
 
-function drawChart(labels, data) {
-  const ctx = document.getElementById("myChart");
+updateChart();
 
-  let myChart = new Chart(ctx, {
-    type: "line",
-    data: {
-      labels: labels,
-      datasets: [{ label: `Bitcoin Price Index`, data: data }],
-    },
+startDateInput.addEventListener("change", (event) => {
+  start = event.target.value;
+  getHistoricalData();
+});
+
+endDateInput.addEventListener("change", (event) => {
+  end = event.target.value;
+  getHistoricalData();
+});
+
+currencyInput.onChange = (event) => {
+  currency = event.target.value;
+  updateChart();
+};
+
+function getHistoricalData() {
+  if (!end || !start) {
+    return;
+  }
+  const startDate = new Date(start);
+  const endDate = new Date(end);
+
+  if (endDate < startDate) {
+    return;
+  }
+
+  axios
+    .get(`${apiUrl}?start=${start}&end=${end}&currency=${currency}`)
+    .then((response) => {
+      const axisX = Object.keys(response.data.bpi);
+      const axisY = Object.values(response.data.bpi);
+      drawChart(axisX, axisY);
+    });
+}
+
+function updateChart() {
+  axios.get(`${apiUrl}?currency=${currency}`).then((response) => {
+    const axisX = Object.keys(response.data.bpi);
+    const axisY = Object.values(response.data.bpi);
+    drawChart(axisX, axisY);
   });
 }
 
-axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-  .then((response) => {
-    axisX = Object.keys(response.data.bpi);
-    axisY = Object.values(response.data.bpi);
-    drawChart(axisX, axisY);
-  })
-  .catch((err) => console.log(err));
+function drawChart(labels, data) {
+  const ctx = document.getElementById("myChart");
+  let myChart = new Chart(ctx, {
+    type: "line",
+    data: {
+      labels,
+      datasets: [{ label: `Bitcoin Price Index`, data }],
+    },
+  });
+}
