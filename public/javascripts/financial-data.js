500bd9d382f0dbe0b73fec4072f16e8d64e7bdff
Thaís Bitencourt <thais.bitencourt@yahoo.com>
2021-04-28 13:34:10 -0300

    add iteration 3

--- public/javascripts/financial-data.js
+++ public/javascripts/financial-data.js
@@ -1,31 +1,48 @@
-const createChart = async () => {
+const createChart = async (options) => {
+  const url = `http://api.coindesk.com/v1/bpi/historical/close.json` +
+    (options ? `?start=${options[0]}&end=${options[1]}` : "");
+
   try {
-    const response = await axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json`);
+    const response = await axios.get(url);
+    console.log(response.data);
 
     const bpiDates = Object.keys(response.data.bpi);
-    const bpiPrices = Object.values(response.data.bpi);
+    const bpiPrices = Object.values(response.data.bpi).map(
+      (val) => Math.round(val) / 1000
+    );
 
-    const ctx = document.getElementById('myChart').getContext('2d');
-    
+    const ctx = document.getElementById("myChart").getContext("2d");
+
     new Chart(ctx, {
-      type: 'line',
+      type: "line",
       data: {
         labels: bpiDates,
-        datasets: [{
-          label: 'Stock price',
-          backgroundColor: 'rgb(224, 224, 210)',
-          borderColor: 'rgb(171, 171, 135)',
-          data: bpiPrices
-        }]
-      }
-    })
-  }
-
-  catch (error) {
+        datasets: [
+          {
+            label: "Stock price",
+            backgroundColor: "rgb(224, 224, 210)",
+            borderColor: "rgb(171, 171, 135)",
+            data: bpiPrices,
+          },
+        ],
+      },
+    });
+  } catch (error) {
     console.log(error);
   }
-      
-}
+};
 
-createChart();
+window.onload = () => {
+  createChart();
 
+  const datesButton = document.getElementById("get-dates-btn");
+
+  datesButton.addEventListener("click", () => {
+    let start = document.getElementById("starter-date").value;
+    let end = document.getElementById("end-date").value;
+    
+    if (!start || !end) return window.alert('Both start and end must be filled.')
+
+    createChart([start, end]);
+  })
+};
