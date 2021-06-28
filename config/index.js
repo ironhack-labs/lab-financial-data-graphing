976d16c6503e9909e8b962f0fc23a7b46b373a7a
Elizabeth Colastra <elizabethcolastra@hotmail.es>
2021-06-28 21:15:11 +0200

    Iteration 1 completed

--- config/index.js
+++ config/index.js
@@ -1,32 +1,38 @@
-// We reuse this import in order to have access to the `body` property in requests
-const express = require("express");
+// // We reuse this import in order to have access to the `body` property in requests
+// const express = require("express");
 
-// ℹ️ Responsible for the messages you see in the terminal as requests are coming in
-// https://www.npmjs.com/package/morgan
-const logger = require("morgan");
+// // ℹ️ Responsible for the messages you see in the terminal as requests are coming in
+// // https://www.npmjs.com/package/morgan
+// const logger = require("morgan");
 
-// ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
-// https://www.npmjs.com/package/cookie-parser
-const cookieParser = require("cookie-parser");
+// // ℹ️ Needed when we deal with cookies (we will when dealing with authentication)
+// // https://www.npmjs.com/package/cookie-parser
+// const cookieParser = require("cookie-parser");
 
-// ℹ️ global package used to `normalize` paths amongst different operating systems
-// https://www.npmjs.com/package/path
-const path = require("path");
+// // ℹ️ global package used to `normalize` paths amongst different operating systems
+// // https://www.npmjs.com/package/path
+// const path = require("path");
 
-// Middleware configuration
-module.exports = (app) => {
-  // In development environment the app logs
-  app.use(logger("dev"));
+// // Middleware configuration
+// module.exports = (app) => {
+//   // In development environment the app logs
+//   app.use(logger("dev"));
 
-  // To have access to `body` property in the request
-  app.use(express.json());
-  app.use(express.urlencoded({ extended: false }));
-  app.use(cookieParser());
+//   // To have access to `body` property in the request
+//   app.use(express.json());
+//   app.use(express.urlencoded({ extended: false }));
+//   app.use(cookieParser());
 
-  // Normalizes the path to the views folder
-  app.set("views", path.join(__dirname, "..", "views"));
-  // Sets the view engine to handlebars
-  app.set("view engine", "hbs");
-  // Handles access to the public folder
-  app.use(express.static(path.join(__dirname, "..", "public")));
-};
+//   // Normalizes the path to the views folder
+//   app.set("views", path.join(__dirname, "..", "views"));
+//   // Sets the view engine to handlebars
+//   app.set("view engine", "hbs");
+//   // Handles access to the public folder
+//   app.use(express.static(path.join(__dirname, "..", "public")));
+// };
+//Peticion a Axios para hacer peticiones HTTP que nos devuelve promesas
+axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+  .then((response) => {
+    console.log(response)
+  })
+  .catch(e => console.error(e))
