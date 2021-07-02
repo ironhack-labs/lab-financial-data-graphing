490485a59b310587f167c385333902983b435701
Shirley Gianina <shirley.cd853f@gmail.com>
2021-07-02 18:54:21 +0200

    Iteracion 3 y Bonus

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,31 +1,56 @@
-const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+function updateChart() {
+  const fromDate = document.getElementById("fromDate").value;
+  const toDate = document.getElementById("toDate").value;
+  const currency = document.getElementById("currency").value;
+  filterData(fromDate, toDate, currency);
+}
 
-axios
-  .get(apiUrl)
-  .then((responseFromAPI) => {
-    console.log(responseFromAPI);
-    printTheChart(responseFromAPI.data);
-  })
-  .catch((err) => console.log("Error while getting the data: ", err));
-
-function printTheChart(stockData) {
-  const dailyData = stockData["bpi"];
-
-  const stockDates = Object.keys(dailyData);
-  const stockPrices = Object.values(dailyData);
+function printChart(stockDates, stockPrices) {
   const ctx = document.getElementById("my-chart").getContext("2d");
-  const chart = new Chart(ctx, {
+
+  const minPrice = Math.min(...stockPrices)
+  const maxPrice = Math.max(...stockPrices)
+
+  document.getElementById('min').innerText = minPrice
+  document.getElementById('max').innerText = maxPrice
+
+  ctx.clearRect(0, 0, 700, 400);
+
+  new Chart(ctx, {
     type: "line",
     data: {
       labels: stockDates,
       datasets: [
         {
-          label: "Stock Chart",
-          backgroundColor: "rgb(255, 99, 132)",
-          borderColor: "rgb(255, 99, 132)",
+          label: "Stock BPI",
+          backgroundColor: "rgb(255,215,0)",
+          borderColor: "rgb(218,165,32)",
           data: stockPrices,
         },
       ],
     },
-  }); // closes chart = new Chart()
-} // closes printTheChart()
+  });
+}
+
+function filterData(fromDate, toDate, currency) {
+  axios
+    .get(
+      `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}`
+    )
+    .then((response) => {
+      const BPI = response.data.bpi;
+      const stockDates = Object.keys(BPI);
+      const stockPrices = Object.values(BPI);
+
+      printChart(stockDates, stockPrices);
+    })
+    .catch((err) => console.log("Error while getting the data: ", err));
+}
+
+
+updateChart()
+document.getElementById("btn").addEventListener("click", updateChart);
+
+
+
+
