4fa8c965b613273296b9c3ad403efb23ba794aec
kpla001 <kpla001@fiu.edu>
2021-09-02 15:42:54 -0400

    fix currency selection bug

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -3,15 +3,10 @@ window.onload = () => {
     axios.get(url)
     .then((responseFromAPI) => {
         // console.log(responseFromAPI);
-
         const timeSeries = responseFromAPI.data.bpi;
-
         const labels = Object.keys(timeSeries);
-
         const prices = labels.map((label) => timeSeries[label]);
         // console.log(labels, prices)
-
-
         const ctx = document.getElementById("myChart").getContext("2d");
             myChart = new Chart(ctx, {
             type: "line",
@@ -47,18 +42,15 @@ window.onload = () => {
     document.getElementById("select-dates").addEventListener("click", () => {
         const startDate = document.getElementById("start-date").value;
         const endDate = document.getElementById("end-date").value;
-        let currency = "USD";
+        let currency = document.getElementById("pickCurrency").value;
         
         axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
             .then((dataFromAPI) => {
-                console.log(dataFromAPI);
+                // console.log(dataFromAPI);
     
                 const bpi = dataFromAPI.data.bpi;
-    
                 const labels = Object.keys(bpi);
-    
                 const prices = labels.map((label) => bpi[label]);
-    
                 const ctx = document.getElementById("myChart").getContext("2d");
                 myChart.destroy();
                 myChart = new Chart(ctx, {
@@ -99,16 +91,52 @@ window.onload = () => {
         const startDate = document.getElementById("start-date").value;
         const endDate = document.getElementById("end-date").value;
         
-        axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
+        if (startDate == "" && endDate == "") {
+            axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?currency=${currency}`)
             .then((dataFromAPI) => {
                 // console.log(dataFromAPI);
-    
                 const bpi = dataFromAPI.data.bpi;
-    
                 const labels = Object.keys(bpi);
-    
                 const prices = labels.map((label) => bpi[label]);
-    
+                const ctx = document.getElementById("myChart").getContext("2d");
+                myChart.destroy();
+                myChart = new Chart(ctx, {
+                    type: "line",
+                    data: {
+                        labels,
+                        datasets: [
+                        {
+                            label: "Bitcoin Prices",
+                            data: prices,
+                            backgroundColor: [
+                            "rgba(255, 99, 132, 0.2)",
+                            "rgba(54, 162, 235, 0.2)",
+                            "rgba(255, 206, 86, 0.2)",
+                            "rgba(75, 192, 192, 0.2)",
+                            "rgba(153, 102, 255, 0.2)",
+                            "rgba(255, 159, 64, 0.2)",
+                            ],
+                            borderColor: [
+                            "rgba(255, 99, 132, 1)",
+                            "rgba(54, 162, 235, 1)",
+                            "rgba(255, 206, 86, 1)",
+                            "rgba(75, 192, 192, 1)",
+                            "rgba(153, 102, 255, 1)",
+                            "rgba(255, 159, 64, 1)",
+                            ],
+                            borderWidth: 1,
+                        },
+                        ],
+                    },
+                });
+            })    
+        } else { 
+                axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
+                .then((dataFromAPI) => {
+                // console.log(dataFromAPI);
+                const bpi = dataFromAPI.data.bpi;
+                const labels = Object.keys(bpi);
+                const prices = labels.map((label) => bpi[label]);
                 const ctx = document.getElementById("myChart").getContext("2d");
                 myChart.destroy();
                 myChart = new Chart(ctx, {
@@ -141,9 +169,8 @@ window.onload = () => {
                     },
                 });
             })
+        }
     });
-
-
 }
 
 
