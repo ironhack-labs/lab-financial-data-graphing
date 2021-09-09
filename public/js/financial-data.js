c0899a570c8453cb3b960f198c182b27dc8a82bc
Rahaf <r.abualhassan@gmail.com>
2021-09-09 23:37:11 +0200

    #4 still under construction

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,4 +1,4 @@
-const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json';
+const apiUrl = 'http://api.coindesk.com/v1/bpi/historical/close.json?index=[USD/CNY]';
 
 axios.get(apiUrl)
 	.then(response => {
@@ -11,6 +11,11 @@ axios.get(apiUrl)
 
 
     const printChart = data => {
+
+        // currencies
+        const currencies = document.getElementById('select-currency');
+
+        // data from API
         const CoinDeskHisData = data.bpi;
         // x axis
         const dates = Object.keys(CoinDeskHisData);
@@ -25,8 +30,18 @@ axios.get(apiUrl)
     
          const ctx = document.querySelector('#myChart').getContext('2d');
 
-    
-        new Chart(ctx, {
+         const skipped = (ctx, value) => ctx.p0.skip || ctx.p1.skip ? value : undefined;
+         const down = (ctx, value) => ctx.p0.parsed.y > ctx.p1.parsed.y ? value : undefined;
+
+         const genericOptions = {
+            fill: false,
+            interaction: {
+              intersect: false
+            },
+            radius: 0,
+          };
+
+        const myChart = new Chart(ctx, { 
             type: 'line',
             data: {
                 // x - axis
@@ -35,41 +50,58 @@ axios.get(apiUrl)
                     {
                         label: 'Historical BPI data',
                         backGroundColor: 'rgb(10, 43, 194)',
-                        borderColor: 'rgb(10, 43, 194)',
-                        
-                        // y - axis 
-                        data: CoinDeskHisData,
-
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 0.2)',
-                            'rgba(54, 162, 235, 0.2)',
-                            'rgba(255, 206, 86, 0.2)',
-                            'rgba(75, 192, 192, 0.2)',
-                            'rgba(153, 102, 255, 0.2)',
-                            'rgba(255, 159, 64, 0.2)'
-                        ],
-                        borderColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
+                        borderColor: 'rgb(75, 192, 192)',
+                        // y - axis
+                        data: bitcoinVal,
+                        segment: {
+                            borderColor: ctx => skipped(ctx, 'rgb(0,0,0,0.2)') || down(ctx, 'rgb(192,75,75)'),
+                            borderDash: ctx => skipped(ctx, [6, 6]),
+                          }, 
+                          
+                         
                         borderWidth: 1
                     },
                     
                 ],
                 
             },
+            options: genericOptions
             
         })
-        function filterDates(){
+        window.filterDates = function () {
             const dates2 = [...dates];
-            console.log(dates2);
-            const startDate = document.querySelector('#start-date');
-            const endDate = document.querySelector('#end-date');
+
+            const startDate = document.getElementById('start-date');
+            const endDate = document.getElementById('end-date');
+
+            // get the index number of the startDate
+            const indexStartD = dates2.indexOf(startDate.value);
+            
+            const indexEndD = dates2.indexOf(endDate.value);
+            
+            // slice the array only showing the selected section / selected slice
+            const filterDate = dates2.slice(indexStartD, indexEndD +1);
+            
+            // replace the labels in the chart
+            myChart.data.labels = filterDate;
+
+            const bitcoinVal2 = [...bitcoinVal];
+            const filterData = bitcoinVal2.slice(indexStartD, indexEndD + 1);
+
+            myChart.data.datasets[0].data = filterData;
+
+            myChart.update();
+            
         }
+
+        
+        currencies.addEventListener('change', chooseCurrency);
+        
+        function chooseCurrency() {
+            // connect currencies.value with the bitcoinVal 
+            // also check if apiUrl is correct
+        }
+        chooseCurrency()
     };
 
     
