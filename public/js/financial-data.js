606d541e05559c7b5e3e42b285a2e4454b0a2867
AitorSantaeugenia <aitor.j.s.m@gmail.com>
2021-09-13 00:30:42 +0200

    iteration 3

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -48,4 +48,59 @@ window.onload = () => {
 			}
 		});
 	});
+
+	const searchButton = document.getElementById('searchDates');
+	searchButton.addEventListener('click', () => {
+		const startingDate = document.getElementById('startingDate').value;
+		const endingDate = document.getElementById('endingDate').value;
+		//console.log(startingDate);
+		//console.log(endingDate);
+		const url = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${startingDate}&end=${endingDate}&currency=BTC`;
+		console.log(url);
+		axios.get(url).then((responseFromAPI) => {
+			//console.log(responseFromAPI);
+			const labels = Object.keys(responseFromAPI.data.bpi);
+			const prices = labels.map((label) => responseFromAPI.data.bpi[label]);
+			const ctx = document.getElementById('myChart').getContext('2d');
+			const myChart = new Chart(ctx, {
+				type: 'line',
+				data: {
+					labels,
+					datasets: [
+						{
+							label: 'Prices of bitcoin price index',
+							data: prices,
+							backgroundColor: [
+								'rgba(255, 99, 132, 0.2)',
+								'rgba(54, 162, 235, 0.2)',
+								'rgba(255, 206, 86, 0.2)',
+								'rgba(75, 192, 192, 0.2)',
+								'rgba(153, 102, 255, 0.2)',
+								'rgba(255, 159, 64, 0.2)'
+							],
+							borderColor: [
+								'rgba(255, 99, 132, 1)',
+								'rgba(54, 162, 235, 1)',
+								'rgba(255, 206, 86, 1)',
+								'rgba(75, 192, 192, 1)',
+								'rgba(153, 102, 255, 1)',
+								'rgba(255, 159, 64, 1)'
+							],
+							borderWidth: 1
+						}
+					]
+				},
+				options: {
+					responsive: true,
+					//add this to read keys
+					maintainAspectRatio: false,
+					scales: {
+						y: {
+							beginAtZero: true
+						}
+					}
+				}
+			});
+		});
+	});
 };
