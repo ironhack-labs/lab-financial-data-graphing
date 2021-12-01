238684f31b1b2b00e9a5264fbef51803fbe586da
Philipp Kastl <picastle@posteo.de>
2021-12-01 20:41:06 +0100

    add iteration 3 and 4

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,6 +1,16 @@
 
-axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-.then(res => {
-    var data = res;
-})
-.catch(error => console.error(error))
+      var fromDate = document.querySelector('#fromDate')
+      var toDate = document.querySelector('#toDate')
+      var currency = 'USD';
+      fromDate.addEventListener('change', function(){
+        fromDate.value = this.value;
+        
+      })
+    const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate.value}&end=${toDate.value}&currency=${currency}`
+      axios
+      axios.get(apiUrl)
+      .then(responseFromAPI => {
+        var data = responseFromAPI.data
+      })
+      .catch(err => console.log('Error while getting the data: ', err));
+
--- public/js/script.js
+++ public/js/script.js
@@ -1,18 +1,58 @@
+
 document.addEventListener(
 
   "DOMContentLoaded",
   () => {
+  
 
+    var fromDate = document.querySelector('#fromDate')
+    var toDate = document.querySelector('#toDate')
+
+    var currency = document.querySelector('#currency')
+
+
+    fromDate.addEventListener('change', function(){
+      fromDate.value = this.value;
+      console.log(fromDate.value)
+      newCanvas();
+      getData()
+      
+    })
+    toDate.addEventListener('change', function(){
+      toDate.value = this.value;
+      console.log(toDate.value)
+      newCanvas();
+      getData()
+      
+    })
+
+    currency.addEventListener('change', function(){
+      currency.value = this.value;
+      console.log(currency.value)
+      newCanvas();
+      getData()
+      
+    })
+
+    getData();
+    function getData(){
+
+    
+  const apiUrl = `http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate.value}&end=${toDate.value}&currency=${currency.value}`
     axios
-    axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+    axios.get(apiUrl)
     .then(responseFromAPI => {
-      printTheChart(responseFromAPI.data);
+
+      printTheChart(responseFromAPI.data)
     })
     .catch(err => console.log('Error while getting the data: ', err));
+  }
+
    
   function printTheChart(stockData) {
+
     const ctx = document.getElementById('myChart').getContext('2d');
-    const chart = new Chart(ctx, {
+     chart = new Chart(ctx, {
       type: 'line',
       data: {
         datasets: [
@@ -25,7 +65,18 @@ document.addEventListener(
         ]
       }
     }); 
+    
   } 
+
+  function newCanvas(){
+    const canvas = document.querySelector('#myChart');
+    const canvasContainer = document.querySelector('#ctn-canvas')
+    canvas.remove()
+    canvasContainer.innerHTML = `<canvas id="myChart" width="400" height="400"></canvas>`;
+
+  }
+  
+
     console.log("lab-financial-data-graphing JS imported successfully!");
   },
   false
