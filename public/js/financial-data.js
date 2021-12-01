e8203b2882bae5b256ad7c22863262761ec936b9
Philipp Kastl <picastle@posteo.de>
2021-12-01 18:33:46 +0100

    add iteration 2

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,6 +1,6 @@
+
 axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
-.then(responseFromApi => console.log(responseFromApi))
+.then(res => {
+    var data = res;
+})
 .catch(error => console.error(error))
-
-
-
--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,34 @@
 document.addEventListener(
+
   "DOMContentLoaded",
   () => {
+
+    axios
+    axios.get("http://api.coindesk.com/v1/bpi/historical/close.json")
+    .then(responseFromAPI => {
+      printTheChart(responseFromAPI.data);
+    })
+    .catch(err => console.log('Error while getting the data: ', err));
+   
+  function printTheChart(stockData) {
+    const ctx = document.getElementById('myChart').getContext('2d');
+    const chart = new Chart(ctx, {
+      type: 'line',
+      data: {
+        datasets: [
+          {
+            label: 'Bitcoin Price',
+            backgroundColor: 'rgb(255, 99, 132)',
+            borderColor: 'rgb(255, 99, 132)',
+            data: stockData.bpi
+          }
+        ]
+      }
+    }); 
+  } 
     console.log("lab-financial-data-graphing JS imported successfully!");
   },
   false
 );
+
+
