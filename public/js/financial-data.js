e0d1d1a6f1f307743eb3cedcc4c7383b51b40252
betilla x <aclara_77@yahoo.com>
2022-02-05 16:39:13 +0000

    iteration 2

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,9 +1,34 @@
 const URL = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
 axios.get(URL)
-.then((data)=>
-{console.log(data);})
+.then((data)=>{
+let bpi = data.data.bpi;
+console.log(typeof bpi)
+printChart(bpi);
+})
 .catch((err)=>{
     console.log(err);
-    next(err);
 })
+
+function printChart(data){
+    const time = Object.keys(data);
+    let value = []
+    for (let key in data ){
+        value.push(data[key]);
+    }
+   console.log(time);
+   console.log(value);
+   const ctx = document.getElementById('chart').getContext('2d');
+   const myChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels:time,
+        datasets: [{
+            label: "bitcoin value",
+            data: value,
+            fill:false,
+            borderColor: 'rgb(75, 192, 192)',
+        }]
+      }
+   } )  
+}
