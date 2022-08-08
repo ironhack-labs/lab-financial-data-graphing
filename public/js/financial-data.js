0c014bf4aad79a33d59f2cd9458ae728d32f2318
Tony5300 <tony_tolen@ziggo.nl>
2022-08-08 18:21:51 +0200

    almost finished iteration 3, did some, refactored my code more so it is easier to read.

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,36 +1,56 @@
 // Iteration 1:
-const ctx = document.getElementById("myChart");
+const BITCOIN_API = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
-axios
-  .get("http://api.coindesk.com/v1/bpi/historical/close.json")
-  .then((bitcoin) => {
-    console.log(bitcoin);
+// Iteration 3: functions to change the dates:
+const startDate = document.querySelector("#fromDate");
+const endDate = document.querySelector("#toDate");
 
-    const myChart = new Chart(ctx, {
-      type: "line",
-      data: {
-        labels: Object.keys(bitcoin.data.bpi),
-        datasets: [
-          {
-            label: "Bitcoin Price indexes",
+startDate.addEventListener("change", () => {
+  fromDate = startDate.value;
+  console.log(`Here is the startdate:`, fromDate);
+  updatedChart();
+});
 
-            data: Object.values(bitcoin.data.bpi),
+endDate.addEventListener("change", () => {
+  toDate = endDate.value;
+  console.log(`Here is the end date:`, toDate);
+  updatedChart();
+});
 
-            backgroundColor: ["rgba(153, 102, 255, 0.2)"],
-            borderColor: ["rgba(54, 162, 235, 1)"],
-            borderWidth: 1,
-          },
-        ],
-      },
-      options: {
-        scales: {
-          y: {
-            beginAtZero: true,
-          },
+updatedChart();
+
+function createChart(bitcoinYAxis, bitcoinXAxis) {
+  let ctx = document.getElementById("myChart");
+  ctx = new Chart(ctx, {
+    type: "line",
+    data: {
+      label: bitcoinYAxis,
+      datasets: [
+        {
+          label: "Bitcoin Price indexes",
+          backgroundColor: "rgb(153, 102, 255)",
+          borderColor: "rgb(54, 162, 235)",
+          data: bitcoinXAxis,
         },
-      },
-    });
-  })
-  .catch((err) => {
-    console.log(err);
+      ],
+    },
   });
+}
+
+function updatedChart() {
+  axios
+    .get(`${BITCOIN_API}`)
+    .then((bitcoin) => {
+      console.log(bitcoin);
+      console.log("DATE:", Object.keys(bitcoin.data.bpi));
+      console.log("Values:", Object.values(bitcoin.data.bpi));
+
+      const dates = Object.keys(bitcoin.data.bpi);
+      const values = Object.values(bitcoin.data.bpi);
+
+      createChart(dates, values);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
