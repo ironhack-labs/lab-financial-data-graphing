cdf7eea8462e4a2674b9f550f25f145a92acb932
Tony5300 <tony_tolen@ziggo.nl>
2022-08-12 14:29:07 +0200

    added some comments

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,46 +1,50 @@
 const BITCOIN_API = `http://api.coindesk.com/v1/bpi/historical/close.json`;
 
-let myChart;
-let fromDate = new Date("2022-01-01").toISOString().split("T")[0];
+let myChart; // global variable to be used to destroy the old chart before it updates to a new chart with newer dates
+
+/* create the dates for the input fields */
+let fromDate = new Date("2022-01-01").toISOString().split("T")[0]; //don't know what it is is doing but it converts the date to the ISO 8601 format, that somehow works with the input field. found this on stack overflow
 let toDate = new Date().toISOString().split("T")[0];
 
-/*Event listener from date */
+/* event listener from date */
 document.querySelector("#fromDate").addEventListener("change", (e) => {
   fromDate = new Date(e.target.value).toISOString().split("T")[0];
   updateBitcoinChart();
   console.log(`Hello from the fromDate Input`);
-});
+}); // the changing of dates is an event. use the value of the fromDate variable to be used in the input field
 
-/*Event listener to date */
+/* event listener to date */
 document.querySelector("#toDate").addEventListener("change", (e) => {
   toDate = new Date(e.target.value).toISOString().split("T")[0];
   updateBitcoinChart();
   console.log(`Hello from the toDate Input`);
-});
+}); // the changing of dates is an event. use the value of the toDate variable to be used in the input field
 
+/* connect to the bitcoin api */
 function getBitcoinData(fromDate, toDate) {
   axios
     .get(`${BITCOIN_API}?start=${fromDate}&end=${toDate}`)
     .then((bitcoinInfo) => {
       console.log(bitcoinInfo);
 
-      createBitcoinChart(bitcoinInfo.data.bpi);
+      createBitcoinChart(bitcoinInfo.data.bpi); // call the createBitcoinChart function to give the parameter to the function itself to be used inside.
     })
     .catch((err) => {
       console.log(`Oopsie daisy, there went something wrong`, err);
     });
 }
 
+/* create the chart */
 function createBitcoinChart(bitcoinInfo) {
   const ctx = document.getElementById("myChart").getContext("2d");
   myChart = new Chart(ctx, {
     type: "line",
     data: {
-      labels: Object.keys(bitcoinInfo), // add the date of the bitcoin api
+      labels: Object.keys(bitcoinInfo),
       datasets: [
         {
           label: "Price Index Bitcoin",
-          data: Object.values(bitcoinInfo), // add the data of the bitcoin api
+          data: Object.values(bitcoinInfo),
           backgroundColor: ["rgba(255, 99, 132, 0.2)"],
           borderColor: ["rgba(153, 102, 255, 1)"],
           borderWidth: 1,
@@ -57,9 +61,11 @@ function createBitcoinChart(bitcoinInfo) {
   });
 }
 
+/* update the chart */
 function updateBitcoinChart() {
   myChart.destroy();
   getBitcoinData(fromDate, toDate);
 }
 
+/* call the bitcoin data function to show it */
 getBitcoinData(fromDate, toDate);
