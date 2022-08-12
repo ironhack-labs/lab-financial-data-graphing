c7f2bd426f07dcbc187d6563210d1784793ab0d3
Tony5300 <tony_tolen@ziggo.nl>
2022-08-12 13:48:42 +0200

    finished iteration 1 and 2.

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,56 +1,98 @@
-// Iteration 1:
-const BITCOIN_API = "http://api.coindesk.com/v1/bpi/historical/close.json";
+const BITCOIN_API = `http://api.coindesk.com/v1/bpi/historical/close.json`;
+let myChart;
 
-// Iteration 3: functions to change the dates:
-const startDate = document.querySelector("#fromDate");
-const endDate = document.querySelector("#toDate");
+function getBitcoinData() {
+  axios
+    .get(`${BITCOIN_API}`)
+    .then((bitcoinInfo) => {
+      console.log(bitcoinInfo);
 
-startDate.addEventListener("change", () => {
-  fromDate = startDate.value;
-  console.log(`Here is the startdate:`, fromDate);
-  updatedChart();
-});
+      createBitcoinChart(bitcoinInfo.data.bpi);
+    })
+    .catch((err) => {
+      console.log(`Oopsie daisy, there went something wrong`, err);
+    });
+}
 
-endDate.addEventListener("change", () => {
-  toDate = endDate.value;
-  console.log(`Here is the end date:`, toDate);
-  updatedChart();
-});
-
-updatedChart();
-
-function createChart(bitcoinYAxis, bitcoinXAxis) {
-  let ctx = document.getElementById("myChart");
-  ctx = new Chart(ctx, {
+function createBitcoinChart(bitcoinInfo) {
+  const ctx = document.getElementById("myChart").getContext("2d");
+  myChart = new Chart(ctx, {
     type: "line",
     data: {
-      label: bitcoinYAxis,
+      // labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"], // add the date of the bitcoin api
+      labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"], // add the date of the bitcoin api
       datasets: [
         {
-          label: "Bitcoin Price indexes",
-          backgroundColor: "rgb(153, 102, 255)",
-          borderColor: "rgb(54, 162, 235)",
-          data: bitcoinXAxis,
+          label: "Price Index Bitcoin",
+          data: [12, 19, 3, 5, 2, 3], // add the data of the bitcoin api
+          backgroundColor: ["rgba(255, 99, 132, 0.2)"],
+          borderColor: ["rgba(153, 102, 255, 1)"],
+          borderWidth: 1,
         },
       ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
     },
   });
 }
 
-function updatedChart() {
-  axios
-    .get(`${BITCOIN_API}`)
-    .then((bitcoin) => {
-      console.log(bitcoin);
-      console.log("DATE:", Object.keys(bitcoin.data.bpi));
-      console.log("Values:", Object.values(bitcoin.data.bpi));
+getBitcoinData();
 
-      const dates = Object.keys(bitcoin.data.bpi);
-      const values = Object.values(bitcoin.data.bpi);
+// getBitcoinData();
+// // Iteration 1:
+// const BITCOIN_API = "http://api.coindesk.com/v1/bpi/historical/close.json";
 
-      createChart(dates, values);
-    })
-    .catch((err) => {
-      console.log(err);
-    });
-}
+// // Iteration 3: functions to change the dates:
+// const startDate = document.querySelector("#fromDate");
+// const endDate = document.querySelector("#toDate");
+
+// startDate.addEventListener("change", () => {
+//   fromDate = startDate.value;
+//   console.log(`Here is the startdate:`, fromDate);
+//   updatedChart();
+// });
+
+// endDate.addEventListener("change", () => {
+//   toDate = endDate.value;
+//   console.log(`Here is the end date:`, toDate);
+//   updatedChart();
+// });
+
+// updatedChart();
+
+// function createChart(bitcoinYAxis, bitcoinXAxis) {
+//   let ctx = document.getElementById("myChart");
+//   ctx = new Chart(ctx, {
+//     type: "line",
+//     data: {
+//       label: bitcoinYAxis,
+//       datasets: [
+//         {
+//           label: "Bitcoin Price indexes",
+//           backgroundColor: "rgb(153, 102, 255)",
+//           borderColor: "rgb(54, 162, 235)",
+//           data: bitcoinXAxis,
+//         },
+//       ],
+//     },
+//   });
+// }
+
+// function updatedChart() {
+//   axios
+//     .get(`${BITCOIN_API}`)
+//     .then((bitcoin) => {
+//       const dates = bitcoin.data.bpi;
+//       const values = bitcoin.data.bpi;
+
+//       createChart(dates, values);
+//     })
+//     .catch((err) => {
+//       console.log(err);
+//     });
+// }
