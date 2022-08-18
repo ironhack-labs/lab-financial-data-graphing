be17b5e7b9e80a4f873aba27932c0a6aaaa92579
Clark68500 <mcevoyclark@gmail.com>
2022-08-18 17:50:30 +0200

    solved

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,12 +1,3 @@
-// const apiUrl = "https://api.coindesk.com/v1/bpi/historical/close.json";
-
-// const apiUrl2 =
-//   "https://api.coindesk.com/v1/bpi/historical/close.json?start=2010-09-01&end=2022-09-05";
-
-// get https://api.coindesk.com/v1/bpi/historical/close.json?start=2013-09-01&end=2013-09-05
-
-// http://api.coindesk.com/v1/bpi/historical/close.json?start=${fromDate}&end=${toDate}&currency=${currency}
-
 function getDom() {
   const startDate = document.getElementById("begin").value;
   const endDate = document.getElementById("end").value;
@@ -31,6 +22,21 @@ button.addEventListener("click", function (event) {
 function printTheChart(histBtcPrice) {
   const btcDates = Object.keys(histBtcPrice);
   const btcPrices = Object.values(histBtcPrice);
+  //console.log(Math.min.apply(Math, btcPrices), Math.max.apply(Math, btcPrices));
+
+  //   document.getElementById("high").innerHTML = `MAX23: ${Math.max.apply(
+  //     Math,
+  //     btcPrices
+  //   )}`;
+  //   document.getElementById("low").innerHTML = `MIN32: ${Math.min.apply(
+  //     Math,
+  //     btcPrices
+  //   )}`;
+
+  document.getElementsByClassName("high")[0].innerHTML =
+    "MAX: " + String(Math.max.apply(Math, btcPrices));
+  document.getElementsByClassName("low")[0].innerHTML =
+    "MIN: " + String(Math.min.apply(Math, btcPrices));
 
   const ctx = document.getElementById("canvas-chart").getContext("2d");
   const chart = new Chart(ctx, {
