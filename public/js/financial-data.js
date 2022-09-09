d2a6d8591f1b23b39021c286b1dfdf84a08d7bac
Jaime Martin <martinjaime1119@gmail.com>
2022-09-09 09:37:22 +0200

    Async/await refactor

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -8,9 +8,6 @@ document.addEventListener(
         let startDate = document.getElementById('start').value
         let endDate = document.getElementById('end').value
 
-        // Pinta la primera vez
-        printGraph(startDate, endDate, currency)
-
         const start = document.getElementById('start')
         start.addEventListener('input', setStartDate)
 
@@ -22,13 +19,13 @@ document.addEventListener(
 
         function setStartDate(start) {
             startDate = start.target.value
-            console.log('startDate', endDate, startDate)
+            console.log('startDate changed')
             printGraph(startDate, endDate, currency)
         }
 
         function setEndDate(end) {
             endDate = end.target.value
-            console.log('endDate', endDate, startDate)
+            console.log('endDate changed')
             printGraph(startDate, endDate, currency)
         }
 
@@ -38,57 +35,48 @@ document.addEventListener(
             printGraph(startDate, endDate, currency)
         }
 
-        function printGraph(startDate, endDate, currency) {
-            console.log('Borrando')
+        const printGraph = async (startDate, endDate, currency) => {
             if (myChart !== null) {
+                console.log('Borrando')
                 myChart.destroy()
             }
             console.log('Pintando')
             console.log(startDate)
             console.log(endDate)
 
-            let btcPrices
+            const res = await axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
+            const btcPrices = res.data.bpi
 
-            axios.get(`https://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate}&end=${endDate}&currency=${currency}`)
-                .then((res) => {
-                    btcPrices = res.data.bpi
+            const labels = Object.keys(btcPrices);
 
-                    const labels = Object.keys(btcPrices);
+            const maxPrice = Math.max(...Object.values(btcPrices))
+            const minPrice = Math.min(...Object.values(btcPrices))
 
-                    const maxPrice = Math.max(...Object.values(btcPrices))
-                    const minPrice = Math.min(...Object.values(btcPrices))
+            document.getElementById('max').textContent = maxPrice
+            document.getElementById('min').textContent = minPrice
 
-                    document.getElementById('max').textContent = maxPrice
-                    document.getElementById('min').textContent = minPrice
+            const data = {
+                labels: labels,
+                datasets: [{
+                    label: 'BTC prices',
+                    backgroundColor: 'rgb(255, 99, 132)',
+                    borderColor: 'rgb(255, 99, 132)',
+                    data: Object.values(btcPrices),
+                }]
+            };
 
-                    const data = {
-                        labels: labels,
-                        datasets: [{
-                            label: 'BTC prices',
-                            backgroundColor: 'rgb(255, 99, 132)',
-                            borderColor: 'rgb(255, 99, 132)',
-                            data: Object.values(btcPrices),
-                        }]
-                    };
+            const config = {
+                type: 'line',
+                data: data,
+                options: {}
+            };
 
-                    const config = {
-                        type: 'line',
-                        data: data,
-                        options: {}
-                    };
-
-                    myChart = new Chart(
-                        document.getElementById('graph'),
-                        config
-                    );
-
-
-
-                })
-                .catch((err) => console.log(err))
-
-
+            myChart = new Chart(
+                document.getElementById('graph'),
+                config
+            );
         }
+        printGraph(startDate, endDate, currency)
     },
     false
 );
