094b5d03fac83ca86c1e0befcfd4483ebbc57f0b
Nicolas_M <nicolas.meot@live.fr>
2023-04-22 16:07:36 +0200

    iteration 3

--- public/js/financial-data.js
+++ public/js/financial-data.js
@@ -1,31 +1,54 @@
 
+const startDate = document.querySelector(".startDate")
+const endDate = document.querySelector(".endDate")
+
 const button = document.querySelector("button")
+let compteur = 0;
 
 button.addEventListener("click", () => {
-    axios.get("http://api.coindesk.com/v1/bpi/historical/close.json?start=2021-01-01&end=2022-12-31")
-    .then(responseFromApi => console.log("this is what we get from the API", responseFromApi))
+    console.log("StartDate =", startDate,"StartDate value =", startDate.value);
+    console.log("EndDate =", endDate,"EndDate value =", endDate.value);
+    compteur +=1
+    if(compteur > 1){
+        chart.destroy()
+    }
+    axios.get(`http://api.coindesk.com/v1/bpi/historical/close.json?start=${startDate.value}&end=${endDate.value}`)
+    .then(responseFromApi => {
+     printTheChart(responseFromApi)
+    })
     .catch(error => console.log("Stg went wrong",error))
 })
 
-const ctx = document.querySelector('#myChart');
-const chart = new Chart(ctx, {
-    type: 'line',
-    data: {
-      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-      datasets: [{
-        label: '# of Votes',
-        data: [12, 19, 3, 5, 2, 3],
-        borderWidth: 1
-      }]
-    },
-    options: {
-      scales: {
-        y: {
-          beginAtZero: true
+
+function printTheChart(stockData){
+    const values = stockData.data.bpi
+    console.log(values)
+    const dates = Object.keys(values);
+    const prices = dates.map(date => values[date]);
+    const ctx = document.querySelector('#myChart')
+
+    chart = new Chart(ctx, {
+        type: 'line',
+        data: {
+          labels: dates,
+          datasets: [{
+            label: '# of Votes',
+            data: prices,
+            borderWidth: 1
+          }]
+        },
+        options: {
+          scales: {
+            y: {
+              beginAtZero: true
+            }
+          }
         }
-      }
-    }
-  });
+      });
+    window.chart = chart
+    console.log('chart=', chart)
+}
+
 
 
 
